e6b43a1e0b2fc2b5262e114105b56dedd333c146
Trent Weiss <ttw2xk@virginia.edu>
2018-02-01 20:03:20 -0500

    it prints the steering angles. yay

--- data-logger/src/logger.cpp
+++ data-logger/src/logger.cpp
@@ -1,8 +1,12 @@
 
+#define ZMQ_BUILD_DRAFT_API
 #include <stdio.h>
-#include <zmq.hpp>
+#include <exception>
+#include "car_data\car_data.h"
+#define INCOMING_BUFLEN 1289
 int main(int argc, char** argv) {
 
-	printf("%s","Hello World!");
+
+	
 	return 0;
 }
