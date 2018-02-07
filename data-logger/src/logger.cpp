018a61ac17f3139cfd237c376316a4dc88e3dfce
Trent Weiss <ttw2xk@virginia.edu>
2018-02-07 00:16:46 -0500

    holy shit it compiles...

--- data-logger/src/logger.cpp
+++ data-logger/src/logger.cpp
@@ -1,12 +1,25 @@
 
-#define ZMQ_BUILD_DRAFT_API
+#include <Windows.h>
+#include<stdio.h>
+#include <boost/timer/timer.hpp>
+#include "simple_udp_listener.h"
 #include <stdio.h>
 #include <exception>
-#include "car_data\car_data.h"
-#define INCOMING_BUFLEN 1289
+#include <boost/bind.hpp>
+#include <boost/thread/thread.hpp>
+#define BUFLEN 1289   //Max length of buffer
+#define PORT 20777   //The port on which to listen for incoming data
+
 int main(int argc, char** argv) {
 
-
+	boost::shared_ptr<const boost::timer::cpu_timer> timer(new boost::timer::cpu_timer);
+	printf("Elapsed time is: %d", timer->elapsed().wall);
 	
+	simple_udp_listener udp_listener(timer);
+	boost::thread udp_thread(boost::bind(&simple_udp_listener::listen, &udp_listener));
+	//boost::function<void (const boost::timer::cpu_timer&, timestamped_udp_data_t[], unsigned int)> f1 = boost::bind(&udp_worker, (const boost::timer::cpu_timer) timer, udp_dataz, MAX_FRAMES);
+	//
+	//boost::thread screencap_trhead(boost::bind(&::screencap_worker, image_dataz, timer, MAX_FRAMES));
+
 	return 0;
 }
--- data-logger/src/simple_udp_listener.cpp
+++ data-logger/src/simple_udp_listener.cpp
@@ -2,16 +2,21 @@
 Simple UDP Server
 */
 
+
+#include "simple_udp_listener.h"
+
 #include<stdio.h>
 #include<winsock2.h>
-
-#include "car_data\car_data.h"
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
-
-#define BUFLEN sizeof(UDPPacket)  //Max length of buffer
+#define UDP_BUFLEN 1289   //Max length of buffer
 #define PORT 20777   //The port on which to listen for incoming data
-#define MAX_FRAMES 10000
-int main()
+simple_udp_listener::simple_udp_listener(boost::shared_ptr<const boost::timer::cpu_timer>& timer) {
+	this->timer = timer;
+}
+simple_udp_listener::~simple_udp_listener() {
+
+}
+void simple_udp_listener::listen()
 {
 	SOCKET s;
 	struct sockaddr_in server, si_other;
@@ -47,26 +52,22 @@ int main()
 		printf("Bind failed with error code : %d", WSAGetLastError());
 		exit(EXIT_FAILURE);
 	}
-	puts("Bind done");
-	int BYTES_TO_GET = BUFLEN;
 	//keep listening for data
-	UDPPacket* packet = (UDPPacket*)malloc(BYTES_TO_GET);
 	unsigned int i = 0;
-	while (i++<MAX_FRAMES)
+	while (i++<2500)
 	{
 
 
-		int rcv_len = recvfrom(s, (char*)packet, BYTES_TO_GET, 0, (struct sockaddr *) &si_other, &slen);
-		if (rcv_len != BYTES_TO_GET) {
+		int rcv_len = recvfrom(s, (char*)&(dataz[i].data), UDP_BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
+		if (rcv_len != UDP_BUFLEN) {
 			printf("Socket error when receiving telemetry data.");
 			exit(-1);
 		}
-		printf("Steering angle: %f\n", packet->m_steer);
+		dataz[i].timestamp = timer->elapsed();
+		//printf("Steering angle: %f\n", packet->m_steer);
 
 	}
 
 	closesocket(s);
 	WSACleanup();
-	free(packet);
-	return 0;
 }
