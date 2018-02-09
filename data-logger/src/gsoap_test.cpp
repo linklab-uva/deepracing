47b9e1b22481a95c804130b6527dc3a06f664156
Trent <ttw2xkre@gmail.com>
2018-02-08 21:20:41 -0500

    it actually compiles...

---
+++ data-logger/src/gsoap_test.cpp
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <deepf1_gsoap/deepf1_nsmap.nsmap>
+//using namespace deepf1;
+int main(int argc, char** argv)
+{
+
+	struct soap soap; // the gSOAP runtime context
+	soap_init(&soap);
+	return 0;
+}
