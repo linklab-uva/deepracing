ea18a0e7e23025e45bc04be35fef9011db1400f9
Trent <ttw2xkre@gmail.com>
2018-02-09 00:04:54 -0500

    HOLY SWEET BABY JESUS IT WORKS

--- data-logger/src/gsoap_test.cpp
+++ data-logger/src/gsoap_test.cpp
@@ -1,11 +1,34 @@
 
 #include <stdio.h>
-#include <deepf1_gsoap/deepf1_nsmap.nsmap>
-//using namespace deepf1;
+//#include "envH.h"
+#include <iostream>
+#include <fstream>
+#include <deepf1_gsoap/deepf1_gsoap.nsmap>
 int main(int argc, char** argv)
 {
 
-	struct soap soap; // the gSOAP runtime context
-	soap_init(&soap);
+	struct soap* soap = soap_new();
+	soap_set_mode(soap, SOAP_XML_INDENT);
+	std::fstream* fs = new std::fstream("cpu_times.xml", std::fstream::out);
+	deepf1::cpu_times* cpu_times = deepf1::soap_new_cpu_times(soap);
+	cpu_times->system = 123;
+	cpu_times->user = 456;
+	cpu_times->wall = 789;
+	soap->os = fs;
+	if (deepf1::soap_write_cpu_times(soap, cpu_times) != SOAP_OK)
+	{
+		std::cerr << "Error writing cpu_times.xml file" << std::endl;
+		soap_stream_fault(soap, std::cerr);
+		exit(1);
+	}
+	fs->close();
+	delete fs;
+	// Delete instances
+	soap_destroy(soap);
+	// Delete data
+	soap_end(soap);
+	// Free soap struct engine context
+	soap_free(soap);
+
 	return 0;
 }
