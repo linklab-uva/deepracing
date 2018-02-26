be093b679317bf98802c9c755fa9524c08cc13d8
Trent <ttw2xkre@gmail.com>
2018-02-26 13:02:36 -0500

    add more robust options for telemetry simulator

--- data-logger/src/simulated_telemetry_server.cpp
+++ data-logger/src/simulated_telemetry_server.cpp
@@ -14,17 +14,27 @@ using namespace deepf1;
 namespace po = boost::program_options;
 int main(int argc, char** argv) {
 	unsigned int sleep_time;
+	std::string address, port;
 	po::options_description desc("Allowed Options");
 	desc.add_options()
-		("sleep_time", po::value<unsigned int>(&sleep_time)->default_value(100), "Number of milliseconds to sleep between simulated packets")
+		("help,h", "Displays options and exits")
+		("address,a", po::value<std::string>(&address)->default_value(ADDR), "IPv4 Address to send data to")
+		("port_number,p", po::value<std::string>(&port)->default_value(PORT), "Port number to send data to")
+		("sleep_time,s", po::value<unsigned int>(&sleep_time)->default_value(100), "Number of milliseconds to sleep between simulated packets")
 		;
 	po::variables_map vm;
 	po::store(po::parse_command_line(argc, argv, desc), vm);
 	po::notify(vm);
-
+	if (vm.find("help") != vm.end()) {
+		std::stringstream ss;
+		ss << "F1 Simulated Telemetry Server. Command line arguments are as follows:" << std::endl;
+		desc.print(ss);
+		std::printf("%s", ss.str().c_str());
+		exit(0);
+	}
 	boost::asio::io_service io_service;
 	udp::resolver resolver(io_service);
-	udp::resolver::query query(udp::v4(), ADDR, PORT);
+	udp::resolver::query query(udp::v4(), address, port);
 	udp::endpoint receiver_endpoint = *resolver.resolve(query);
 	udp::socket socket(io_service);
 	socket.open(udp::v4());
