20f6501d29c16631ee8f40a5d8d42b05555e31cc
Trent Weiss <ttw2xk@virginia.edu>
2018-04-06 18:32:03 -0400

    save all raw data to file.

--- data-logger/src/logger.cpp
+++ data-logger/src/logger.cpp
@@ -55,10 +55,10 @@ int main(int argc, char** argv) {
 		("udp_frames,u", po::value<unsigned int>(&udp_len)->default_value(100), "How many frames of game data to capture")
 		("port_number,p", po::value<unsigned short>(&port_number)->default_value(PORT), "Port number to listen for telemetry data on")
 		("screen_frames,s", po::value<unsigned int>(&image_len)->default_value(100), "How many frames of screencap data to capture")
-		("capture_x,x", po::value<float>(&capture_x)->default_value(100), "x coordinate for origin of capture area in pixels")
-		("capture_y,y", po::value<float>(&capture_y)->default_value(250), "y coordinate for origin of capture area pixels")
+		("capture_x,x", po::value<float>(&capture_x)->default_value(0), "x coordinate for origin of capture area in pixels")
+		("capture_y,y", po::value<float>(&capture_y)->default_value(325), "y coordinate for origin of capture area pixels")
 		("capture_width,w", po::value<float>(&capture_width)->default_value(1600), "Width of capture area pixels")
-		("capture_height,h", po::value<float>(&capture_height)->default_value(375), "height of capture area pixels")
+		("capture_height,h", po::value<float>(&capture_height)->default_value(300), "height of capture area pixels")
 		("max_delta,m", po::value<float>(&max_delta)->default_value(25.0), "Maximum difference in timestamp (in milliseconds) to allow for associating data to an image")
 		("data_directory,d", po::value<std::string>(&data_directory)->default_value(std::string("data")), "Top-level directory to place the annotations & images.")
 		("initial_sleep_time,i", po::value<long>(&sleep_time)->default_value(5000), "How many milliseconds to sleep before starting data recording.")
@@ -85,15 +85,19 @@ int main(int argc, char** argv) {
 	deepf1::simple_screen_listener screen_listener(timer, capture_area, application_name, image_len);
 
 
-	std::function<void()> screen_worker = std::bind(&deepf1::simple_screen_listener::listen, &screen_listener);
 	std::function<void()> udp_worker = std::bind(&deepf1::simple_udp_listener::listen, &udp_listener);
+	Sleep(1500);
+	std::function<void()> screen_worker = std::bind(&deepf1::simple_screen_listener::listen, &screen_listener);
 
 	std::thread udp_thread(udp_worker);
 	std::thread screen_thread(screen_worker);
 	screen_thread.join();
+	udp_listener.stop();
 	udp_thread.join();
 	std::vector<deepf1::timestamped_image_data_t> screen_data = screen_listener.get_data();
 	std::vector<deepf1::timestamped_udp_data_t> udp_data = udp_listener.get_data();
+	std::cout << "Received " << udp_data.size() << " elements" << std::endl;
+
 	writeToFiles(data_directory, screen_data, udp_data, max_delta);
 
 	return 0;
@@ -142,11 +146,28 @@ namespace deepf1 {
 		fs::create_directory(annotations_dir);
 		fs::path images_dir = root_dir / fs::path("images");
 		fs::create_directory(images_dir);
+
+
+		fs::path raw_annotations_dir = root_dir / fs::path("raw_annotations");
+		fs::create_directory(raw_annotations_dir);
+		fs::path raw_images_dir = root_dir / fs::path("raw_images");
+		fs::create_directory(raw_images_dir);
+
+		fs::path raw_images_timestamps = raw_images_dir / fs::path("raw_image_timestamps.csv");
 		soap* soap = soap_new(SOAP_XML_INDENT);
 		deepf1_gsoap_conversions::gsoap_conversions convert(soap);
 		unsigned long point_number = 1;
+		unsigned long raw_point_number = 1;
 		std::shared_ptr<std::fstream> file_out;
+		std::shared_ptr<std::ofstream> raw_image_timestamps_stream(new std::ofstream(raw_images_timestamps.string(), std::fstream::out));
 		for (auto it = screen_data.begin(); it != screen_data.end(); it++) {
+			
+			std::stringstream raw_image_ss;
+			raw_image_ss << "raw_image_" << raw_point_number << ".jpg";
+			fs::path raw_image_path = raw_images_dir / fs::path(raw_image_ss.str());
+			cv::imwrite(raw_image_path.string(), (it->image));
+			(*raw_image_timestamps_stream) << raw_image_ss.str() << ", " << (it->timestamp.wall) << std::endl;
+			raw_point_number++;
 
 			deepf1::timestamped_udp_data udp_tag = find_closest_value(udp_data, it->timestamp);
 			float delta = ((float)(std::abs(udp_tag.timestamp.wall - it->timestamp.wall)))/1E6;
@@ -176,6 +197,24 @@ namespace deepf1 {
 			++point_number;
 		
 			fs::path annotation_path = annotations_dir / fs::path(annotation_ss.str());
+			file_out.reset(new std::fstream(annotation_path.string(), std::fstream::out));
+			soap->os = file_out.get();
+			deepf1_gsoap::soap_write_ground_truth_sample(soap, ground_truth);
+			file_out->close();
+		}
+		raw_point_number = 1;
+		for (int i = 0; i < udp_data.size(); i++) {
+			deepf1::timestamped_udp_data current_data = udp_data[i];
+			::deepf1_gsoap::ground_truth_sample * ground_truth = deepf1_gsoap::soap_new_ground_truth_sample(soap);
+			::deepf1_gsoap::UDPPacket* pack = convert.convert_to_gsoap(*(current_data.data));
+			ground_truth->sample = *pack;
+			ground_truth->timestamp = current_data.timestamp.wall;
+			ground_truth->image_file = "NOT ASSOCIATED";
+
+			std::stringstream annotation_ss;
+			annotation_ss << "raw_data_point_" << raw_point_number << ".xml";
+			++raw_point_number;
+			fs::path annotation_path = raw_annotations_dir / fs::path(annotation_ss.str());
 			file_out.reset(new std::fstream(annotation_path.string(), std::fstream::out));
 			soap->os = file_out.get();
 			deepf1_gsoap::soap_write_ground_truth_sample(soap, ground_truth);
