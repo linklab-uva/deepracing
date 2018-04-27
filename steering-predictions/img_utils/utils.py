bd9dea03d6d136714ebb309f08c7cb92d3ff649c
Trent Weiss <ttw2xk@virginia.edu>
2018-04-27 19:50:38 -0400

    add ability to overlay steering wheel

---
+++ steering-predictions/img_utils/utils.py
@@ -0,0 +1,10 @@
+def overlay_image(dest_image, src_image, x_offset, y_offset):
+    y1, y2 = y_offset, y_offset + src_image.shape[0]
+    x1, x2 = x_offset, x_offset + src_image.shape[1]
+
+    alpha_s = src_image[:, :, 3] / 255.0
+    alpha_l = 1.0 - alpha_s
+
+    for c in range(0, 3):
+        dest_image[y1:y2, x1:x2, c] = (alpha_s * src_image[:, :, c] +
+                                  alpha_l * dest_image[y1:y2, x1:x2, c])
