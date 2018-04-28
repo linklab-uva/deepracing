6627bb0b1d95e9562188831e10cbbbfb70584263
Trent Weiss <ttw2xk@virginia.edu>
2018-04-27 21:07:38 -0400

    it writes to file. yay

--- steering-predictions/overlay_images.py
+++ steering-predictions/overlay_images.py
@@ -9,8 +9,8 @@ import argparse
 import os
 def main():
     parser = argparse.ArgumentParser(description="Deepf1 playground")
-    parser.add_argument("--x", type=int, default=0,  help="X coordinate of uppper-left corner of box to capture")
-    parser.add_argument("--y", type=int, default=0,  help="Y coordinate of uppper-left corner of box to capture")
+    parser.add_argument("--x", type=int, default=0,  help="X coordinate for where to put the steering wheel")
+    parser.add_argument("--y", type=int, default=0,  help="Y coordinate for where to put the steering wheel")
     args = parser.parse_args()
     x = args.x
     y = args.y
@@ -18,16 +18,23 @@ def main():
     annotations_path = os.path.join('slow_run_australia_track2','zeroth_degree_interpolation.csv')
     annotations_file = open(annotations_path,'r')
     annotations = annotations_file.readlines()
-    print(annotations[50].split(","))
-    print(len(annotations))
-    print(img_path)
     background = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
     wheel = cv2.imread('steering_wheel.png', cv2.IMREAD_UNCHANGED)  
+    output_folder = os.path.join('slow_run_australia_track2','overlayed_images')
+    filename, ts, anglestr = annotations[0].split(",")
+    anglestr = anglestr.replace("\n","")
     cv2.namedWindow('Display image')          ## create window for display
     imutils.overlay_image(background,wheel,x,y)
     cv2.imshow('Display image', background)          ## Show image in the window
     cv2.waitKey(0)
-
+    angle = float(anglestr)
+    print(angle)
+    name, ext = filename.split(".")
+    _,_,img_num_str = name.split("_")
+    img_num_str = img_num_str.replace("\n","")
+    print(img_num_str)
+    output_path = os.path.join(output_folder,'overlayed_image_' + img_num_str + ".jpg")
+    cv2.imwrite(output_path,background)
     
 
 if __name__ == '__main__':
