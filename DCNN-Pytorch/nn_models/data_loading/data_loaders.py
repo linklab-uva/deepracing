66d6a6b35326720925cd4a37a287eabbae8e3f62
Trent Weiss <ttw2xk@virginia.edu>
2018-05-03 18:46:14 -0400

    it trains

--- DCNN-Pytorch/nn_models/data_loading/data_loaders.py
+++ DCNN-Pytorch/nn_models/data_loading/data_loaders.py
@@ -3,19 +3,52 @@ import torch.random
 from torch.utils.data.dataset import Dataset
 import os
 from data_loading.image_loading import load_image
+from tqdm import tqdm as tqdm
+import pickle
 class F1Dataset(Dataset):
-    def __init__(self, root_folder, annotation_filepath):
+    def __init__(self, root_folder, annotation_filepath, im_size, label_size):
         super(F1Dataset, self).__init__()
+        self.im_size=im_size
         self.root_folder = root_folder
         self.annotation_filepath = annotation_filepath
         self.annotations_file = open(os.path.join(self.root_folder,self.annotation_filepath), "r")
         self.annotations = self.annotations_file.readlines()
+        self.length = len(self.annotations)
+        self.images = torch.empty(self.__len__(),im_size[0],im_size[1],im_size[2], dtype=torch.float)
+        self.labels = torch.empty(self.__len__(),label_size, dtype=torch.float)
+    def write_pickles(self,image_pickle, label_pickle):
+        filename = image_pickle
+        fp = open(filename, 'wb')
+        pickle.dump(self.images.numpy(), fp)
+        fp.close()
+        print('File %s is saved.' % filename)
+
+        filename = label_pickle
+        fp = open(filename, 'wb')
+        pickle.dump(self.labels.numpy(), fp)
+        fp.close()
+        print('File %s is saved.' % filename)
+    def read_pickles(self,image_pickle, label_pickle):
+        filename = image_pickle
+        fp = open(filename, 'rb')
+        self.images = torch.from_numpy(pickle.load(fp))
+        fp.close()
+
+        filename = label_pickle
+        fp = open(filename, 'rb')
+        self.labels =  torch.from_numpy(pickle.load(fp))
+        fp.close()
+    def read_files(self):
+        idx = 0
+        print("loading data")
+        for line in tqdm(self.annotations):
+            fp, ts, steering, throttle, brake = line.split(",")
+            self.labels[idx].fill_(float(steering))
+            im = load_image(os.path.join(self.root_folder,"raw_images",fp),size=(self.im_size[1],self.im_size[2]),scale_factor=255.0)
+            self.images[idx]=torch.from_numpy(im)
+            idx = idx + 1   
+           
     def __getitem__(self, index):
-        # stuff
-        fp, ts, steering, throttle, brake = self.annotations[index].split(",")
-        label = torch.empty(1, dtype=torch.double)
-        label.fill_(float(steering))
-        im = load_image(os.path.join(self.root_folder,"raw_images",fp),size=(66,200),scale_factor=255.0)
-        return torch.from_numpy(im), label
+        return self.images[index], self.labels[index]
     def __len__(self):
-        return len(self.annotations)
+        return self.length
--- DCNN-Pytorch/nn_models/main.py
+++ DCNN-Pytorch/nn_models/main.py
@@ -1,4 +1,3 @@
-import torch
 import cv2
 import numpy as np
 import nn_models
@@ -6,25 +5,69 @@ import data_loading.image_loading as il
 import nn_models.Models as models
 import data_loading.data_loaders as loaders
 import numpy.random
+import torch, random
+import torch.nn as nn 
+import torch.optim as optim
+from tqdm import tqdm as tqdm
+import pickle
+from datetime import datetime
+import os
+import string
+def run_epoch(network, criterion, optimizer, trainLoader, use_gpu):
+    network.train()  # This is important to call before training!
+    cum_loss = 0.0
+    batch_size = trainLoader.batch_size
+    num_samples=0
+    t = tqdm(enumerate(trainLoader))
+    for (i, (inputs, labels)) in t:
+        if use_gpu:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+        # Forward pass:
+        outputs = network(inputs)
+        loss = criterion(outputs, labels)
+
+        # Backward pass:
+        optimizer.zero_grad()
+        # Loss is a variable, and calling backward on a Variable will
+        # compute all the gradients that lead to that Variable taking on its
+        # current value.
+        loss.backward() 
+
+        # Weight and bias updates.
+        optimizer.step()
+
+        # logging information.
+        cum_loss += loss.item()
+        num_samples += batch_size
+        t.set_postfix(cum_loss = cum_loss/num_samples)
+ 
+
+def train_model(network, criterion, optimizer, trainLoader, file_prefix, n_epochs = 10, use_gpu = False):
+    if use_gpu:
+        network = network.cuda()
+        criterion = criterion.cuda()
+    # Training loop.
+    for epoch in range(0, n_epochs):
+        print("Epoch %d of %d" %((epoch+1),n_epochs))
+        run_epoch(network, criterion, optimizer, trainLoader, use_gpu)
+        log_path = os.path.join("log",""+file_prefix+str((epoch+1))+ ".model")
+        torch.save(network.state_dict(), log_path)
 im = il.load_image("test_image.jpg",size=(66,200),scale_factor=255.0)
 print(im)
-pilot_net = models.PilotNet()
-pilot_net.double()
-pilot_net.train()
-print(pilot_net)
-input = torch.from_numpy(np.random.rand(10,3,66,200))
-results = pilot_net(input)
-print(results)
+network = models.PilotNet()
+network.float()
 
+learningRate = 0.01
+trainset = loaders.F1Dataset("D:\\test_data\\slow_australia_track_run3","run3_linear.csv",(3,66,200),1)
+print(trainset)
+trainset.read_pickles('slow_australia_track_run3_images.pkl','slow_australia_track_run3_linear.pkl')
+trainLoader = torch.utils.data.DataLoader(trainset, batch_size = 8, shuffle = True, num_workers = 0)
+print(trainLoader)
+#Definition of our loss.
+criterion = nn.MSELoss()
 
-captain_net = models.CaptainNet()
-captain_net.double()
-captain_net.train()
-print(captain_net)
-input = torch.from_numpy(np.random.rand(10,3,66,200))
-results = captain_net(input)
-print(results)
+# Definition of optimization strategy.
+optimizer = optim.SGD(network.parameters(), lr = learningRate, momentum=0.01)
 
-dl = loaders.F1Dataset("D:\\test_data\\slow_australia_track_run3","run3_linear.csv")
-print(dl)
-print(dl.__getitem__(5))
+train_model(network, criterion, optimizer, trainLoader, "pilotnet_linear_interpolation", n_epochs = 200, use_gpu = True)
