d7692e508ed3b2690e25f2839fdc03715e9ddb4f
Trent Weiss <ttw2xk@virginia.edu>
2018-05-03 16:28:27 -0400

    the model is defined and ready to be trained

--- DCNN-Pytorch/nn_models/data_loading/image_loading.py
+++ DCNN-Pytorch/nn_models/data_loading/image_loading.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 def load_image(filepath, size=(0,0), scale_factor=1.0):
-    img = cv2.imread(filepath, cv2.IMREAD_UNCHANGED).astype(np.float32)
+    img = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
     if(size!=(0,0)):
         img_resized= cv2.resize(img,dsize=(size[1],size[0]), interpolation = cv2.INTER_CUBIC)
     else:
--- DCNN-Pytorch/nn_models/main.py
+++ DCNN-Pytorch/nn_models/main.py
@@ -4,7 +4,14 @@ import numpy as np
 import nn_models
 import data_loading.image_loading as il
 import nn_models.Models as models
-im = il.load_image("test_image.jpg")
+import numpy.random
+im = il.load_image("test_image.jpg",size=(66,200),scale_factor=255.0)
 print(im)
 net = models.PilotNet()
+net.double()
+net.eval()
 print(net)
+input = torch.from_numpy(np.random.rand(1,3,66,200))
+input[0]=torch.from_numpy(im)
+results = net(input)
+print(results)
