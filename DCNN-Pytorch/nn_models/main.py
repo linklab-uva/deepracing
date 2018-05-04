b8538ec8e292cb4a47d5bb4f08db32a3dd0cc54c
Trent Weiss <ttw2xk@virginia.edu>
2018-05-04 15:30:10 -0400

    clean up main file a bit

--- DCNN-Pytorch/nn_models/main.py
+++ DCNN-Pytorch/nn_models/main.py
@@ -30,9 +30,6 @@ def run_epoch(network, criterion, optimizer, trainLoader, use_gpu):
 
         # Backward pass:
         optimizer.zero_grad()
-        # Loss is a variable, and calling backward on a Variable will
-        # compute all the gradients that lead to that Variable taking on its
-        # current value.
         loss.backward() 
 
         # Weight and bias updates.
@@ -54,30 +51,34 @@ def train_model(network, criterion, optimizer, trainLoader, file_prefix, n_epoch
         run_epoch(network, criterion, optimizer, trainLoader, use_gpu)
         log_path = os.path.join("log",""+file_prefix+"_epoch"+str((epoch+1))+ ".model")
         torch.save(network.state_dict(), log_path)
-parser = argparse.ArgumentParser(description="LSTM URL Classification Training")
-parser.add_argument("--gpu", action="store_true", help="Accelerate with GPU")
-parser.add_argument("--batch_size", type=int, default = 8, help="Batch Size")
-parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs to run")
-parser.add_argument("--learning_rate", type=float, default=0.01, help="Number of training epochs to run")
-parser.add_argument("--momentum", type=float, default=0.0, help="Momentum value to use on the SGD optimizer")
-parser.add_argument("--root_dir", type=str, required=True, help="Root dir of the F1 dataset to use")
-parser.add_argument("--annotation_file", type=str, required=True, help="Annotation file to use")
-args = parser.parse_args()
-prefix, ext = args.annotation_file.split(".")
-network = models.PilotNet()
-network.float()
+def main():
+    parser = argparse.ArgumentParser(description="Steering prediction with PilotNet")
+    parser.add_argument("--gpu", action="store_true", help="Accelerate with GPU")
+    parser.add_argument("--batch_size", type=int, default = 8, help="Batch Size")
+    parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs to run")
+    parser.add_argument("--learning_rate", type=float, default=0.01, help="Number of training epochs to run")
+    parser.add_argument("--momentum", type=float, default=0.0, help="Momentum value to use on the SGD optimizer")
+    parser.add_argument("--root_dir", type=str, required=True, help="Root dir of the F1 dataset to use")
+    parser.add_argument("--annotation_file", type=str, required=True, help="Annotation file to use")
+    args = parser.parse_args()
+    prefix, ext = args.annotation_file.split(".")
+    network = models.PilotNet()
+    network.float()
 
-trainset = loaders.F1Dataset(args.root_dir,args.annotation_file,(3,66,200),1)
-if(os.path.isfile("./" + prefix+"_images.pkl") and os.path.isfile("./" + prefix+"_annotations.pkl")):
-    trainset.read_pickles(prefix+"_images.pkl",prefix+"_annotations.pkl")
-else:  
-    trainset.read_files()
-    trainset.write_pickles(prefix+"_images.pkl",prefix+"_annotations.pkl")
-trainLoader = torch.utils.data.DataLoader(trainset, batch_size = 8, shuffle = True, num_workers = 0)
-print(trainLoader)
-#Definition of our loss.
-criterion = nn.MSELoss()
+    trainset = loaders.F1Dataset(args.root_dir,args.annotation_file,(3,66,200),1)
+    if(os.path.isfile("./" + prefix+"_images.pkl") and os.path.isfile("./" + prefix+"_annotations.pkl")):
+        trainset.read_pickles(prefix+"_images.pkl",prefix+"_annotations.pkl")
+    else:  
+        trainset.read_files()
+        trainset.write_pickles(prefix+"_images.pkl",prefix+"_annotations.pkl")
+    trainLoader = torch.utils.data.DataLoader(trainset, batch_size = 8, shuffle = True, num_workers = 0)
+    print(trainLoader)
+    #Definition of our loss.
+    criterion = nn.MSELoss()
 
-# Definition of optimization strategy.
-optimizer = optim.SGD(network.parameters(), lr = args.learning_rate, momentum=args.momentum)
-train_model(network, criterion, optimizer, trainLoader, prefix, n_epochs = args.epochs, use_gpu = args.gpu)
+    # Definition of optimization strategy.
+    optimizer = optim.SGD(network.parameters(), lr = args.learning_rate, momentum=args.momentum)
+    train_model(network, criterion, optimizer, trainLoader, prefix, n_epochs = args.epochs, use_gpu = args.gpu)
+
+if __name__ == '__main__':
+    main()
