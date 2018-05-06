d5e897ffec86f34d26e9c432ccbf64c0fb1d229f
Trent Weiss <ttw2xk@virginia.edu>
2018-05-06 15:22:05 -0400

    it seems to train...

--- DCNN-Pytorch/nn_models/data_loading/data_loaders.py
+++ DCNN-Pytorch/nn_models/data_loading/data_loaders.py
@@ -82,52 +82,79 @@ class F1Dataset(Dataset):
         return img_tensor, label_tensor.view(1)
     def __len__(self):
         return self.length
-class F1AllControlDataset(Dataset):
-    def __init__(self, root_folder, annotation_filepath, im_size):
-        super(F1AllControlDataset, self).__init__()
+class F1SequenceDataset(Dataset):
+    def __init__(self, root_folder, annotation_filepath, im_size,\
+        sequence_length=25, use_float32=False, img_transformation = None, label_transformation = None):
+        super(F1SequenceDataset, self).__init__()
         self.im_size=im_size
-        self.label_size = 3
+        self.label_size = 1
+        self.use_float32=use_float32
         self.root_folder = root_folder
+        self.img_transformation = img_transformation
+        self.label_transformation = label_transformation
         self.annotation_filepath = annotation_filepath
         self.annotations_file = open(os.path.join(self.root_folder,self.annotation_filepath), "r")
         self.annotations = self.annotations_file.readlines()
-        self.length = len(self.annotations)
+        self.sequence_length=sequence_length
+        self.length = len(self.annotations) - sequence_length
         self.images = []
         self.labels = []
+        self.preloaded=False
     def write_pickles(self,image_pickle, label_pickle):
         filename = image_pickle
         fp = open(filename, 'wb')
-        pickle.dump(self.images.numpy(), fp)
+        pickle.dump(self.images, fp)
         fp.close()
         print('File %s is saved.' % filename)
 
         filename = label_pickle
         fp = open(filename, 'wb')
-        pickle.dump(self.labels.numpy(), fp)
+        pickle.dump(self.labels, fp)
         fp.close()
         print('File %s is saved.' % filename)
     def read_pickles(self,image_pickle, label_pickle):
         filename = image_pickle
         fp = open(filename, 'rb')
-        self.images = torch.from_numpy(pickle.load(fp))
+        self.images = pickle.load(fp)
         fp.close()
 
         filename = label_pickle
         fp = open(filename, 'rb')
-        self.labels =  torch.from_numpy(pickle.load(fp))
+        self.labels =  pickle.load(fp)
         fp.close()
-    def read_files(self, use_float32 = False):
+        self.preloaded=True
+    def read_files(self):
         print("loading data")
         for (idx,line) in tqdm(enumerate(self.annotations)):
             fp, ts, steering, throttle, brake = line.split(",")
-            im = load_image(os.path.join(self.root_folder,"raw_images",fp), use_float32 = use_float32)
+            im = load_image(os.path.join(self.root_folder,"raw_images",fp))
+            im = cv2.resize(im, (self.im_size[1], self.im_size[0]), interpolation = cv2.INTER_CUBIC)
+            im = np.transpose(im, (2, 0, 1))
             self.images.append(im)
-            self.labels.append(np.array((float(steering), float(throttle), float(brake))))
+            self.labels.append(np.array((float(steering))))
+        self.preloaded=True
     def __getitem__(self, index):
-        np_arr = self.images[index]
-        img = cv2.resize(image, (self.im_size[1], self.im_size[0]))
-        img_tensor = torch.from_numpy(img)
-        label_tensor = torch.from_numpy(self.labels[index])
-        return self.images[index], self.labels[index]
+        if(self.preloaded):
+            images = self.images[index:index+self.sequence_length]
+            labels = self.labels[index:index+self.sequence_length]
+            seq = np.array([np.array(xi) for xi in images])
+            seq_labels = np.array([np.array(xi) for xi in labels])
+        else:
+            raise NotImplementedError("Must preload images for sequence dataset")
+        if(self.use_float32):
+            seq = seq.astype(np.float32)
+            seq_labels = seq_labels.astype(np.float32)
+        else:
+            seq = seq.astype(np.float64)
+            seq_labels = seq_labels.astype(np.float64)
+        label_tensor = torch.from_numpy(seq_labels)
+        img_tensor = torch.from_numpy(seq)
+        if(not (self.img_transformation == None)):
+            for i in range(0, img_tensor.shape[0]):
+                img_tensor[i]=self.img_transformation(img_tensor[i])
+        if(not (self.label_transformation == None)):
+            for i in range(0, label_tensor.shape[0]):
+                label_tensor[i]=self.label_transformation(label_tensor[i])
+        return img_tensor, label_tensor.view(self.sequence_length,1)
     def __len__(self):
         return self.length
--- DCNN-Pytorch/nn_models/nn_models/Models.py
+++ DCNN-Pytorch/nn_models/nn_models/Models.py
@@ -51,33 +51,52 @@ class PilotNet(nn.Module):
 
 class AdmiralNet(nn.Module):
     """PyTorch Implementation of NVIDIA's PilotNet"""
-    def __init__(self):
-        super(CaptainNet, self).__init__()
+    def __init__(self, seq_length=25):
+        super(AdmiralNet, self).__init__()
         # Convolutional layers.
-        self.output_size = 3
+        self.output_size = 1
         self.conv1 = nn.Conv2d(3, 24, kernel_size=5, stride=2)
         self.conv2 = nn.Conv2d(24, 36, kernel_size=5, stride=2)
         self.conv3 = nn.Conv2d(36, 48, kernel_size=5, stride=2)
         self.conv4 = nn.Conv2d(48, 64, kernel_size=3)
         self.conv5 = nn.Conv2d(64, 64, kernel_size=3)
+        #batch norm layers
+        self.Norm_1 = nn.BatchNorm2d(24)
+        self.Norm_2 = nn.BatchNorm2d(36)
+        self.Norm_3 = nn.BatchNorm2d(48) 
+        self.Norm_4 = nn.BatchNorm2d(64)
         
+        #recurrent layers
+        self.seq_length=seq_length
+        self.lstm = nn.LSTM(64*1*18, 100, batch_first = True)
+
         # Linear layers.
-        self.fc1 = nn.Linear(64*1*18, 100)
-        self.fc2 = nn.Linear(100, 50)
-        self.fc3 = nn.Linear(50, 10)
-        self.prediction_layer = nn.Linear(10, self.output_size)
+        self.prediction_layer = nn.Linear(100, self.output_size)
+
+        #activations
+        self.relu = nn.ReLU()
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.conv2(out)
-        out = self.conv3(out)
-        out = self.conv4(out)
-        out = self.conv5(out)
-        # This flattens the output of the previous layer into a vector.
-        out = out.view(out.size(0), -1) 
-        out = self.fc1(out)
-        out = self.fc2(out)
-        out = self.fc3(out)
-        predictions = self.prediction_layer(out)
+        #resize for convolutional layers
+        x = x.view(-1, 3, 66, 200) 
+        x = self.conv1(x)
+        x = self.Norm_1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.Norm_2(x)
+        x = self.relu(x)
+        x = self.conv3(x)
+        x = self.Norm_3(x)
+        x = self.relu(x)
+        x = self.conv4(x)
+        x = self.Norm_4(x)
+        x = self.relu(x)
+        x = self.conv5(x)
+        x = self.relu(x)
+        #print(x.shape)
+        # Unpack for the LSTM.
+        x = x.view(-1, self.seq_length, 64*1*18) 
+        x, finalHiddenState = self.lstm(x)
+        predictions = self.prediction_layer(x)
         return predictions
 
