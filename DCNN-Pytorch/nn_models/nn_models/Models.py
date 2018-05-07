96f6193d1f9e5b55262c58d5e4c6223bbf05ecb0
Trent Weiss <ttw2xk@virginia.edu>
2018-05-07 17:40:54 -0400

    add resnet adapter stuff

--- DCNN-Pytorch/nn_models/nn_models/Models.py
+++ DCNN-Pytorch/nn_models/nn_models/Models.py
@@ -11,9 +11,9 @@ class ResNetAdapter(nn.Module):
         super(ResNetAdapter, self).__init__()
         resnet_model = visionmodels.resnet152(pretrained=True)
         self.features = nn.Sequential(*list(resnet_model.children())[:-2])
-        self.classifier = nn.Sequential(*[nn.Linear(43008, 2048),nn.ReLU(True),nn.Dropout(),\
-                        nn.Linear(2048, 1024),nn.ReLU(True),nn.Dropout(),\
-                        nn.Linear(1024, 128),nn.ReLU(True),nn.Dropout(),\
+        self.classifier = nn.Sequential(*[nn.Linear(43008, 2048),\
+                        nn.Linear(2048, 1024),\
+                        nn.Linear(1024, 128),\
                         nn.Linear(128, 1)])
     def forward(self, x):
         x = self.features(x)
