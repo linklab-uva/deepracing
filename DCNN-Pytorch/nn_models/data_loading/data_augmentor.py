400ef2f24069d99b3656aad50a5881232e21dde7
Dipshil Agrawal <dipshil@gmail.com>
2018-05-28 13:00:59 -0400

    Update data_augmentor.py

--- DCNN-Pytorch/nn_models/data_loading/data_augmentor.py
+++ DCNN-Pytorch/nn_models/data_loading/data_augmentor.py
@@ -14,7 +14,13 @@ def main():
             img = load_image(args.input_file).astype(np.float32)
             horizontal_flip = img[:, ::-1]
             blured_image = cv2.GaussianBlur(img, (5,5),0)
+            pre,post = filename.split('.')
+            pre=pre+'_1'
+            filename=pre+post
             cv2.imwrite(os.path.join(directory,filename),horizontal_flip)
+            pre,post = filename.split('.')
+            pre=pre+'_2'
+            filename=pre+post
             cv2.imwrite(os.path.join(directory,filename),blured_image)
             
 if __name__ == '__main__':
