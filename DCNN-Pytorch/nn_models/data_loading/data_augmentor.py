96f88080952afb589c106cf243b483a229baab67
dipshil <dipshil@gmail.com>
2018-05-29 09:56:02 -0400

    bug fixes

--- DCNN-Pytorch/nn_models/data_loading/data_augmentor.py
+++ DCNN-Pytorch/nn_models/data_loading/data_augmentor.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import argparse
 import os
+from tqdm import tqdm as tqdm
 
 def load_image(filepath):
     img = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
@@ -12,19 +13,29 @@ def main():
     parser.add_argument("--path", type=str, required=True)
     args = parser.parse_args()
     directory = args.path
-    for filename in os.listdir(directory):
+    for filename in tqdm(os.listdir(directory),desc='Augmenting Data'):
         if filename.endswith(".jpg"):
             img = load_image(filename).astype(np.float32)
+            bright = img + 30
+            dark = img - 30
             horizontal_flip = img[:, ::-1]
             blured_image = cv2.GaussianBlur(img, (5,5),0)
             pre,post = filename.split('.')
             pre=pre+'_1'
-            filename=pre+'.'+post
-            cv2.imwrite(os.path.join(directory,filename),horizontal_flip)
+            fname=pre+'.'+post
+            cv2.imwrite(os.path.join(directory,fname),horizontal_flip)
             pre,post = filename.split('.')
             pre=pre+'_2'
-            filename=pre+'.'+post
-            cv2.imwrite(os.path.join(directory,filename),blured_image)
+            fname=pre+'.'+post
+            cv2.imwrite(os.path.join(directory,fname),blured_image)
+            pre,post = filename.split('.')
+            pre=pre+'_3'
+            fname=pre+'.'+post
+            cv2.imwrite(os.path.join(directory,fname),bright)
+            pre,post = filename.split('.')
+            pre=pre+'_4'
+            fname=pre+'.'+post
+            cv2.imwrite(os.path.join(directory,fname),dark)
             
 if __name__ == '__main__':
     main()
