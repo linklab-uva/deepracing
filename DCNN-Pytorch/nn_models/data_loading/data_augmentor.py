dfec7e0a0d9fc579b8793612ecb159dadd38d332
dipshil <dipshil@gmail.com>
2018-05-29 09:47:44 -0400

    bug fixes

--- DCNN-Pytorch/nn_models/data_loading/data_augmentor.py
+++ DCNN-Pytorch/nn_models/data_loading/data_augmentor.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+import argparse
+import os
 
 def load_image(filepath):
     img = cv2.imread(filepath, cv2.IMREAD_UNCHANGED)
@@ -8,19 +10,20 @@ def load_image(filepath):
 def main():
     parser = argparse.ArgumentParser(description="Data Augmentor")
     parser.add_argument("--path", type=str, required=True)
+    args = parser.parse_args()
     directory = args.path
     for filename in os.listdir(directory):
         if filename.endswith(".jpg"):
-            img = load_image(args.input_file).astype(np.float32)
+            img = load_image(filename).astype(np.float32)
             horizontal_flip = img[:, ::-1]
             blured_image = cv2.GaussianBlur(img, (5,5),0)
             pre,post = filename.split('.')
             pre=pre+'_1'
-            filename=pre+post
+            filename=pre+'.'+post
             cv2.imwrite(os.path.join(directory,filename),horizontal_flip)
             pre,post = filename.split('.')
             pre=pre+'_2'
-            filename=pre+post
+            filename=pre+'.'+post
             cv2.imwrite(os.path.join(directory,filename),blured_image)
             
 if __name__ == '__main__':
