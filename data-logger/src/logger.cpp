b9915d24013149decb8ce8a290edac4021451aa1
dipshil <dipshil@gmail.com>
2018-06-26 12:56:33 -0400

    data logger upgraded

--- data-logger/src/logger.cpp
+++ data-logger/src/logger.cpp
@@ -25,6 +25,7 @@ namespace deepf1{
 	
 	void writeToFiles(const std::string& dir,
 		std::vector<deepf1::timestamped_image_data_t>& screen_data,
+		std::vector<deepf1::timestamped_image_data_t>& screen_data2,
 		deepf1::simple_udp_listener& udp_listener);
 	bool udp_data_comparator(const deepf1::timestamped_udp_data& a, const deepf1::timestamped_udp_data& b);
 	deepf1::timestamped_udp_data find_closest_value(std::vector<deepf1::timestamped_udp_data>& udp_dataz,
@@ -41,6 +42,7 @@ int main(int argc, char** argv) {
 
 	unsigned int udp_len, image_len;
 	float capture_x, capture_y, capture_width, capture_height;
+	float capture_x_pov, capture_y_pov, capture_width_pov, capture_height_pov;
 	int monitor_number;
 	long sleep_time;
 	unsigned short port_number;
@@ -51,10 +53,14 @@ int main(int argc, char** argv) {
 		("udp_frames,u", po::value<unsigned int>(&udp_len)->default_value(100), "How many frames of game data to capture")
 		("port_number,p", po::value<unsigned short>(&port_number)->default_value(PORT), "Port number to listen for telemetry data on")
 		("screen_frames,s", po::value<unsigned int>(&image_len)->default_value(100), "How many frames of screencap data to capture")
-		("capture_x,x", po::value<float>(&capture_x)->default_value(0), "x coordinate for origin of capture area in pixels")
-		("capture_y,y", po::value<float>(&capture_y)->default_value(325), "y coordinate for origin of capture area pixels")
-		("capture_width,w", po::value<float>(&capture_width)->default_value(1825), "Width of capture area pixels")
-		("capture_height,h", po::value<float>(&capture_height)->default_value(500), "height of capture area pixels")
+		("capture_x,x", po::value<float>(&capture_x)->default_value(50), "x coordinate for origin of capture area in pixels")
+		("capture_y,y", po::value<float>(&capture_y)->default_value(0), "y coordinate for origin of capture area pixels")
+		("capture_width,w", po::value<float>(&capture_width)->default_value(2510), "Width of capture area pixels")
+		("capture_height,h", po::value<float>(&capture_height)->default_value(1000), "height of capture area pixels")
+		("capture_x_pov,x_pov", po::value<float>(&capture_x_pov)->default_value(0), "x coordinate for origin of capture pov_area in pixels")
+		("capture_y_pov,y_pov", po::value<float>(&capture_y_pov)->default_value(430), "y coordinate for origin of capture pov_area pixels")
+		("capture_width_pov,w_pov", po::value<float>(&capture_width_pov)->default_value(2510), "Width of capture pov_area pixels")
+		("capture_height_pov,h_pov", po::value<float>(&capture_height_pov)->default_value(200), "height of capture pov_area pixels")
 		("data_directory,d", po::value<std::string>(&data_directory)->default_value(std::string("data")), "Top-level directory to place the annotations & images.")
 		("initial_sleep_time,i", po::value<long>(&sleep_time)->default_value(5000), "How many milliseconds to sleep before starting data recording.")
 		("application_name,a", po::value<std::string>(&application_name)->default_value(std::string("")), "Name of the application to capture. If not set, defaults to the desktop window.")
@@ -71,6 +77,7 @@ int main(int argc, char** argv) {
 	}
 
 	cv::Rect2d capture_area = cv::Rect2d(capture_x, capture_y, capture_width, capture_height);
+	cv::Rect2d capture_area_pov = cv::Rect2d(capture_x_pov, capture_y_pov, capture_width_pov, capture_height_pov);
 	std::printf("Starting data capture in %lld milliseconds\n", sleep_time);
 	Sleep(sleep_time);
 
@@ -78,22 +85,27 @@ int main(int argc, char** argv) {
 	std::shared_ptr<const boost::timer::cpu_timer> timer(new boost::timer::cpu_timer);
 	deepf1::simple_udp_listener udp_listener(timer, udp_len, port_number);
 	deepf1::simple_screen_listener screen_listener(timer, capture_area, application_name, image_len);
+	deepf1::simple_screen_listener screen_listener2(timer, capture_area_pov, application_name, image_len);
 
 
 	std::function<void()> udp_worker = std::bind(&deepf1::simple_udp_listener::listen, &udp_listener);
 	Sleep(1500);
 	std::function<void()> screen_worker = std::bind(&deepf1::simple_screen_listener::listen, &screen_listener);
+	std::function<void()> screen_worker2 = std::bind(&deepf1::simple_screen_listener::listen, &screen_listener2);
 
 	std::thread udp_thread(udp_worker);
 	std::thread screen_thread(screen_worker);
+	std::thread screen_thread2(screen_worker2);
 	screen_thread.join();
+	screen_thread2.join();
 	std::cout << "Screencapping done" << std::endl;
 	udp_listener.stop();
 	udp_thread.join();
 	std::vector<deepf1::timestamped_image_data_t> screen_data = screen_listener.get_data();
+	std::vector<deepf1::timestamped_image_data_t> screen_data2 = screen_listener2.get_data();
 	std::vector<deepf1::timestamped_udp_data_t> udp_data = udp_listener.get_data();
 	std::printf("Started receiving packets at timestamp: %lld \n", udp_listener.get_collection_start().wall);
-	writeToFiles(data_directory, screen_data, udp_listener);
+	writeToFiles(data_directory, screen_data,screen_data2, udp_listener);
 
 	return 0;
 }
@@ -133,33 +145,44 @@ namespace deepf1 {
 	}
 	void writeToFiles(const std::string& dir,
 		std::vector<deepf1::timestamped_image_data_t>& screen_data,
+		std::vector<deepf1::timestamped_image_data_t>& screen_data2,
 		deepf1::simple_udp_listener& udp_listener) {
 		std::vector<deepf1::timestamped_udp_data> udp_data = udp_listener.get_data();
 		fs::path root_dir = fs::path(dir);
 		fs::create_directory(root_dir);
 		fs::path raw_annotations_dir = root_dir / fs::path("raw_annotations");
 		fs::create_directory(raw_annotations_dir);
-		fs::path raw_images_dir = root_dir / fs::path("raw_images");
+		fs::path raw_images_dir = root_dir / fs::path("raw_images_full");
+		fs::path raw_images_pov_dir = root_dir / fs::path("raw_images");
 		fs::create_directory(raw_images_dir);
+		fs::create_directory(raw_images_pov_dir);
 
-		fs::path raw_images_timestamps = raw_images_dir / fs::path("raw_image_timestamps.csv");
+		fs::path raw_images_timestamps = raw_images_pov_dir / fs::path("raw_image_timestamps.csv");
 		soap* soap = soap_new(SOAP_XML_INDENT);
 		deepf1_gsoap_conversions::gsoap_conversions convert(soap);
 		unsigned long point_number = 1;
 		unsigned long raw_point_number = 1;
 		std::shared_ptr<std::fstream> file_out;
 		std::shared_ptr<std::ofstream> raw_image_timestamps_stream(new std::ofstream(raw_images_timestamps.string(), std::fstream::out));
-		std::cout << "Writing " << screen_data.size() << " raw images to file." << std::endl;
+		std::cout << "Writing " << screen_data.size() << " full raw images to file." << std::endl;
 		for (auto it = screen_data.begin(); it != screen_data.end(); it++) {
 			
 			std::stringstream raw_image_ss;
-			raw_image_ss << "raw_image_" << raw_point_number << ".jpg";
+			raw_image_ss << "raw_image_full" << raw_point_number << ".jpg";
 			fs::path raw_image_path = raw_images_dir / fs::path(raw_image_ss.str());
 			cv::imwrite(raw_image_path.string(), (it->image));
-			(*raw_image_timestamps_stream) << raw_image_ss.str() << ", " << (it->timestamp.wall) << std::endl;
 			raw_point_number++;
-
-
+	
+		}
+		raw_point_number = 1;
+		std::cout << "Writing " << screen_data2.size() << " raw images to file." << std::endl;
+		for (auto it2 = screen_data2.begin(); it2 != screen_data2.end(); it2++) {
+			std::stringstream raw_image_ss_pov;
+			raw_image_ss_pov << "raw_image_" << raw_point_number << ".jpg";
+			fs::path raw_image_pov_path = raw_images_pov_dir / fs::path(raw_image_ss_pov.str());
+			cv::imwrite(raw_image_pov_path.string(), (it2->image));
+			(*raw_image_timestamps_stream) << raw_image_ss_pov.str() << ", " << (it2->timestamp.wall) << std::endl;
+			raw_point_number++;
 		}
 		raw_point_number = 1;
 		std::cout << "Writing " << udp_data.size() << " raw annotations to file." << std::endl;
