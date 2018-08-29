102a15dd2fbd3788ae0aff6223dfa4977c81e537
dipshil <dipshil@gmail.com>
2018-08-29 16:10:12 -0400

    add python project to main solution

--- data-logger/python_binding/pyf1_test.py
+++ data-logger/python_binding/pyf1_test.py
@@ -1,11 +1,10 @@
 import pyf1_datalogger, numpy as np, cv2
 dl = pyf1_datalogger.ScreenVideoCapture()
-
-dl.open("F1 2017",100,100,500,500)
-
-im = dl.read()
-print(np.mean(im))
-cv2.namedWindow("F1 2017",0)
-cv2.imshow("F1 2017",im)
-cv2.waitKey(0)
+app="F1 2017"
+dl.open(app,0,200,1700,300)
+cv2.namedWindow(app,cv2.WINDOW_AUTOSIZE)
+while True:
+    im = dl.read()
+    cv2.imshow(app,im)
+    cv2.waitKey(5)
 
