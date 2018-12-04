574ec796a297dafe4b63ad69f0271f244981ff71
Trent Weiss <ttw2xk@virginia.edu>
2018-12-04 16:08:28 -0500

    use std chrono for duration counting

--- data-logger/include/car_data/timestamped_car_data.h
+++ data-logger/include/car_data/timestamped_car_data.h
@@ -1,4 +1,7 @@
-#pragma once
+#ifndef TIMESTAMPED_CAR_DATA_H
+#define  TIMESTAMPED_CAR_DATA_H
+
+
 #include "car_data/car_data.h"
 #include <chrono>
 namespace deepf1
@@ -9,3 +12,5 @@ namespace deepf1
 	}; 
 	typedef struct timestamped_udp_data timestamped_udp_data_t;
 }
+
+#endif
--- data-logger/include/car_data/timestamped_image_data.h
+++ data-logger/include/car_data/timestamped_image_data.h
@@ -1,10 +1,16 @@
-#pragma once
-#include <boost/timer/timer.hpp>
+#ifndef TIMESTAMPED_IMAGE_DATA_H
+#define  TIMESTAMPED_IMAGE_DATA_H
+
+
+#include <chrono>
 #include <opencv2/core.hpp>
 namespace deepf1
 {
 	struct timestamped_image_data {
 		cv::Mat image;
-		boost::timer::cpu_times timestamp;
+		std::chrono::microseconds timestamp;
 	};typedef struct timestamped_image_data timestamped_image_data_t;
 }
+
+
+#endif
