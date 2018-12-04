3ce090abe66e9493bd8396d7602310dca324d99b
Trent Weiss <ttw2xk@virginia.edu>
2018-12-04 16:29:22 -0500

    add folder for image logging library

---
+++ data-logger/src/image_logging/F1FrameGrabManager.cpp
@@ -0,0 +1,21 @@
+/*
+ * F1FrameGrabManager.cpp
+ *
+ *  Created on: Dec 4, 2018
+ *      Author: ttw2xk
+ */
+
+#include "image_logging/F1FrameGrabManager.h"
+
+namespace deepf1 {
+
+F1FrameGrabManager::F1FrameGrabManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+F1FrameGrabManager::~F1FrameGrabManager() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace deepf1 */
