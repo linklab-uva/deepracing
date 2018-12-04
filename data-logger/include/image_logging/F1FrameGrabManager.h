0a86b494dfc539ee00de6da16305ff2136c02a43
Trent Weiss <ttw2xk@virginia.edu>
2018-12-04 16:29:22 -0500

    add folder for image logging library

---
+++ data-logger/include/image_logging/F1FrameGrabManager.h
@@ -0,0 +1,21 @@
+/*
+ * F1FrameGrabManager.h
+ *
+ *  Created on: Dec 4, 2018
+ *      Author: ttw2xk
+ */
+
+#ifndef INCLUDE_IMAGE_LOGGING_F1FRAMEGRABMANAGER_H_
+#define INCLUDE_IMAGE_LOGGING_F1FRAMEGRABMANAGER_H_
+
+namespace deepf1 {
+
+class F1FrameGrabManager {
+public:
+	F1FrameGrabManager();
+	virtual ~F1FrameGrabManager();
+};
+
+} /* namespace deepf1 */
+
+#endif /* INCLUDE_IMAGE_LOGGING_F1FRAMEGRABMANAGER_H_ */
