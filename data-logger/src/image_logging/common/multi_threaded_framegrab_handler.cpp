020872b694ab3945e10c21818a4c3928c429f3d8
Trent Weiss <ttw2xk@virginia.edu>
2018-12-06 13:02:39 -0500

    sure

--- data-logger/src/image_logging/common/multi_threaded_framegrab_handler.cpp
+++ data-logger/src/image_logging/common/multi_threaded_framegrab_handler.cpp
@@ -8,10 +8,12 @@
 #include "image_logging/common/multi_threaded_framegrab_handler.h"
 #include "opencv2/imgcodecs.hpp"
 #include <iostream>
+#include "TimestampedImage.pb.h"
+#include <fstream>
 namespace deepf1
 {
 
-MultiThreadedFrameGrabHandler::MultiThreadedFrameGrabHandler(unsigned int thread_count) : running_(false)
+MultiThreadedFrameGrabHandler::MultiThreadedFrameGrabHandler(unsigned int thread_count) : running_(false), counter_(1)
 {
   thread_count_= thread_count;
 }
@@ -28,12 +30,7 @@ bool MultiThreadedFrameGrabHandler::isReady()
 
 void MultiThreadedFrameGrabHandler::handleData(const TimestampedImageData& data)
 {
-///  std::cout<<"Handling data"<<std::endl;
-//  TimestampedImageData data_copy;
-//  data_copy.timestamp = data.timestamp;
-//  data.image.copyTo(data_copy.image);
   queue_->push(data);
-
 }
 void MultiThreadedFrameGrabHandler::init(const std::chrono::high_resolution_clock::time_point& begin,
                                          const cv::Size& window_size)
@@ -50,7 +47,7 @@ void MultiThreadedFrameGrabHandler::init(const std::chrono::high_resolution_cloc
 
 void MultiThreadedFrameGrabHandler::workerFunc_()
 {
-  std::cout<<"Spawned a thread" <<std::endl;
+  std::cout<<"Spawned a worker thread to log images" <<std::endl;
   while(running_)
   {
     if(queue_->empty())
@@ -62,10 +59,17 @@ void MultiThreadedFrameGrabHandler::workerFunc_()
     {
       continue;
     }
-
-    long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(data.timestamp - begin_).count();
-    std::string fn = "image_" + std::to_string(milliseconds) + ".jpg";
+    unsigned counter = counter_.fetch_and_increment();
+    google::protobuf::uint64 delta = (google::protobuf::uint64)(std::chrono::duration_cast<std::chrono::microseconds>(data.timestamp - begin_).count());
+    std::string fn = "image_" + std::to_string(counter) + ".jpg";
     cv::imwrite(fn,data.image);
+    deepf1::protobuf::TimestampedImage tag;
+    tag.set_image_file(fn);
+    tag.set_timestamp(delta);
+    std::string pb_fn = "image_" + std::to_string(counter) + ".pb";
+    std::ofstream ostream(pb_fn.c_str());
+    tag.SerializeToOstream(&ostream);
+    ostream.close();
   }
 }
 
