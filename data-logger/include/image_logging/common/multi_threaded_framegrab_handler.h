72296c65a7c9ded4ff8b7d906463adbc73255409
Trent Weiss <ttw2xk@virginia.edu>
2018-12-06 10:33:29 -0500

    works with 1 thread on debugger

---
+++ data-logger/include/image_logging/common/multi_threaded_framegrab_handler.h
@@ -0,0 +1,35 @@
+/*
+ * multi_threaded_framegrab_handler.h
+ *
+ *  Created on: Dec 6, 2018
+ *      Author: ttw2xk
+ */
+
+#ifndef INCLUDE_IMAGE_LOGGING_COMMON_MULTI_THREADED_FRAMEGRAB_HANDLER_H_
+#define INCLUDE_IMAGE_LOGGING_COMMON_MULTI_THREADED_FRAMEGRAB_HANDLER_H_
+#include "image_logging/framegrab_handler.h"
+#include <tbb/concurrent_queue.h>
+ #include "tbb/task_group.h"
+namespace deepf1
+{
+
+class MultiThreadedFrameGrabHandler : public IF1FrameGrabHandler
+{
+public:
+  MultiThreadedFrameGrabHandler();
+  virtual ~MultiThreadedFrameGrabHandler();
+  bool isReady() override;
+  void handleData(const TimestampedImageData& data) override;
+  void init(const std::chrono::high_resolution_clock::time_point& begin, const cv::Size& window_size) override;
+private:
+  std::shared_ptr< tbb::concurrent_queue<TimestampedImageData> >queue_;
+  std::shared_ptr< tbb::task_group> thread_pool_ ;
+  bool running_;
+  std::chrono::high_resolution_clock::time_point begin_;
+
+  void workerFunc_();
+};
+
+} /* namespace deepf1 */
+
+#endif /* INCLUDE_IMAGE_LOGGING_COMMON_MULTI_THREADED_FRAMEGRAB_HANDLER_H_ */
---
+++ data-logger/src/image_logging/common/multi_threaded_framegrab_handler.cpp
@@ -0,0 +1,74 @@
+/*
+ * multi_threaded_framegrab_handler.cpp
+ *
+ *  Created on: Dec 6, 2018
+ *      Author: ttw2xk
+ */
+
+#include "image_logging/common/multi_threaded_framegrab_handler.h"
+#include "opencv2/imgcodecs.hpp"
+#include <iostream>
+namespace deepf1
+{
+
+MultiThreadedFrameGrabHandler::MultiThreadedFrameGrabHandler() : running_(false)
+{
+
+}
+
+MultiThreadedFrameGrabHandler::~MultiThreadedFrameGrabHandler()
+{
+  running_ = false;
+}
+
+bool MultiThreadedFrameGrabHandler::isReady()
+{
+  return true;
+}
+
+void MultiThreadedFrameGrabHandler::handleData(const TimestampedImageData& data)
+{
+///  std::cout<<"Handling data"<<std::endl;
+  TimestampedImageData data_copy;
+  data_copy.timestamp = data.timestamp;
+  data.image.copyTo(data_copy.image);
+  queue_->push(data_copy);
+
+}
+void MultiThreadedFrameGrabHandler::init(const std::chrono::high_resolution_clock::time_point& begin,
+                                         const cv::Size& window_size)
+{
+  begin_ = begin;
+  running_ = true;
+  queue_.reset(new tbb::concurrent_queue<TimestampedImageData>);
+  thread_pool_.reset(new tbb::task_group);
+  for(int i = 0; i < 1; i ++)
+  {
+    thread_pool_->run(std::bind(&MultiThreadedFrameGrabHandler::workerFunc_,this));
+  }
+}
+
+void MultiThreadedFrameGrabHandler::workerFunc_()
+{
+  std::cout<<"Spawned a thread" <<std::endl;
+  while(running_)
+  {
+    if(queue_->empty())
+    {
+      std::cout<<"Queue is empty"<<std::endl;
+      continue;
+    }
+    TimestampedImageData data;
+    if(!queue_->try_pop(data))
+    {
+      std::cout<<"Could not pop"<<std::endl;
+      continue;
+    }
+    std::cout<<"Pop successful."<<std::endl;
+    long long milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(data.timestamp - begin_).count();
+    std::string fn = "image_" + std::to_string(milliseconds) + ".jpg";
+    cv::imwrite(fn,data.image);
+  }
+}
+
+} /* namespace deepf1 */
