52bc36b82b8aa881e58c780fdc6fa51a61d7b221
Dipshil Agrawal <dipshil@gmail.com>
2019-01-16 16:43:02 -0500

    use status messages

--- data-logger/cmake/get_tbb.cmake
+++ data-logger/cmake/get_tbb.cmake
@@ -1,5 +1,5 @@
 include(${THIRD_PARTY_REPOS}/tbb/cmake/TBBGet.cmake)
 tbb_get(TBB_ROOT tbb_root CONFIG_DIR TBB_DIR)
-find_package(TBB REQUIRED)
-message("Unpacked TBB to : ${tbb_root}")
-message("TBB Configuration at : ${TBB_DIR}")
+find_package(TBB REQUIRED tbb)
+message(STATUS "Unpacked TBB to : ${tbb_root}")
+message(STATUS "TBB Configuration at : ${TBB_DIR}")
