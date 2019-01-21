7c0e1473e83333fd300af33143af8de3765c913b
Trent Weiss <ttw2xk@virginia.edu>
2019-01-20 21:01:06 -0500

    update test script

--- DCNN-Pytorch/nn_models/test_admiralnet.py
+++ DCNN-Pytorch/nn_models/test_admiralnet.py
@@ -80,7 +80,7 @@ def main():
     if(gpu>=0):
         criterion = criterion.cuda(gpu)
     network.eval()
-    loader = torch.utils.data.DataLoader(trainset, batch_size = 1, shuffle = False, num_workers = 0)
+    loader = torch.utils.data.DataLoader(valset, batch_size = 1, shuffle = False, num_workers = 0)
     
     t = tqdm(enumerate(loader))
     for idx,(inputs,) in t:
