7f5c675e0f218722993dea1acecdb6aa61f7f0d9
Trent Weiss <ttw2xk@virginia.edu>
2019-02-12 11:21:29 -0500

    add image backends

--- DCNN-Pytorch/nn_models/data_loading/data_loaders.py
+++ DCNN-Pytorch/nn_models/data_loading/data_loaders.py
@@ -8,78 +8,47 @@ import pickle
 import cv2
 import numpy as np
 import torchvision.transforms as transforms
+import torchvision
 import PIL
 from PIL import Image as PILImage
-import torchvision
+import data_loading.backend.ImageBackend as image_backends
+def imagesToFlow(images):
+    images_np = images.numpy().transpose((0,2,3,1))
+    flows = torch.FloatTensor(images_np.shape[0]-1, 2, images_np.shape[1], images_np.shape[2])
+    grayscale = torchvision.transforms.Grayscale()
+    totensor = torchvision.transforms.ToTensor()
+    for i in range(0,flows.shape[0]):
+        first = cv2.cvtColor(images_np[i], cv2.COLOR_RGB2GRAY)
+        first = (255*first).astype(np.uint8)
+        print(first.shape)
+        second = cv2.cvtColor(images_np[i+1], cv2.COLOR_RGB2GRAY)
+        second = (255*second).astype(np.uint8)
+        print(second.shape)
+        flow = cv2.calcOpticalFlowFarneback(first,second, None, 0.5, 3, 20, 8, 5, 1.2, 0).astype(np.float32)
+        flows[i] = totensor(flow)
+    return flows
 class F1ImageSequenceDataset(Dataset):
-    def __init__(self, annotation_filepath, im_size, context_length = 25, sequence_length=25):
+    def __init__(self, backend, context_length = 10, sequence_length=1):
         super(F1ImageSequenceDataset, self).__init__()
+        if not isinstance(backend, image_backends.DeepF1ImageBackend):
+            raise NotImplementedError("backend must be a subtype of " + image_backends.DeepF1ImageBackend.__text_signature__)
+        self.backend=backend
         self.context_length=context_length
         self.sequence_length=sequence_length
-        self.totensor = torchvision.transforms.ToTensor()
-        self.grayscale = torchvision.transforms.Grayscale()
-        self.resize = torchvision.transforms.Resize(im_size)
-        self.annotation_filename = os.path.basename(annotation_filepath)
-        self.annotations = open(annotation_filepath).readlines()
-        self.root_folder = os.path.dirname(annotation_filepath)
-        self.image_folder = os.path.join(self.root_folder,'raw_images')
-        self.len = len(self.annotations) - context_length - sequence_length
-        self.images = None
-        self.labels = None
-        self.image_pickle_postfix = "_sequence_images.pt"
-        self.label_pickle_postfix = "_sequence_labels.pt"
-    def loadPickles(self):
-        
-        splits = self.annotation_filename.split(".")
-        prefix=splits[0]
-
-        lblname = prefix + self.label_pickle_postfix
-        imgname = prefix + self.image_pickle_postfix
-
-
-        self.images = torch.load(os.path.join(self.root_folder,imgname))
-
-
-        self.labels = torch.load(os.path.join(self.root_folder,lblname))
-
-    def writePickles(self):
-        splits = self.annotation_filename.split(".")
-        prefix=splits[0]
-        lblname = prefix + self.label_pickle_postfix
-        imgname = prefix + self.image_pickle_postfix
-        torch.save(self.labels,open(os.path.join(self.root_folder,lblname), 'w+b'))
-        torch.save(self.images,open(os.path.join(self.root_folder,imgname), 'w+b'))
-    def loadFiles(self):
-        self.images = torch.zeros(len(self.annotations), 1, self.resize.size[0], self.resize.size[1], dtype = torch.float32)
-        self.labels = torch.zeros(len(self.annotations), 3, dtype = torch.float32)
-        for idx in tqdm(range(len(self.annotations)),desc='Loading Data',leave=True):
-            fp, ts, steering, throttle, brake = self.annotations[idx].split(",")
-            impil = PILImage.open( os.path.join( self.image_folder, fp ) )
-            im = self.totensor( self.grayscale( self.resize( impil ) ) )
-            self.images[idx] = im
-
-            self.labels[idx][0] = float(steering)
-            self.labels[idx][1] = float(throttle)
-            self.labels[idx][2] = float(brake)
-
+        self.len = self.backend.image_tensor.shape[0] - context_length - sequence_length - 1
+    
     def __getitem__(self, index):
-        if(self.images is not None and self.labels is not None):
-            images_start = index
-            images_end = images_start + self.context_length - 1
-            images = self.images[ images_start : images_end + 1]
-                    
-            labels_start = images_end
-            labels_end = labels_start + self.sequence_length
-            labels = self.labels[labels_start : labels_end]
-        else:
-            raise( NotImplementedError("Only preloading images is supported for OpticalFlow dataset") )
+        images_start = index
+        images_end = images_start + self.context_length
+        images = self.backend.getImageRange(images_start, images_end)
+                
+        labels_start = images_end
+        labels_end = labels_start + self.sequence_length
+        labels = self.backend.getLabelRange(labels_start, labels_end)
         return images , labels
     def __len__(self):
         return self.len
-    def cuda(self, gpu=0):
-        if(self.images is not None and self.labels is not None):
-            self.images = self.images.cuda(gpu)
-            self.labels = self.labels.cuda(gpu)
+
 
 
 class F1CombinedDataset(Dataset):
--- DCNN-Pytorch/nn_models/playground.py
+++ DCNN-Pytorch/nn_models/playground.py
@@ -18,39 +18,21 @@ import argparse
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 from torch.utils.data.dataset import Subset
+import data_loading.backend.ImageBackend as image_backends
 def main():
-   # parser = argparse.ArgumentParser(description="Playground")
-   # parser.add_argument("--dataset_file", type=str, required=True, help="Dataset file to use")
-  #  args = parser.parse_args()
-    #dataset1 = loaders.F1ImageDataset("/home/ttw2xk/f1data/test_dataset/linear_1.csv",(66,200))
-    context_length = 10
-    sequence_length = 1
-    gpu = 0
-    input_channels = 3
-    size = (66,200)
-    output_dimension = 1
-    hidden_dimension = 100
+  image_tensor = torch.load('/home/ttw2xk/deepf1data/australia_fullview_run2/linear_image_tensor.pt')
+  label_tensor = torch.load('/home/ttw2xk/deepf1data/australia_fullview_run2/linear_label_tensor.pt')
+  backend=image_backends.DeepF1ImageTensorBackend(image_tensor=image_tensor, label_tensor=label_tensor)
 
+ # backend.loadImages('/home/ttw2xk/deepf1data/australia_fullview_run2/linear.csv',(66,200))
+ # torch.save(backend.image_tensor, '/home/ttw2xk/deepf1data/australia_fullview_run2/linear_image_tensor.pt')
+  #torch.save(backend.label_tensor, '/home/ttw2xk/deepf1data/australia_fullview_run2/linear_label_tensor.pt')
 
-    dataset = loaders.F1CombinedDataset("D:/test_data/australia_fullview_run1/fullview_test.csv",size,\
-      context_length=context_length, sequence_length=sequence_length)
-    dataset.loadFiles()
-
-    network = models.AdmiralNet_V2(gpu=gpu,context_length = context_length, sequence_length = sequence_length,\
-    hidden_dim=hidden_dimension, output_dimension = output_dimension, input_channels=input_channels)
-    network = network.cuda(gpu)
-
-    inp = torch.rand(1, context_length, input_channels, size[0], size[1]).cuda(gpu)
-    print(inp.shape)
-
-    output = network(inp)
-
-    print(output.shape)
-
-#    print(flows[24])
-
-
-    #dataset.writePickles()
-
+  ds = loaders.F1ImageSequenceDataset(backend)
+  images, labels = ds[0]
+  print(images.shape)
+  print(labels.shape)
+  flows = loaders.imagesToFlow(images)
+  print(flows.shape)
 if __name__ == '__main__':
-    main()
+  main()
