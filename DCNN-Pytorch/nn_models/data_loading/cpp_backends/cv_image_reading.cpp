ad26e227828edfae4e5ce4109858bf1ad9dc1076
Trent Weiss <ttw2xk@virginia.edu>
2019-02-17 21:58:25 -0500

    yay

--- DCNN-Pytorch/nn_models/data_loading/cpp_backends/cv_image_reading.cpp
+++ DCNN-Pytorch/nn_models/data_loading/cpp_backends/cv_image_reading.cpp
@@ -10,6 +10,7 @@
 
 #include "ndarray_converter.h"
 #include <pybind11/stl.h>
+#include <exception>
 namespace py = pybind11;
 
 cv::Mat readImage(const std::string& image_name, int flag)
@@ -19,12 +20,24 @@ cv::Mat readImage(const std::string& image_name, int flag)
 
 std::vector<cv::Mat> readImages(const std::string& prefix, int first_image, int num_images, int flag, const std::vector<int> size)
 {
-    std::vector<cv::Mat> rtn(num_images);
+    std::vector<cv::Mat> rtn;
+    switch (flag)
+    {
+        case cv::ImreadModes::IMREAD_GRAYSCALE:
+            rtn = std::vector<cv::Mat>(num_images, cv::Mat(size[0], size[1], CV_8U) );
+            break;
+        case cv::ImreadModes::IMREAD_COLOR:
+            rtn = std::vector<cv::Mat>(num_images, cv::Mat(size[0], size[1], CV_8UC3) );
+            break;
+        default:
+            throw std::runtime_error("Only IMREAD_GRAYSCALE and IMREAD_COLOR are currently supported");
+    }
+    
     unsigned int end = first_image + num_images;
     for(int i = first_image; i < end; i ++)
     {
         std::string file = prefix + std::to_string(i+1) + ".jpg";
-        cv::resize(cv::imread(file, flag), rtn[i-first_image], cv::Size2i(size[1], size[0])) ;
+        cv::resize( cv::imread(file, flag), rtn[i-first_image], cv::Size2i(size[1], size[0]) ) ;
     }
     return rtn;
 }
@@ -32,7 +45,7 @@ std::vector<cv::Mat> readImageFlows(const std::string& prefix, int first_image, 
 {
     std::vector<cv::Mat> rtn(num_images, cv::Mat(size[0], size[1], CV_32FC2));
     std::string file = prefix + std::to_string(first_image+1) + ".jpg";
-    cv::Mat first(size[0], size[1], CV_8UC1);
+    cv::Mat first(size[0], size[1], CV_8U);
     cv::Size2i insize(size[1], size[0]);
     cv::resize(cv::imread(file, cv::ImreadModes::IMREAD_GRAYSCALE), first, insize);
     unsigned int start = first_image + 2;
@@ -40,7 +53,7 @@ std::vector<cv::Mat> readImageFlows(const std::string& prefix, int first_image, 
     for(int i = start; i < end; i ++)
     {
         file = prefix + std::to_string(i) + ".jpg";
-        cv::Mat second(size[0], size[1], CV_8UC1);
+        cv::Mat second(size[0], size[1], CV_8U);
         cv::resize( cv::imread( file, cv::ImreadModes::IMREAD_GRAYSCALE) , second, insize );
         cv::calcOpticalFlowFarneback( first, second, rtn[i-start], 0.5, 3, 15, 3, 5, 1.2, 0 );
         first = second;
