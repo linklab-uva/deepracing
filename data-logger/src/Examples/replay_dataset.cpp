1aded5f153841113c61b2ec73e37ba706e6b1531
Trent Weiss <ttw2xk@virginia.edu>
2019-04-03 19:16:34 -0400

    working through issues.

--- data-logger/src/Examples/replay_dataset.cpp
+++ data-logger/src/Examples/replay_dataset.cpp
@@ -17,6 +17,7 @@
 #include <boost/program_options.hpp>
 #include <f1_datalogger/post_processing/post_processing_utils.h>
 #include <google/protobuf/util/json_util.h>
+#include <boost/thread/barrier.hpp> 
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 void exit_with_help(po::options_description& desc)
@@ -38,44 +39,14 @@ void countdown(unsigned int seconds, std::string text = "")
 }
 bool sortByTimestamp(const deepf1::protobuf::TimestampedUDPData& a, const deepf1::protobuf::TimestampedUDPData& b)
 {
-	return a.timestamp() < b.timestamp();
+	return a.udp_packet().m_time() < b.udp_packet().m_time();
 }
 class ReplayDataset_DataGrabHandler : public deepf1::IF1DatagrabHandler
 {
 public:
-	ReplayDataset_DataGrabHandler(std::vector<deepf1::protobuf::TimestampedUDPData>& data, std::shared_ptr<vjoy_plusplus::vJoy> vjoy)
+	ReplayDataset_DataGrabHandler(boost::barrier& bar) :
+		bar_(bar), waiting_(true)
 	{
-		this->vjoy = vjoy;
-		std::sort(data.begin(), data.end(), sortByTimestamp);
-		std::cout << "Got a dataset with " << data.size() << " elements." << std::endl;
-		for (unsigned int i = 0 ; i < data.size() ; i ++)
-		{
-			deepf1::protobuf::TimestampedUDPData datapoint = data.at(i);
-			std::printf("Tag %u has Steering: %f, Throttle: %f, Brake: %f. Lap Time: %f\n", i, datapoint.udp_packet().m_steer(), datapoint.udp_packet().m_throttle(), datapoint.udp_packet().m_brake(), datapoint.udp_packet().m_laptime());
-			if (datapoint.udp_packet().m_laptime() < 1E-4)
-			{
-				std::printf("Found the starting packet at %u with Steering: %f, Throttle: %f, Brake: %f. Lap Time: %f\n", i, datapoint.udp_packet().m_steer(), datapoint.udp_packet().m_throttle(), datapoint.udp_packet().m_brake(), datapoint.udp_packet().m_laptime());
-				std::vector<deepf1::protobuf::TimestampedUDPData>::const_iterator first = data.begin() + i;
-				std::vector<deepf1::protobuf::TimestampedUDPData>::const_iterator last = data.end();
-				dataz_ = std::vector<deepf1::protobuf::TimestampedUDPData>(first, last);
-				break;
-			}
-		}
-		std::cout << "Extracted with " << dataz_.size() << " elements from dataset." << std::endl;
-		for (unsigned int i = 0; i < dataz_.size(); i++)
-		{	
-			laptimes.push_back(dataz_.at(i).udp_packet().m_laptime());
-			steering.push_back(dataz_.at(i).udp_packet().m_steer());
-			throttle.push_back(dataz_.at(i).udp_packet().m_throttle());
-			brake.push_back(dataz_.at(i).udp_packet().m_brake());
-		}
-
-		//std::string first_element;
-		//google::protobuf::util::JsonOptions opshinz;
-		//opshinz.always_print_primitive_fields = true;
-		//opshinz.add_whitespace = true;
-		//google::protobuf::util::MessageToJsonString(dataz_.at(0), &first_element, opshinz);
-		//std::cout << first_element << std::endl;
 	}
 	bool isReady() override
 	{
@@ -83,56 +54,16 @@ public:
 	}
 	void handleData(const deepf1::TimestampedUDPData& data) override
 	{
-		current_packet_ = data;
-		std::vector<double>::iterator up;
-		up = std::upper_bound(laptimes.begin(), laptimes.end(), data.data.m_lapTime);
-		unsigned int idx = (up - laptimes.begin());
-		double t2 = laptimes[idx];
-		if (t2 >= 0.5)
+		if (waiting_ && data.data.m_lapTime<=1E-3)
 		{
-			double t1 = laptimes[idx-1];
-			double DT = t2 - t1;
-			double steer2 = steering[idx];
-			double steer1 = steering[idx-1];
-			double throttle2 = throttle[idx];
-			double throttle1 = throttle[idx - 1];
-			double brake2 = brake[idx];
-			double brake1 = brake[idx - 1];
-			double steer_slope = (steer2 - steer1) / DT;
-			double throttle_slope = (throttle2 - throttle1) / DT;
-			double brake_slope = (brake2 - brake1) / DT;
-			double dt = data.data.m_lapTime - t1;
-			double steer_command = steer1 + steer_slope * dt;
-			if (std::abs(steer_command) < 1E-3)
-			{
-				steer_command = 0.0;
-			}
-			double throttle_command = throttle1 + throttle_slope * dt;
-			if (throttle_command < 1E-3)
-			{
-				throttle_command = 0.0;
-			}
-			double brake_command = brake1 + brake_slope * dt;
-			if (brake_command < 1E-3)
-			{
-				brake_command = 0.0;
-			}
-			js.wAxisY = (unsigned int)std::round(-16383.813867*steer_command + 16383.630437);
-			js.wAxisZ = (unsigned int)(max_vjoythrottle*throttle_command);
-			js.wAxisZRot = (unsigned int)(max_vjoybrake*brake_command);
-			vjoy->update(js);
-			//std::printf("Applying a command of Steering: %f, Throttle: %f, Brake: %f\n", steer_command, throttle_command, brake_command);
-
+			bar_.wait();
+			waiting_ = false;
 		}
 	}
 	void init(const std::string& host, unsigned int port, const std::chrono::high_resolution_clock::time_point& begin) override
 	{
-		max_vjoysteer = (double)vjoy_plusplus::vJoy::maxAxisvalue(), max_vjoythrottle = (double)vjoy_plusplus::vJoy::maxAxisvalue(), max_vjoybrake = (double)vjoy_plusplus::vJoy::maxAxisvalue();
+		
 		this->begin = begin;
-	}
-	deepf1::TimestampedUDPData getCurrentPacket()
-	{
-		return current_packet_;
 	}
 	std::chrono::high_resolution_clock::time_point getBegin()
 	{
@@ -140,19 +71,14 @@ public:
 	}
 private:
 	std::chrono::high_resolution_clock::time_point begin;
-	deepf1::TimestampedUDPData current_packet_;
-	std::vector<double> laptimes, steering, throttle, brake;
-	std::vector<deepf1::protobuf::TimestampedUDPData> dataz_;
-	std::shared_ptr<vjoy_plusplus::vJoy> vjoy;
-	double max_vjoysteer, max_vjoythrottle, max_vjoybrake;
-	vjoy_plusplus::JoystickPosition js;
+	boost::barrier& bar_;
+	bool waiting_;
 };
 class ReplayDataset_FrameGrabHandler : public deepf1::IF1FrameGrabHandler
 {
 public:
 	ReplayDataset_FrameGrabHandler()
 	{
-		
 	}
 	virtual ~ReplayDataset_FrameGrabHandler()
 	{
@@ -177,6 +103,12 @@ public:
 private:
 	std::chrono::high_resolution_clock::time_point begin;
 };
+void setControls(const std::vector<double>& laptimes, const std::vector<double>& steering,
+	const std::vector<double>& throttle, const std::vector<double>& brake, 
+	const double& time, const double& max_vjoy, vjoy_plusplus::JoystickPosition& js)
+{
+	
+}
 int main(int argc, char** argv)
 {
 	std::unique_ptr<std::string> search(new std::string);
@@ -200,23 +132,108 @@ int main(int argc, char** argv)
 	catch (const boost::exception& e) {
 		exit_with_help(desc);
 	}
-	std::shared_ptr<vjoy_plusplus::vJoy> vjoy(new vjoy_plusplus::vJoy(1));
-	vjoy_plusplus::JoystickPosition iReport;
+	vjoy_plusplus::vJoy vjoy(1);
+	vjoy_plusplus::JoystickPosition js;
 	unsigned int min = vjoy_plusplus::vJoy::minAxisvalue(), max = vjoy_plusplus::vJoy::maxAxisvalue();
 	unsigned int middle = (unsigned int)std::round(0.5*(double)(min + max));
-	iReport.lButtons = 0x00000000;
-	iReport.wAxisY = middle;
-	iReport.wAxisZ = 0;
-	iReport.wAxisZRot = 0;
-	vjoy->update(iReport);
+	js.lButtons = 0x00000000;
+	js.wAxisY = middle;
+	js.wAxisZ = 0;
+	js.wAxisZRot = 0;
+	vjoy.update(js);
 	std::vector<deepf1::protobuf::TimestampedUDPData> data = deepf1::post_processing::PostProcessingUtils::parseUDPDirectory(*dir);
+	std::vector<deepf1::protobuf::TimestampedUDPData> sorted_data;
+	std::sort(data.begin(), data.end(), sortByTimestamp);
+	std::cout << "Got a dataset with " << data.size() << " elements." << std::endl;
+	for (unsigned int i = 0; i < data.size(); i++)
+	{
+		deepf1::protobuf::TimestampedUDPData datapoint = data.at(i);
+		std::printf("Tag %u has Steering: %f, Throttle: %f, Brake: %f. Lap Time: %f\n", i, datapoint.udp_packet().m_steer(), datapoint.udp_packet().m_throttle(), datapoint.udp_packet().m_brake(), datapoint.udp_packet().m_laptime());
+		if (datapoint.udp_packet().m_laptime() < 1E-4)
+		{
+			std::printf("Found the starting packet at %u with Steering: %f, Throttle: %f, Brake: %f. Lap Time: %f\n", i, datapoint.udp_packet().m_steer(), datapoint.udp_packet().m_throttle(), datapoint.udp_packet().m_brake(), datapoint.udp_packet().m_laptime());
+			std::vector<deepf1::protobuf::TimestampedUDPData>::const_iterator first = data.begin() + i;
+			std::vector<deepf1::protobuf::TimestampedUDPData>::const_iterator last = data.end();
+			sorted_data = std::vector<deepf1::protobuf::TimestampedUDPData>(first, last);
+			break;
+		}
+	}
+	std::cout << "Extracted with " << sorted_data.size() << " elements from dataset." << std::endl;
+	std::vector<double> laptimes, steering, throttle, brake;
+	double timesteps = 3.0;
+	for (unsigned int i = 1; i < sorted_data.size(); i++)
+	{
+		double currentTime = sorted_data.at(i - 1).udp_packet().m_laptime();
+		double currentSteer = sorted_data.at(i - 1).udp_packet().m_steer();
+		double currentThrottle = sorted_data.at(i - 1).udp_packet().m_throttle();
+		double currentBrake = sorted_data.at(i - 1).udp_packet().m_brake();
+
+		double timeUB = sorted_data.at(i).udp_packet().m_laptime();
+		double steerUB = sorted_data.at(i).udp_packet().m_steer();
+		double throttleUB = sorted_data.at(i).udp_packet().m_throttle();
+		double brakeUB = sorted_data.at(i).udp_packet().m_brake();
+
+		double DT;
+		double throttle2;
+		double throttle1;
+		double brake2;
+		double brake1;
+		double steer_slope;
+		double throttle_slope;
+		double brake_slope;
+		double steer_command;
+		double throttle_command;
+		double brake_command;
+		laptimes.push_back(currentTime);
+		steering.push_back(currentSteer);
+		throttle.push_back(currentThrottle);
+		brake.push_back(currentBrake);
+
+		DT = timeUB - currentTime;
+		steer_slope = (steerUB - currentSteer) / DT;
+		throttle_slope = (throttleUB - currentThrottle) / DT;
+		brake_slope = (brakeUB - currentBrake) / DT;
+		double dt = DT / timesteps;
+		for (double time = currentTime + dt; time < timeUB; time += dt)
+		{
+			double dt = time - currentTime;
+			steer_command = currentSteer + steer_slope * dt;
+		    throttle_command = currentThrottle + throttle_slope * dt;
+			brake_command = currentBrake + brake_slope * dt;
+			laptimes.push_back(time);
+			steering.push_back(steer_command);
+			throttle.push_back(throttle_command);
+			brake.push_back(brake_command);
+		}
+	}
+	double max_vjoysteer = (double)vjoy_plusplus::vJoy::maxAxisvalue(), max_vjoythrottle = (double)vjoy_plusplus::vJoy::maxAxisvalue(), max_vjoybrake = (double)vjoy_plusplus::vJoy::maxAxisvalue();
+	double middle_vjoysteer = max_vjoysteer / 2.0;
+	std::chrono::high_resolution_clock clock;
+	boost::barrier bar(2);
 	std::shared_ptr<ReplayDataset_FrameGrabHandler> image_handler(new ReplayDataset_FrameGrabHandler());
-	std::shared_ptr<ReplayDataset_DataGrabHandler> udp_handler(new ReplayDataset_DataGrabHandler(data, vjoy));
+	std::shared_ptr<ReplayDataset_DataGrabHandler> udp_handler(new ReplayDataset_DataGrabHandler(boost::ref(bar)));
 	deepf1::F1DataLogger dl(*search, image_handler, udp_handler);
 	dl.start();
-	countdown(3, "Starting in");
-	std::string s;
-	std::cin >> s;
+	double time;
+    int idx;
+	bar.wait();
+	//std::cout << "Got past the barrier" << std::endl;
+	std::chrono::high_resolution_clock::time_point begin = clock.now();
+	//Best fit line is : y = -16383.813867*x + 16383.630437
+	while (true)
+	{
+		time = 1E-6*((double)(std::chrono::duration_cast<std::chrono::microseconds>(clock.now() - begin).count()));
+		idx = (std::upper_bound(laptimes.begin(), laptimes.end(), time) - laptimes.begin());
+		//if (idx >= laptimes.size() - (unsigned int)timesteps - 1)
+		//{
+		//	break;
+		//}
+		js.wAxisY = (unsigned int)std::round(-16383.813867 * steering[idx] + 16383.630437);
+		js.wAxisZ = (unsigned int)std::round(max_vjoythrottle*throttle[idx]);
+		js.wAxisZRot = (unsigned int)std::round(max_vjoybrake*brake[idx]);
+		vjoy.update(js);
+	}
+	std::cout << "Thanks for Playing!" << std::endl;
 	//cv::waitKey(0);
 
 }
