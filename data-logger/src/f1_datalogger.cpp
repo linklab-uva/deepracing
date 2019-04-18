86dce97a8e717b15c728dacbfb4604e84d48210b
Trent Weiss <ttw2xk@virginia.edu>
2019-04-18 16:51:46 -0400

    don't start up null handlers

--- data-logger/src/f1_datalogger.cpp
+++ data-logger/src/f1_datalogger.cpp
@@ -18,34 +18,55 @@ F1DataLogger::F1DataLogger(const std::string& search_string, std::shared_ptr<IF1
 {
   begin_ = std::chrono::high_resolution_clock::time_point(clock_->now());
   std::cout<<"Creating managers"<<std::endl;
-  std::cout << "Creating Data Grab Manager" << std::endl;
-  data_grab_manager_.reset(new F1DataGrabManager(clock_, data_grab_handler, host, port));
-  std::cout << "Created Data Grab Manager" << std::endl;
-  std::cout << "Creating Frame Grab Manager" << std::endl;
-  frame_grab_manager_.reset(new F1FrameGrabManager(clock_, frame_grab_handler, search_string) );
-  std::cout << "Created Frame Grab Manager" << std::endl;
-  std::cout<<"Created managers"<<std::endl;
-  const scl::Window& window = frame_grab_manager_->getWindow();
-  cv::Size size;
-  size.height = window.Size.y;
-  size.width = window.Size.x;
 
-  std::cout<<"Got a Window of Size (W x H): " << std::endl << size << std::endl;
-  frame_grab_handler->init(begin_, size);
-  data_grab_handler->init(host, port, begin_);
+  if (!(!data_grab_handler))
+  {
+	  std::cout << "Creating Data Grab Manager" << std::endl;
+	  data_grab_manager_.reset(new F1DataGrabManager(clock_, data_grab_handler, host, port));
+	  std::cout << "Created Data Grab Manager" << std::endl;
+  }
+  if (!(!frame_grab_handler))
+  {
+	  std::cout << "Creating Frame Grab Manager" << std::endl;
+	  frame_grab_manager_.reset(new F1FrameGrabManager(clock_, frame_grab_handler, search_string));
+	  std::cout << "Created Frame Grab Manager" << std::endl;
+  }
+  std::cout<<"Created managers"<<std::endl;
+  if (!(!frame_grab_handler))
+  {
+	  const scl::Window& window = frame_grab_manager_->getWindow();
+	  cv::Size size;
+	  size.height = window.Size.y;
+	  size.width = window.Size.x;
+	  std::cout << "Got a Window of Size (W x H): " << std::endl << size << std::endl;
+	  frame_grab_handler->init(begin_, size);
+  }
+  if (!(!data_grab_handler))
+  {
+	  data_grab_handler->init(host, port, begin_);
+  }
 
 }
 F1DataLogger::~F1DataLogger()
 {
-  data_grab_manager_->stop();
-  data_grab_manager_.reset();
-  frame_grab_manager_.reset();
+	if (!(!data_grab_manager_))
+	{
+		data_grab_manager_->stop();
+	}
+	data_grab_manager_.reset();
+	frame_grab_manager_.reset();
 }
 
 void F1DataLogger::start(double capture_frequency)
 {
-  frame_grab_manager_->start(capture_frequency);
-  data_grab_manager_->start();
+	if (!(!frame_grab_manager_))
+	{
+		frame_grab_manager_->start(capture_frequency);
+	}
+	if (!(!data_grab_manager_))
+	{
+		data_grab_manager_->start();
+	}
 }
 
 const std::chrono::high_resolution_clock::time_point F1DataLogger::getStart() const
