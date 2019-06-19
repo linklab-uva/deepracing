d75705bafe81f1fe0cd95a4c494298141f5c0820
Trent Weiss <ttw2xk@virginia.edu>
2019-06-18 23:27:51 -0400

    add new types for the 2018 spec.

--- data-logger/include/f1_datalogger/car_data/timestamped_car_data.h
+++ data-logger/include/f1_datalogger/car_data/timestamped_car_data.h
@@ -12,6 +12,92 @@ namespace deepf1
 		TimePoint timestamp;
 	}; 
 	typedef struct timestamped_udp_data TimestampedUDPData;
+/*
+  virtual void handleData(const deepf1::twenty_eighteen::PacketCarSetupData& data) = 0;
+  virtual void handleData(const deepf1::twenty_eighteen::PacketCarStatusData& data) = 0;
+  virtual void handleData(const deepf1::twenty_eighteen::PacketCarTelemetryData& data) = 0;
+  virtual void handleData(const deepf1::twenty_eighteen::PacketEventData& data) = 0;
+  virtual void handleData(const deepf1::twenty_eighteen::PacketLapData& data) = 0;
+  virtual void handleData(const deepf1::twenty_eighteen::PacketMotionData& data) = 0;
+  virtual void handleData(const deepf1::twenty_eighteen::PacketParticipantsData& data) = 0;
+  virtual void handleData(const deepf1::twenty_eighteen::PacketSessionData& data) = 0;
+*/
+namespace twenty_eighteen
+{
+	struct TimestampedPacketCarSetupData{
+		TimestampedPacketCarSetupData(const PacketCarSetupData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketCarSetupData data;
+		TimePoint timestamp;
+	};
+	struct TimestampedPacketCarStatusData{
+		TimestampedPacketCarStatusData(const PacketCarStatusData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketCarStatusData data;
+		TimePoint timestamp;
+	};
+	struct TimestampedPacketCarTelemetryData{
+		TimestampedPacketCarTelemetryData(const PacketCarTelemetryData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketCarTelemetryData data;
+		TimePoint timestamp;
+	};
+	struct TimestampedPacketEventData{
+		TimestampedPacketEventData(const PacketEventData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketEventData data;
+		TimePoint timestamp;
+	};
+	struct TimestampedPacketLapData{
+		TimestampedPacketLapData(const PacketLapData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketLapData data;
+		TimePoint timestamp;
+	};
+	struct TimestampedPacketMotionData{
+		TimestampedPacketMotionData(const PacketMotionData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketMotionData data;
+		TimePoint timestamp;
+	};
+	struct TimestampedPacketParticipantsData{
+		TimestampedPacketParticipantsData(const PacketParticipantsData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketParticipantsData data;
+		TimePoint timestamp;
+	};
+	struct TimestampedPacketSessionData{
+		TimestampedPacketSessionData(const PacketSessionData& data, const TimePoint& timestamp)
+		{
+			this->data=data;
+			this->timestamp=timestamp;	
+		}
+		deepf1::twenty_eighteen::PacketSessionData data;
+		TimePoint timestamp;
+	};
+}
+	
 }
 
 #endif
