adbe551dbbb9fd8f55dd83bceb1185ac17140d88
Trent Weiss <ttw2xk@virginia.edu>
2019-07-13 11:17:48 -0400

    add ios controllers implementation

--- data-logger/src/controllers/ios/f1_interface_factory.cpp
+++ data-logger/src/controllers/ios/f1_interface_factory.cpp
@@ -3,5 +3,5 @@
 #include <stdexcept>
 std::shared_ptr<deepf1::F1Interface> deepf1::F1InterfaceFactory::getDefaultInterface(unsigned int device_id)
 {
-  throw std::runtime_error("Virtual game interface is not supported on Linux in this version... :( Sorry...");
+  throw std::runtime_error("Virtual game interface is not supported on IOS in this version... :( Sorry...");
 }
