c4b988a245ec92532f94ab64528571ee52810176
Trent Weiss <ttw2xk@virginia.edu>
2019-07-17 10:21:31 -0400

    it works.

---
+++ DCNN-Pytorch/nn_models/dataset_playground.py
@@ -0,0 +1,19 @@
+import torch
+import torch.utils.data as data_utils
+import h5utils
+import argparse
+from tqdm import tqdm as tqdm
+
+parser = argparse.ArgumentParser(description="Dataset Playground")
+parser.add_argument("dataset_file", type=str,  help="H5 file to use")
+parser.add_argument("--processes", type=int, default=0,  help="Number of extra processes to use")
+args = parser.parse_args()
+
+dset = h5utils.DeepRacingH5Dataset(args.dataset_file)
+
+image_torch, position_torch, rotation_torch, linear_velocity_torch, angular_velocity_torch, session_time = dset[0]
+dataloader = data_utils.DataLoader(dset, batch_size=4,
+                        shuffle=True, num_workers=args.processes)
+t = tqdm(enumerate(dataloader))
+for i_batch, (image_torch, position_torch, rotation_torch, linear_velocity_torch, angular_velocity_torch, session_time) in t:
+    t.set_postfix({"index":i_batch , "image_shape": image_torch.shape,"rotation_shape" : rotation_torch.shape})
--- DCNN-Pytorch/nn_models/h5utils/DeepracingH5Dataset.py
+++ DCNN-Pytorch/nn_models/h5utils/DeepracingH5Dataset.py
@@ -1,5 +1,3 @@
-import os
-import cv2
 import numpy as np
 import torchvision.transforms as transforms
 import torchvision
@@ -8,12 +6,31 @@ from PIL import Image as PILImage
 import h5py
 import skimage
 import torch
-import torch.random
 from torch.utils.data.dataset import Dataset
+import os
 class DeepRacingH5Dataset:
     def __init__(self, h5filepath: str):
         super(DeepRacingH5Dataset, self).__init__()
+        self.h5file = h5py.File(h5filepath, mode="r")
+        self.image_dset = self.h5file["/images"]
+        self.position_dset = self.h5file["/position"]
+        self.rotation_dset = self.h5file["/rotation"]
+        self.linear_velocity_dset = self.h5file["/linear_velocity"]
+        self.angular_velocity_dset = self.h5file["/angular_velocity"]
+        self.session_time_dset = self.h5file["/session_time"]
+        self.imtransform = transforms.ToTensor()
     def __getitem__(self, index):
-        raise( NotImplementedError("Not yet implemented") )
+        image_np = self.image_dset[index]
+        position_np = self.position_dset[index]
+        rotation_np = self.rotation_dset[index]
+        linear_velocity_np = self.linear_velocity_dset[index]
+        angular_velocity_np = self.angular_velocity_dset[index]
+        session_time = self.session_time_dset[index]
+        image_torch = self.imtransform(image_np)
+        position_torch = torch.from_numpy(position_np)
+        rotation_torch = torch.from_numpy(rotation_np)
+        linear_velocity_torch = torch.from_numpy(linear_velocity_np)
+        angular_velocity_torch = torch.from_numpy(angular_velocity_np)
+        return image_torch, position_torch, rotation_torch, linear_velocity_torch, angular_velocity_torch, session_time
     def __len__(self):
-        raise( NotImplementedError("Not yet implemented") )
+        return self.image_dset.shape[0]
