89163532debd7bdad70584a78a9fb6bee604d663
Trent Weiss <ttw2xk@virginia.edu>
2019-07-17 12:38:56 -0400

    phuck tqdm

--- DCNN-Pytorch/nn_models/dataset_playground.py
+++ DCNN-Pytorch/nn_models/dataset_playground.py
@@ -2,8 +2,6 @@ import torch
 import torch.utils.data as data_utils
 import h5utils
 import argparse
-from tqdm import tqdm as tqdm
-
 parser = argparse.ArgumentParser(description="Dataset Playground")
 parser.add_argument("dataset_file", type=str,  help="H5 file to use")
 parser.add_argument("--processes", type=int, default=0,  help="Number of extra processes to use")
@@ -18,7 +16,7 @@ dset = h5utils.DeepRacingH5SequenceDataset(args.dataset_file, 10, 5, map_entire_
 image_torch, position_torch, rotation_torch, linear_velocity_torch, angular_velocity_torch, session_time = dset[0]
 dataloader = data_utils.DataLoader(dset, batch_size=4,
                         shuffle=True, num_workers=args.processes)
-t = tqdm(enumerate(dataloader))
+t = enumerate(dataloader)
 for i_batch, (image_torch, position_torch, rotation_torch, linear_velocity_torch, angular_velocity_torch, session_time) in t:
     print(position_torch)
     print(rotation_torch)
