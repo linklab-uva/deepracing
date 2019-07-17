b1a216dea4ccc5872054610d6da459e8fdad8399
Trent Weiss <ttw2xk@virginia.edu>
2019-07-17 12:53:53 -0400

    use tqdm.

--- DCNN-Pytorch/nn_models/dataset_playground.py
+++ DCNN-Pytorch/nn_models/dataset_playground.py
@@ -2,12 +2,13 @@ import torch
 import torch.utils.data as data_utils
 import h5utils
 import argparse
+from tqdm import tqdm as tqdm
 parser = argparse.ArgumentParser(description="Dataset Playground")
 parser.add_argument("dataset_file", type=str,  help="H5 file to use")
 parser.add_argument("--processes", type=int, default=0,  help="Number of extra processes to use")
 parser.add_argument("--map_entire_file", action="store_true")
 args = parser.parse_args()
-
+print("Hello World!")
 #dset = h5utils.DeepRacingH5Dataset(args.dataset_file, map_entire_file = args.map_entire_file)
 dset = h5utils.DeepRacingH5SequenceDataset(args.dataset_file, 10, 5, map_entire_file = args.map_entire_file)
 
@@ -16,7 +17,6 @@ dset = h5utils.DeepRacingH5SequenceDataset(args.dataset_file, 10, 5, map_entire_
 #image_torch, position_torch, rotation_torch, linear_velocity_torch, angular_velocity_torch, session_time = dset[0]
 dataloader = data_utils.DataLoader(dset, batch_size=4,
                         shuffle=True, num_workers=args.processes)
-t = enumerate(dataloader)
+t = tqdm(enumerate(dataloader))
 for i_batch, (image_torch, position_torch, rotation_torch, linear_velocity_torch, angular_velocity_torch, session_time) in t:
-    print(position_torch)
-    print(rotation_torch)
+    print(angular_velocity_torch)
