6aa05e61e5b0a41a95664921feb46aa0e408990d
Trent Weiss <ttw2xk@virginia.edu>
2019-07-17 17:19:25 -0400

    it works on fake data.

--- DCNN-Pytorch/nn_models/playground.py
+++ DCNN-Pytorch/nn_models/playground.py
@@ -1,17 +1,21 @@
-import time
-import uinput
-events = (uinput.BTN_JOYSTICK, uinput.ABS_Y + (0, 255, 0, 0))
-device = uinput.Device(events)
-time.sleep(2.5)
-print("lez go")
-device.emit(uinput.ABS_Y, 128, syn=False)
-device.emit(uinput.BTN_X, 1, syn=False)
-time.sleep(0.05)
-device.emit(uinput.BTN_X, 0, syn=False)
-time.sleep(0.05)
-device.emit(uinput.BTN_X, 1, syn=False)
-time.sleep(0.05)
-device.emit(uinput.BTN_X, 0, syn=False)
-for i in range(128):
-    device.emit(uinput.ABS_Y, i)
-    time.sleep(0.1)
+import torch
+import nn_models.Models as models
+import numpy as np
+gpu=0
+context_length = np.random.randint(1,high=20)
+sequence_length = np.random.randint(1,high=20)
+hidden_dimension=100
+input_channels = 3
+temporal_conv_feature_factor = 2
+net = models.AdmiralNetPosePredictor(gpu=gpu,context_length = context_length, sequence_length = sequence_length,\
+    hidden_dim=hidden_dimension, input_channels=input_channels, temporal_conv_feature_factor = temporal_conv_feature_factor)
+net = net.cuda(gpu)
+net.train()
+batch_size = 1
+inp = torch.rand(batch_size,context_length,input_channels, 66, 200)
+inp= inp.cuda(gpu)
+
+print("Starting")
+position_predictions, rotation_predictions = net(inp)
+print(position_predictions.shape)
+print(rotation_predictions.shape)
