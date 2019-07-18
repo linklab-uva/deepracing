8341ce1972d288a4b1f3a48e9a8f2185e2915b60
Trent Weiss <ttw2xk@virginia.edu>
2019-07-18 11:21:51 -0400

    add quaternion distance

---
+++ DCNN-Pytorch/nn_models/nn_models/LossFunctions.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+class QuaternionDistance(nn.Module):
+    def __init__(self):
+        super(QuaternionDistance, self).__init__()
+    def forward(self, input, target):
+        prod = torch.mul(input,target)
+        dot = torch.sum(prod,dim=2) 
+        print(dot.shape)
+        acos = torch.acos(dot)
+        print(acos.shape)
+        batched_sum = torch.sum(acos, dim = 1)
+        print(batched_sum.shape)
+        return torch.sum( batched_sum )
--- DCNN-Pytorch/nn_models/playground.py
+++ DCNN-Pytorch/nn_models/playground.py
@@ -1,6 +1,8 @@
 import torch
+import nn_models.LossFunctions as loss_functions
 import nn_models.Models as models
 import numpy as np
+
 gpu=0
 context_length = np.random.randint(1,high=20)
 sequence_length = np.random.randint(1,high=20)
@@ -11,11 +13,19 @@ net = models.AdmiralNetPosePredictor(gpu=gpu,context_length = context_length, se
     hidden_dim=hidden_dimension, input_channels=input_channels, temporal_conv_feature_factor = temporal_conv_feature_factor)
 net = net.cuda(gpu)
 net.train()
-batch_size = 1
+batch_size = 16
 inp = torch.rand(batch_size,context_length,input_channels, 66, 200)
 inp= inp.cuda(gpu)
+loss = loss_functions.QuaternionDistance()
+loss = loss.cuda(gpu)
 
 print("Starting")
 position_predictions, rotation_predictions = net(inp)
 print(position_predictions.shape)
 print(rotation_predictions.shape)
+rotation_targets = torch.rand(batch_size, sequence_length, 4)
+rotation_target_norms = torch.norm(rotation_targets,dim=2)
+rotation_targets = rotation_targets/rotation_target_norms[:,:,None]
+rotation_targets = rotation_targets.cuda(gpu)
+loss_ = loss(rotation_predictions,rotation_targets)
+print(loss_)
