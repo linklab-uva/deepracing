a8674476384883f51332edc8bcaecb32c67d2d95
Trent Weiss <ttw2xk@virginia.edu>
2019-08-01 14:18:40 -0400

    lmdb implementation

--- data-logger/python/image_server.py
+++ data-logger/python/image_server.py
@@ -13,32 +13,38 @@ import time
 from concurrent import futures
 import logging
 import argparse
+import lmdb
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-class ImageFileServer(ImageRPC_pb2_grpc.ImageServiceServicer):
-  def __init__(self, image_folder : str, resize_factor : float = 1.0):
-    super(ImageFileServer, self).__init__()
-    self.image_folder=image_folder
-    self.resize_factor=resize_factor
+class ImageLMDBServer(ImageRPC_pb2_grpc.ImageServiceServicer):
+  def __init__(self, dbfolder : str):
+    super(ImageLMDBServer, self).__init__()
+    self.dbfolder=dbfolder
+    self.size_key = "imsize"
+    self.key_encoding = "ascii"
+    self.lmdb_env = lmdb.open(dbfolder, map_size=1e11)
+    with self.lmdb_env.begin(write=False) as txn:
+        self.im_size = np.frombuffer(txn.get(self.size_key.encode(self.key_encoding)), dtype=np.uint16)
+        print(self.im_size)
+    #self.txn = self.lmdb_env.begin(write=False)
   def GetImage(self, request, context):
-    imcv = cv2.resize( cv2.imread( os.path.join(self.image_folder, request.key) , cv2.IMREAD_UNCHANGED ) , None , fx=self.resize_factor , fy=self.resize_factor , interpolation=cv2.INTER_AREA )
     rtn =  ImageRPC_pb2.ImageResponse()
-    rtn.image.channel_order = ChannelOrder_pb2.ChannelOrder.BGR
-    rtn.image.rows = imcv.shape[0]
-    rtn.image.cols = imcv.shape[1]
-    rtn.image.image_data = imcv.flatten().tobytes()
+    rtn.image.channel_order = ChannelOrder_pb2.ChannelOrder.RGB
+    rtn.image.rows = self.im_size[0]
+    rtn.image.cols = self.im_size[1]
+    with self.lmdb_env.begin(write=False) as txn:
+        rtn.image.image_data = txn.get(request.key.encode(self.key_encoding))
     return rtn
 
 def serve():
     parser = argparse.ArgumentParser(description='Image server.')
     parser.add_argument('address', type=str)
     parser.add_argument('port', type=int)
-    parser.add_argument('image_folder', type=str)
-    parser.add_argument('resize_factor', type=float)
+    parser.add_argument('db_folder', type=str)
     parser.add_argument('--num_workers', type=int, default=1)
     args = parser.parse_args()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=args.num_workers))
-    lmdbserver = ImageFileServer(args.image_folder, resize_factor=args.resize_factor)
+    lmdbserver = ImageLMDBServer(args.db_folder)
     ImageRPC_pb2_grpc.add_ImageServiceServicer_to_server(lmdbserver, server)
     server.add_insecure_port('%s:%d' % (args.address,args.port) )
     print("Starting server")
