07340927e4c14a6d2f49a0a7d121eb3f59226280
Trent Weiss <ttw2xk@virginia.edu>
2019-08-23 15:03:57 -0400

    add import statement.

--- data-logger/python/deepracing/backend/__init__.py
+++ data-logger/python/deepracing/backend/__init__.py
@@ -3,3 +3,4 @@ from .ImageBackends import ImageGRPCClient as ImageGRPCClient
 from .ImageBackends import ImageFolderWrapper as ImageFolderWrapper
 from .LabelBackends import PoseSequenceLabelGRPCClient as PoseSequenceLabelGRPCClient
 from .LabelBackends import PoseSequenceLabelLMDBWrapper as PoseSequenceLabelLMDBWrapper
+from .OpticalFlowBackend import OpticalFlowLMDBWrapper as OpticalFlowLMDBWrapper
