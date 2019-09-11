2677a09d7c9c79e06c50f14f8d367d83f0b44a8b
Trent Weiss <ttw2xk@virginia.edu>
2019-09-11 19:21:48 -0400

    add script for making a trackfile

---
+++ data-logger/python/create_trackfile.py
@@ -0,0 +1,64 @@
+import numpy as np
+import numpy.linalg as la
+import quaternion
+import scipy
+import skimage
+import PIL
+from PIL import Image as PILImage
+import TimestampedPacketMotionData_pb2
+import argparse
+import os
+import google.protobuf.json_format
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import TimestampedImage_pb2
+import Pose3d_pb2
+import cv2
+import PoseSequenceLabel_pb2
+import bisect
+import FrameId_pb2
+import Vector3dStamped_pb2
+import scipy.interpolate
+import deepracing.protobuf_utils
+import deepracing.pose_utils as pose_utils
+def sortKey(packet):
+    return packet.udp_packet.m_header.m_sessionTime
+parser = argparse.ArgumentParser()
+parser.add_argument("motion_data_dir", help="Path to motion data to generate trackfile from",  type=str)
+parser.add_argument("--trackfileout", help="Path to an ARMA format matrix file",  type=str, default="track.arma")
+parser.add_argument("--json", action="store_true", help="Look for json files in motion_data_dir instead of binary .pb files")
+
+args = parser.parse_args()
+motion_data_dir = args.motion_data_dir
+use_json = args.json 
+trackfileout = args.trackfileout
+motion_packets = sorted(pose_utils.getAllMotionPackets(motion_data_dir, args.json), key=sortKey)
+t =  np.array([sortKey(p) for p in motion_packets])
+
+car_index = 0
+poses = [ pose_utils.extractPose(p.udp_packet, car_index = car_index) for p in motion_packets]
+X = np.array([ pose[0] for pose in poses])
+Xdot = np.array([pose_utils.extractVelocity(p.udp_packet, car_index = car_index) for p in motion_packets])
+
+
+Xdotnorm = Xdot.copy()
+for i in range(Xdotnorm.shape[0]):
+    Xdotnorm[i,:] = Xdotnorm[i,:]/la.norm(Xdotnorm[i,:])
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X[:,0], X[:,1], X[:,2], c='r', marker='o', s = np.ones_like(X[:,0]))
+ax.quiver(X[:,0], X[:,1], X[:,2], Xdotnorm[:,0], Xdotnorm[:,1], Xdotnorm[:,2])
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+plt.show()
+
+matout = np.hstack((np.array([t]).transpose(),X,Xdot))
+headerstring = "ARMA_MAT_TXT_FN008\n" + \
+                str(matout.shape[0]) + " " + str(matout.shape[1])
+dirname = os.path.dirname(trackfileout)
+if not os.path.isdir(dirname):
+    os.makedirs(dirname)
+np.savetxt(open(trackfileout,"w"), matout, delimiter="\t", header=headerstring, comments="")
