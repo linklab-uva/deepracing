36606a1c57b16264a28fa7d185ed3584084d2a79
Trent Weiss <ttw2xk@virginia.edu>
2019-12-03 12:52:18 -0500

    update pilotnet training config

--- DCNN-Pytorch/deepracing_models/main_pilotnet.py
+++ DCNN-Pytorch/deepracing_models/main_pilotnet.py
@@ -24,7 +24,7 @@ import matplotlib.animation as animation
 import time
 import cv2
 loss = torch.zeros(1)
-def run_epoch(network, optimizer, trainLoader, gpu, loss_function, imsize=(66,200), debug=False, use_tqdm=True, use_float=True):
+def run_epoch(network, optimizer, trainLoader, gpu, loss_function, imsize=(66,200), debug=False, use_tqdm=True):
     global loss
     cum_loss = 0.0
     batch_size = trainLoader.batch_size
@@ -42,13 +42,8 @@ def run_epoch(network, optimizer, trainLoader, gpu, loss_function, imsize=(66,20
             cv2.namedWindow("Image",cv2.WINDOW_AUTOSIZE)
             cv2.imshow("Image", cv2.cvtColor(image_ubyte,cv2.COLOR_RGB2BGR))
             cv2.waitKey(0)
-        if use_float:
-            image_torch = image_torch.float()
-            control_output = control_output.float()
-        else:
-            image_torch = image_torch.double()
-            control_output = control_output.double()
-
+        image_torch = image_torch.double()
+        control_output = control_output.double()
         if gpu>=0:
             image_torch = image_torch.cuda(gpu)
             control_output = control_output.cuda(gpu)
@@ -71,34 +66,36 @@ def run_epoch(network, optimizer, trainLoader, gpu, loss_function, imsize=(66,20
             t.set_postfix({"cum_loss" : cum_loss/num_samples})
 def go():
     parser = argparse.ArgumentParser(description="Train PilotNet Control Predictor")
-    parser.add_argument("config_file", type=str,  help="Configuration file to load")
+    parser.add_argument("training_config", type=str,  help="Training Parameters Configuration file to load")
+    parser.add_argument("dataset_config", type=str,  help="Dataset Configuration file to load")
     parser.add_argument("output_directory", type=str,  help="Where to put the resulting model files")
 
     parser.add_argument("--debug", action="store_true",  help="Display images upon each iteration of the training loop")
     parser.add_argument("--override", action="store_true",  help="Delete output directory and replace with new data")
     parser.add_argument("--tqdm", action="store_true",  help="Display tqdm progress bar on each epoch")
     args = parser.parse_args()
-    config_file = args.config_file
+    training_config_file = args.training_config
+    dataset_config_file = args.dataset_config
     output_directory = args.output_directory
     debug = args.debug
-    with open(config_file) as f:
-        config = yaml.load(f, Loader = yaml.SafeLoader)
+    with open(training_config_file) as f:
+        training_config = yaml.load(f, Loader = yaml.SafeLoader)
+    with open(dataset_config_file) as f:
+        dataset_config = yaml.load(f, Loader = yaml.SafeLoader)
 
-    image_size = config["image_size"]
-    input_channels = config["input_channels"]
-    gpu = config["gpu"]
-    batch_size = config["batch_size"]
-    learning_rate = config["learning_rate"]
-    momentum = config["momentum"]
-    num_epochs = config["num_epochs"]
-    num_workers = config["num_workers"]
-    debug = config["debug"]
-    control_loss_reduction = config["control_loss_reduction"]
-    use_float = config["use_float"]
+    image_size = dataset_config["image_size"]
+    input_channels = training_config["input_channels"]
+    gpu = training_config["gpu"]
+    batch_size = training_config["batch_size"]
+    learning_rate = training_config["learning_rate"]
+    momentum = training_config["momentum"]
+    num_epochs = training_config["num_epochs"]
+    num_workers = training_config["num_workers"]
+    control_loss_reduction = training_config["control_loss_reduction"]
     if (not args.override) and os.path.isdir(output_directory):
         s = ""
         while(not (s=="y" or s=="n")):
-             s = input("Directory " + output_directory + " already exists. Overwrite it with new data? [y\\n]\n")
+             s = input( "Directory %s already exists. Overwrite it with new data? [y\\n]\n" % (output_directory) )
         if s=="n":
             print("Thanks for playing!")
             exit(0)
@@ -106,16 +103,12 @@ def go():
     elif os.path.isdir(output_directory):
         shutil.rmtree(output_directory)
     os.makedirs(output_directory)
-    net = models.PilotNet(input_channels=input_channels, output_dim=3)
+    net = models.PilotNet(input_channels=input_channels, output_dim=2)
     
     mse_loss = torch.nn.MSELoss(reduction=control_loss_reduction)
     optimizer = optim.SGD(net.parameters(), lr = learning_rate, momentum=momentum)
-    if use_float:
-        net = net.float()
-        mse_loss = mse_loss.float()
-    else:
-        net = net.double()
-        mse_loss = mse_loss.double()
+    net = net.double()
+    mse_loss = mse_loss.double()
     if gpu>=0:
         mse_loss = mse_loss.cuda(gpu)
         net = net.cuda(gpu)
@@ -123,25 +116,26 @@ def go():
         max_spare_txns = 16
     else:
         max_spare_txns = num_workers
-
-    #image_wrapper = deepracing.backend.ImageFolderWrapper(os.path.dirname(image_db))
-    datasets = config["datasets"]
+    datasets = dataset_config["datasets"]
     dsets=[]
     use_optflow=True
     for dataset in datasets:
         print("Parsing database config: %s" %(str(dataset)))
-        image_db = dataset["image_db"]
-        label_db = dataset["label_db"]
+        image_folder = dataset["image_folder"]
+        image_lmdb = os.path.join(image_folder,"image_lmdb")
+        label_folder = dataset["label_folder"]
+        label_lmdb = os.path.join(label_folder,"lmdb")
         key_file = dataset["key_file"]
+
         label_wrapper = deepracing.backend.ControlLabelLMDBWrapper()
-        label_wrapper.readDatabase(label_db, mapsize=3e9, max_spare_txns=max_spare_txns )
+        label_wrapper.readDatabase(label_lmdb, mapsize=3e9, max_spare_txns=max_spare_txns )
+
         image_size = np.array(image_size)
         image_mapsize = float(np.prod(image_size)*3+12)*float(len(label_wrapper.getKeys()))*1.1
         image_wrapper = deepracing.backend.ImageLMDBWrapper(direct_caching=False)
-        image_wrapper.readDatabase(image_db, max_spare_txns=max_spare_txns, mapsize=image_mapsize )
+        image_wrapper.readDatabase(image_lmdb, max_spare_txns=max_spare_txns, mapsize=image_mapsize )
         
-        curent_dset = data_loading.proto_datasets.ControlOutputDataset(image_wrapper, label_wrapper, key_file, \
-                     image_size = image_size)
+        curent_dset = data_loading.proto_datasets.ControlOutputDataset(image_wrapper, label_wrapper, key_file, image_size = image_size)
         dsets.append(curent_dset)
     if len(dsets)==1:
         dset = dsets[0]
@@ -151,7 +145,8 @@ def go():
     dataloader = data_utils.DataLoader(dset, batch_size=batch_size,
                         shuffle=True, num_workers=num_workers)
     print("Dataloader of of length %d" %(len(dataloader)))
-    yaml.dump(config, stream=open(os.path.join(output_directory,"config.yaml"), "w"), Dumper = yaml.SafeDumper)
+    yaml.dump(training_config, stream=open(os.path.join(output_directory,"training_config.yaml"), "w"), Dumper = yaml.SafeDumper)
+    yaml.dump(dataset_config, stream=open(os.path.join(output_directory,"dataset_config.yaml"), "w"), Dumper = yaml.SafeDumper)
     i = 0
     while i < num_epochs:
         time.sleep(2.0)
@@ -160,7 +155,7 @@ def go():
         #dset.clearReaders()
         try:
             tick = time.time()
-            run_epoch(net, optimizer, dataloader, gpu, mse_loss, debug=debug, use_tqdm=args.tqdm, use_float = use_float)
+            run_epoch(net, optimizer, dataloader, gpu, mse_loss, debug=debug, use_tqdm=args.tqdm)
             tock = time.time()
             print("Finished epoch %d in %f seconds." % ( postfix , tock-tick ) )
         except Exception as e:
@@ -177,10 +172,7 @@ def go():
         irand = np.random.randint(0,high=len(dset))
         input_test = torch.rand( 1, input_channels, image_size[0], image_size[1], dtype=torch.float32 )
         input_test[0], control_label = dset[irand]
-        if use_float:
-            input_test = input_test.float()
-        else:
-            input_test = input_test.double()
+        input_test = input_test.double()
         if gpu>=0:
             input_test = input_test.cuda(gpu)
         control_pred = net(input_test)
