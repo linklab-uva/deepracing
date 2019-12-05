4cc7241e7b178a5ee08d4b671212a79e361ef3d2
Trent Weiss <ttw2xk@virginia.edu>
2019-12-05 17:09:11 -0500

    pca analysis is interesting

--- DCNN-Pytorch/deepracing_models/analyze_PCA.py
+++ DCNN-Pytorch/deepracing_models/analyze_PCA.py
@@ -21,8 +21,8 @@ with open(dataset_name+"_data_matrix.pt",'rb') as f:
     datamatrix_torch = torch.load(f)
 # with open(dataset_name+"_covariance.pt",'rb') as f:
 #     C_torch = torch.load(f)
-# with open(dataset_name+"_eigenvalues.pt",'rb') as f:
-#     eigenvalues_real = torch.load(f)
+with open(dataset_name+"_eigenvalues.pt",'rb') as f:
+    eigenvalues_real = torch.load(f)
 with open(dataset_name+"_eigenvectors.pt",'rb') as f:
     eigenvectors = torch.load(f)
 num_images = 9
@@ -36,8 +36,11 @@ im_reconstruc = torch.zeros(num_images+1,D)
 im_reconstruc[0] = im.clone()
 for i in range(1,num_images+2):
     d = int(round(D/(2**i)))
+    s = torch.diag(eigenvalues_real[0:d])
     pcs = eigenvectors[:,0:d]
     pcsT = pcs.transpose(0,1)
-    im_reconstruc[i-1] = (torch.matmul(torch.matmul(im - means, pcs), pcsT) + means).clone()
+    proj = torch.matmul(im - means, pcs)
+    unproj = torch.matmul(proj, pcsT)
+    im_reconstruc[i-1] = (unproj + means).clone()
 plot_images(im_reconstruc.view(num_images+1,imchannels, imrows, imcols), 1, num_images+1)
 plt.show()
