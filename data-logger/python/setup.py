1828579e70af308edbd6956ccfe2a1727093d810
Trent Weiss <ttw2xk@virginia.edu>
2020-01-07 17:18:02 -0500

    merge stuff from GitHub

---
+++ data-logger/python/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+
+setup(
+    name='deepracing',
+    version='0.0.1',
+    packages=['deepracing',
+              'deepracing/backend',
+              'deepracing/controls',
+              'deepracing/imutils',
+              'deepracing/protobuf_utils',],
+    license='Apache License 2.0',
+    long_description=open('README.txt').read(),
+    install_requires=open("requirements.txt").readlines(),
+)
