47213383ea443e05f6a96320332ca2da2ecd22cc
Trent Weiss <ttw2xk@virginia.edu>
2020-03-06 14:18:20 -0500

    yay

---
+++ DCNN-Pytorch/get_comet_experiment_ame2e.py
@@ -0,0 +1,88 @@
+import comet_ml
+import torch
+import os
+import argparse
+from comet_ml.api import API, APIExperiment
+import yaml
+import torch.nn as NN
+import numpy as np
+import torch.utils.data as data_utils
+import deepracing_models.data_loading.proto_datasets as PD
+from tqdm import tqdm as tqdm
+import deepracing_models.nn_models.LossFunctions as loss_functions
+import deepracing_models.nn_models.Models
+parser = argparse.ArgumentParser(description="Download experiment from Comet.ml")
+parser.add_argument("experiment_key", type=str, help="Experiment key to grab from comet.")
+parser.add_argument("--restkey", type=str, required=False, default=None, help="Experiment key to grab from comet.")
+parser.add_argument("--epoch_number", type=int, required=False, default=100, help="Experiment key to grab from comet.")
+parser.add_argument("--output_directory", type=str, required=False, default=".", help="Where to put the config and data files.")
+
+#XMUs9uI19KQNdYrQhuXPnAfpB
+args = parser.parse_args() 
+experiment_key = args.experiment_key
+epoch_number = args.epoch_number
+restkey = args.restkey
+output_directory = os.path.join(args.output_directory, experiment_key + "_from_comet")
+if not os.path.isdir(output_directory):
+    os.makedirs(output_directory)
+if restkey is None:
+    api = API()
+else:
+    api = API(rest_api_key=restkey)
+    
+experiment : APIExperiment = api.get_experiment("electric-turtle", "deepracingadmiralnet-e2e", experiment_key)
+assetlist = experiment.get_asset_list()
+assetdict = {d['fileName']: d['assetId'] for d in assetlist}
+
+
+print("Getting hyperparameters from comet")
+parameters_summary = experiment.get_parameters_summary()
+configin = {d["name"] : d["valueCurrent"] for d in parameters_summary}
+config = {}
+config["image_size"] = np.fromstring( configin["image_size"].replace(" ","").replace("[","").replace("]",""), sep=',', dtype=np.int32 ).tolist()
+# try:
+#     config["image_size"] = np.fromstring( configin["image_size"].replace(" ","").replace("[","").replace("]",""), sep=',', dtype=np.int32 ).tolist()
+# except:
+#     config["image_size"] = [66,200]
+config["input_channels"] = int( configin["input_channels"] )
+config["hidden_dimension"] = int( configin["hidden_dimension"] )
+config["sequence_length"] = int( configin["sequence_length"] )
+config["context_length"] = int( configin["context_length"] )
+config["output_dimension"] = int( configin["output_dimension"] )
+
+print(config)
+outputconfigfile = os.path.join(output_directory,"config.yaml")
+with open(outputconfigfile, 'w') as f:
+    yaml.dump(config,stream=f,Dumper=yaml.SafeDumper)
+
+
+#get network weight file
+weightfilename = "admiralnet_epoch_%d_params.pt" %(epoch_number,)
+optimizerfilename = "admiralnet_epoch_%d_optimizer.pt" %(epoch_number,)
+
+print("Getting network weights from comet")
+params_binary = experiment.get_asset(assetdict[weightfilename])
+
+outputweightfile = os.path.join(output_directory,weightfilename)
+with open(outputweightfile, 'wb') as f:
+    f.write(params_binary)
+
+#get optimizer weight file
+print("Getting optimizer weights from comet")
+optimizer_binary = experiment.get_asset(assetdict[optimizerfilename])
+
+
+outputoptimizerfile = os.path.join(output_directory,optimizerfilename)
+with open(outputoptimizerfile, 'wb') as f:
+    f.write(optimizer_binary)
+
+
+context_length = config["context_length"]
+sequence_length = config["sequence_length"]
+input_channels = config["input_channels"]
+hidden_dimension = config["hidden_dimension"]
+output_dimension = config["output_dimension"]
+net = deepracing_models.nn_models.Models.AdmiralNetKinematicPredictor( context_length = context_length , sequence_length = sequence_length, input_channels=input_channels, hidden_dim = hidden_dimension, output_dimension=output_dimension) 
+net = net.double()
+with open(outputweightfile, 'rb') as f:
+    net.load_state_dict(torch.load(f, map_location=torch.device("cpu")))
--- DCNN-Pytorch/test_openloop_models_RMSE.py
+++ DCNN-Pytorch/test_openloop_models_RMSE.py
@@ -28,6 +28,7 @@ def main():
     parser = argparse.ArgumentParser(description="Test Open Loop Models")
     parser.add_argument("pilotnet_model_file", type=str)
     parser.add_argument("cnnlstm_model_file", type=str)
+    parser.add_argument("admiralnet_e2e_model_file", type=str)
     parser.add_argument("dataset_file", type=str)
     parser.add_argument("--gpu", type=int, default=0)
     parser.add_argument("--write_images", action="store_true")
@@ -61,6 +62,19 @@ def main():
     with open(cnnlstm_model_file,'rb') as f:
         cnnlstm.load_state_dict(torch.load(f,map_location=torch.device("cpu")))
     cnnlstm = cnnlstm.double().cuda(gpu)
+
+    #load end-to-end AdmiralNet stuff
+    admiralnet_e2e_model_file = args.admiralnet_e2e_model_file
+    dataset_file = args.dataset_file
+    admiralnet_e2e_model_dir = os.path.dirname(admiralnet_e2e_model_file)
+    admiralnet_e2e_config_file = os.path.join(admiralnet_e2e_model_dir,'config.yaml')
+    with open(admiralnet_e2e_config_file,'r') as f:
+        admiralnet_e2e_config = yaml.load(f, Loader=yaml.SafeLoader)
+    print(admiralnet_e2e_config)
+    admiralnet_e2e = models.AdmiralNetKinematicPredictor(input_channels=admiralnet_e2e_config["input_channels"], output_dimension=admiralnet_e2e_config["output_dimension"],context_length=admiralnet_e2e_config["context_length"],sequence_length=admiralnet_e2e_config["sequence_length"], hidden_dim=admiralnet_e2e_config["hidden_dimension"])
+    with open(admiralnet_e2e_model_file,'rb') as f:
+        admiralnet_e2e.load_state_dict(torch.load(f,map_location=torch.device("cpu")))
+    admiralnet_e2e = admiralnet_e2e.double().cuda(gpu)
     
 
     with open(dataset_file,'r') as f:
@@ -74,6 +88,7 @@ def main():
         dataset_root = dataset["dataset_root"]
         image_folder = os.path.join(dataset_root,"images")
         image_lmdb = os.path.join(image_folder,"image_lmdb")
+        optflow_lmdb = os.path.join(image_folder,"optical_flow_lmdb")
         label_folder = os.path.join(dataset_root,"steering_labels")
         label_lmdb = os.path.join(label_folder,"lmdb")
         key_file = os.path.join(dataset_root,dataset["key_file"])
@@ -84,8 +99,11 @@ def main():
         image_mapsize = float(np.prod(image_size)*3+12)*float(len(label_wrapper.getKeys()))*1.1
         image_wrapper = deepracing.backend.ImageLMDBWrapper(direct_caching=False)
         image_wrapper.readDatabase(image_lmdb, max_spare_txns=max_spare_txns, mapsize=image_mapsize )
+
+        optflow_wrapper = deepracing.backend.OpticalFlowLMDBWrapper()
+        optflow_wrapper.readDatabase( optflow_lmdb, max_spare_txns=max_spare_txns, mapsize=4*image_mapsize )
         
-        curent_dset = PD.ControlOutputSequenceDataset(image_wrapper, label_wrapper, key_file, image_size = image_size)
+        curent_dset = PD.ControlOutputSequenceDataset(image_wrapper, label_wrapper, key_file, image_size = image_size, optflow_db_wrapper=optflow_wrapper)
         dsets.append(curent_dset)
     if len(dsets)==1:
         dset = dsets[0]
@@ -97,14 +115,24 @@ def main():
     t = tqdm(dataloader)
     diffs_pilot = []
     diffs_cnnlstm =  []
+    diffs_ame2e =  []
     pilotnet.eval()
     cnnlstm.eval()
-    for (i, (image_torch, control_output) ) in enumerate(t):
+    admiralnet_e2e.eval()
+    for (i, (image_torch, optflow_torch, control_output) ) in enumerate(t):
         image_torch_cuda = image_torch.double().cuda(gpu)
+        image_torch_cuda.requires_grad_(False)
+        optflow_torch_cuda = optflow_torch.double().cuda(gpu)
+        optflow_torch_cuda.requires_grad_(False)
         control_output_cuda = control_output[:,0,:].squeeze().double().cuda(gpu)
+        control_output_cuda.requires_grad_(False)
 
         predictionscnnlstm = cnnlstm(image_torch_cuda)[:,0,:].squeeze()
         diffs_cnnlstm.append((predictionscnnlstm - control_output_cuda).detach().cpu().numpy())
+
+        
+        predictionsame2e = admiralnet_e2e( torch.cat( (image_torch_cuda, optflow_torch_cuda), dim=2 ) )[:,0,:].squeeze()
+        diffs_ame2e.append((predictionsame2e - control_output_cuda).detach().cpu().numpy())
     # for (i, (image_torch, control_output) ) in enumerate(t):
     #     image_torch_cuda = image_torch.double().cuda(gpu)
     #     control_output_cuda = control_output[:,0,:].squeeze().double().cuda(gpu)
@@ -116,6 +144,8 @@ def main():
     print(diffs_pilot_torch.shape)
     diffs_cnnlstm_torch = torch.from_numpy(np.array(diffs_cnnlstm))
     print(diffs_cnnlstm_torch.shape)
+    diffs_ame2e_torch = torch.from_numpy(np.array(diffs_ame2e))
+    print(diffs_ame2e_torch.shape)
 
 
 
@@ -127,6 +157,9 @@ def main():
     print("RMSE steering: %f" %(torch.sqrt(torch.mean(torch.pow(diffs_cnnlstm_torch[:,0],2)))))
     print("RMSE throttle: %f" %(torch.sqrt(torch.mean(torch.pow(diffs_cnnlstm_torch[:,1],2)))))
 
+    print("End-to-end AdmiralNet:")
+    print("RMSE steering: %f" %(torch.sqrt(torch.mean(torch.pow(diffs_ame2e_torch[:,0],2)))))
+    print("RMSE throttle: %f" %(torch.sqrt(torch.mean(torch.pow(diffs_ame2e_torch[:,1],2)))))
 
 if __name__ == '__main__':
     main()
