140c980d98d5073ce94c64a8d8ea08ee1f086298
Trent Weiss <ttw2xk@virginia.edu>
2020-06-02 10:12:02 -0400

    alglib wasn't really being used for anything. got rid of it.

--- data-logger/include/f1_datalogger/udp_logging/utils/eigen_utils.h
+++ data-logger/include/f1_datalogger/udp_logging/utils/eigen_utils.h
@@ -13,11 +13,10 @@ namespace deepf1
 		EigenUtils();
 		~EigenUtils();
     static Eigen::MatrixXd loadArmaTxt(const std::string& armafile);
-		static std::vector < Eigen::Vector4d > loadTrackFile(const std::string& trackfile, const double& interpolation_factor = -1.0, bool debug = false);
-		static Eigen::MatrixXd vectorToMatrix(const std::vector < Eigen::Vector4d >& vector);
-		static Eigen::Affine3d motionPacketToPose(const deepf1::twenty_eighteen::protobuf::CarMotionData& motion_packet);
-		static Eigen::Affine3d motionPacketToPose(const deepf1::twenty_eighteen::CarMotionData& motion_packet);
-		static Eigen::Affine3d interpPoses(const Eigen::Affine3d& a, const Eigen::Affine3d& b, const double& s);
+	static Eigen::MatrixXd vectorToMatrix(const std::vector < Eigen::Vector4d >& vector);
+	static Eigen::Affine3d motionPacketToPose(const deepf1::twenty_eighteen::protobuf::CarMotionData& motion_packet);
+	static Eigen::Affine3d motionPacketToPose(const deepf1::twenty_eighteen::CarMotionData& motion_packet);
+	static Eigen::Affine3d interpPoses(const Eigen::Affine3d& a, const Eigen::Affine3d& b, const double& s);
     static Eigen::Affine3d protoToEigen(const deepf1::protobuf::eigen::Pose3d& poseProto);
     static deepf1::protobuf::eigen::Pose3d eigenToProto(const Eigen::Affine3d& poseEigen, const double& session_time, deepf1::protobuf::eigen::FrameId frameid);
 	};
