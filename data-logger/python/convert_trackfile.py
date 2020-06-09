9bced6575436355cbbcf052362c6295938cd0780
Trent Weiss <ttw2xk@virginia.edu>
2020-06-09 02:26:50 -0400

    better script for converting trackfiles

--- data-logger/python/convert_trackfile.py
+++ data-logger/python/convert_trackfile.py
@@ -19,85 +19,77 @@ import FrameId_pb2
 import Vector3dStamped_pb2
 import scipy.interpolate
 import deepracing.arma_utils
+import yaml
+import json
+
 parser = argparse.ArgumentParser()
 parser.add_argument("trackfile", help="Path to trackfile to convert",  type=str)
-parser.add_argument("--add_interpolated_values", help="Path to trackfile to convert",  action="store_true")
+parser.add_argument("--num_samples", default=0, type=int, help="Number of values to sample from the spline. Default (0) means no sampling and just copy the data as is")
+parser.add_argument("--k", default=3, type=int, help="Degree of spline interpolation, ignored if num_samples is 0")
 args = parser.parse_args()
-trackfilein = str(args.trackfile)
+argdict = vars(args)
+trackfilein = argdict["trackfile"]
+num_samples = argdict["num_samples"]
+k = argdict["k"]
 trackin = np.loadtxt(trackfilein,delimiter=",",skiprows=2)
 print(trackin)
 print(trackin.shape)
 I = np.argsort(trackin[:,0])
 track = trackin[I].copy()
-r = track[:,0]
+r = track[:,0].copy()
 
 
 
-X = np.zeros((track.shape[0],3))
-X[:,0] = track[:,1]
-X[:,1] = track[:,3]
-X[:,2] = track[:,2]
+Xin = np.zeros((track.shape[0],4))
+Xin[:,0] = ((r-r[0])/r[-1]).copy()
+Xin[:,1] = track[:,1]
+Xin[:,2] = track[:,3]
+Xin[:,3] = track[:,2]
 
-x = X[:,0]
-y = X[:,1]
-z = X[:,2]
+
+if num_samples>0:
+    spline : scipy.interpolate.BSpline = scipy.interpolate.make_interp_spline(Xin[:,0], Xin[:,1:], k = k)
+    tsamp = np.linspace(0, 1, num = num_samples)
+    splinevals=spline(tsamp)
+    rout = tsamp*r[-1]
+    X = np.column_stack((rout,splinevals))
+    # X[:,0] = tsamp
+    # X[:,1:] = splinevals
+else:
+    X = Xin.copy()
+    X[:,0] = r.copy()
+
+
+
+x = X[:,1]
+y = X[:,2]
+z = X[:,3]
 print(X)
 print(X.shape)
 
 
-spline : scipy.interpolate.BSpline = scipy.interpolate.make_interp_spline(r, X)
-
-splinedot = spline.derivative(nu=1)
-splinedotdot = spline.derivative(nu=2)
-
-Xdot = splinedot(r)
-
-
-xdot = Xdot[:,0]
-ydot = Xdot[:,1]
-zdot = Xdot[:,2]
-
-
-xdotdot = Xdot[:,0]
-ydotdot = Xdot[:,1]
-zdotdot = Xdot[:,2]
-
-
-if args.add_interpolated_values:
-    print("Adding interpolated values")
-    rlin = np.linspace(r[0],r[-1], num = int(round(float(len(r))/2.0)))
-    Xspl = spline(rlin)
-    Xdotspl = splinedot(rlin)
-    Xstack = np.vstack((X,Xspl))
-    Xdotstack = np.vstack((Xdot,Xdotspl))
-    rstack = np.hstack((r,rlin))
-    Istack = np.argsort(rstack)
-    r = rstack[Istack]
-    X = Xstack[Istack]
-    Xdot = Xdotstack[Istack]
-
-
-
-
-Xdotnorm = Xdot.copy()
-for i in range(Xdotnorm.shape[0]):
-    Xdotnorm[i,:] = Xdotnorm[i,:]/la.norm(Xdotnorm[i,:])
-print("New X shape: ", X.shape)
-print("New Xdot shape: ", Xdot.shape)
-print("New Xdotdotnorm shape: ", Xdotnorm.shape)
-
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(X[:,0], X[:,1], X[:,2], c='r', marker='o', s = np.ones_like(x))
-ax.quiver(X[:,0], X[:,1], X[:,2], Xdotnorm[:,0], Xdotnorm[:,1], Xdotnorm[:,2])
-ax.set_xlabel('X Label')
-ax.set_ylabel('Y Label')
-ax.set_zlabel('Z Label')
+#ax = fig.add_subplot(111, projection='3d')
+plt.scatter(Xin[:,1], Xin[:,3], c='b', marker='o', s = 2.0*np.ones_like(x))
+plt.scatter(x, z, c='r', marker='o', s = 2.0*np.ones_like(x))
+#plt.plot(x, z, c='b')#, marker='o', s = np.ones_like(x))
+# ax.set_xlabel('X Label')
+# ax.set_ylabel('Y Label')
+# ax.set_zlabel('Z Label')
 plt.show()
 
 armaout = os.path.splitext(trackfilein)[0] + ".arma.txt"
-# matout = np.hstack((np.array([r]).transpose(),X,Xdot,Xdotdot))
-# headerstring = "ARMA_MAT_TXT_FN008\n" + \
-#                 str(matout.shape[0]) + " " + str(matout.shape[1])
-# np.savetxt(armaout, matout, delimiter="\t", header=headerstring, comments="")
-deepracing.arma_utils.writeArmaFile(armaout, r,X,Xdot)
+#matout = np.hstack((np.array([r]).transpose(),X,Xdot,Xdotdot))
+headerstring = "ARMA_MAT_TXT_FN008\n" + \
+                str(X.shape[0]) + " " + str(X.shape[1])
+np.savetxt(armaout, X, delimiter="\t", header=headerstring, comments="")
+jsonout = os.path.splitext(trackfilein)[0] + ".json"
+with open(jsonout,"w") as f:
+    jsondict : dict = {}
+    jsondict["dist"] = X[:,0].tolist()
+    jsondict["x"] = X[:,1].tolist()
+    jsondict["y"] = X[:,2].tolist()
+    jsondict["z"] = X[:,3].tolist()
+    json.dump( jsondict , f , indent=1 )
+    
+#deepracing.arma_utils.writeArmaFile(armaout, r,X,Xdot)
