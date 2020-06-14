584af2c5436cacf1d449f114023e3fda7f2f4371
Trent Weiss <ttw2xk@virginia.edu>
2020-06-13 21:51:42 -0400

    create optimizer AFTER loading model

--- DCNN-Pytorch/resume_bezier_predictor.py
+++ DCNN-Pytorch/resume_bezier_predictor.py
@@ -321,7 +321,7 @@ def go():
     else:
         raise ValueError("Uknown optimizer " + optimizer)
     with open(optimizerfilename,"rb") as f:
-        optimizer.load_state_dict(torch.load(f, map_location=net.device()))
+        optimizer.load_state_dict(torch.load(f, map_location=torch.device("cpu")))
     
     
     if use_float:
