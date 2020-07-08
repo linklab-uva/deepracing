970e748965927e154538c85f17ce9d714c491acc
Trent Weiss <ttw2xk@virginia.edu>
2020-07-08 08:50:50 -0400

    add script for doing nothing but running the network in a loop

---
+++ DCNN-Pytorch/benchmark_bezier.py
@@ -0,0 +1,17 @@
+import torch
+from deepracing_models.nn_models.Models import AdmiralNetCurvePredictor as AdmiralNetCurvePredictor
+import time
+print("Building the network")
+network = AdmiralNetCurvePredictor(params_per_dimension=7)
+network = network.double().cuda(0)
+running = True
+print("Running the network in a loop")
+while running:
+    try:
+        inp = torch.randn(1,5,3,66,200, device=torch.device("cuda:0"), dtype=torch.float64)
+        output = network(inp)
+        time.sleep(0.1)
+    except KeyboardInterrupt as e:
+        running = False
+
+
