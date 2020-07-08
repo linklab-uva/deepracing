be8c828b63b0f91ccdbd24f7fbd4587fb10df367
Trent Weiss <ttw2xk@virginia.edu>
2020-07-08 12:07:10 -0400

    use 2 spaces instead of tabs on a script

--- DCNN-Pytorch/benchmark_bezier.py
+++ DCNN-Pytorch/benchmark_bezier.py
@@ -7,11 +7,11 @@ network = network.double().cuda(0)
 running = True
 print("Running the network in a loop")
 while running:
-    try:
-        inp = torch.randn(1,5,3,66,200, device=torch.device("cuda:0"), dtype=torch.float64)
-        output = network(inp)
-        time.sleep(0.1)
-    except KeyboardInterrupt as e:
-        running = False
+  try:
+      inp = torch.randn(1,5,3,66,200, device=torch.device("cuda:0"), dtype=torch.float64)
+      output = network(inp)
+      time.sleep(0.1)
+  except KeyboardInterrupt as e:
+      running = False
 
 
