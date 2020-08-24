ed887a6d964ca5325cfd3e48224254e05ab40321
Trent Weiss <ttw2xk@virginia.edu>
2020-08-24 10:43:37 -0400

    also publish raceline

--- ros2bindings/f1_datalogger_ros/src/boundary_publisher.cpp
+++ ros2bindings/f1_datalogger_ros/src/boundary_publisher.cpp
@@ -50,12 +50,25 @@ Json::Value readJsonFile(std::shared_ptr<rclcpp::Node> node, std::string filepat
         RCLCPP_FATAL(node->get_logger(), "no \"z\" key found in the json dictionary");
     }
     
-    std::array<int,3> sizes = {xarray.size(), yarray.size(), zarray.size()};
-    if (! std::all_of(sizes.begin(), sizes.end(), [xarray, yarray, zarray](int i){return i==xarray.size();}) )
+    std::array<uint32_t,3> sizes = {(uint32_t)xarray.size(), (uint32_t)yarray.size(), (uint32_t)zarray.size()};
+    if (! std::all_of(sizes.begin(), sizes.end(), [xarray, yarray, zarray](uint32_t i){return i==xarray.size();}) )
     {
-       RCLCPP_FATAL(node->get_logger(), "All three arrays are not the same size"); 
+       RCLCPP_FATAL(node->get_logger(), "All three arrays are not the same size. Size of x array: %lu. Size of y array: %lu. Size of z array: %lu",sizes[0], sizes[1], sizes[2]); 
     }
     return rootval;
+}
+pcl::PointCloud<pcl::PointXYZ> loadTrackFile(std::shared_ptr<rclcpp::Node> node, const std::string& filepath)
+{
+    Json::Value boundary_dict = readJsonFile(node, filepath);
+    Json::Value boundary_x = boundary_dict["x"];
+    Json::Value boundary_y = boundary_dict["y"];
+    Json::Value boundary_z = boundary_dict["z"];
+    pcl::PointCloud<pcl::PointXYZ> rtn;
+    for (unsigned int i =0; i < boundary_x.size(); i++)
+    {
+        rtn.push_back(pcl::PointXYZ(boundary_x[i].asDouble(), boundary_y[i].asDouble(), boundary_z[i].asDouble()));
+    }
+    return rtn;
 }
 class NodeWrapper_
 {
@@ -94,8 +107,8 @@ int main(int argc, char** argv)
     std::shared_ptr< rclcpp::Publisher<sensor_msgs::msg::PointCloud2> > racelinepub = node->create_publisher<sensor_msgs::msg::PointCloud2>("/optimal_raceline",1);
     
     NodeWrapper_ nw(node);
-    pcl::PointCloud<pcl::PointXYZ> innercloudPCL, outercloudPCL;
-    sensor_msgs::msg::PointCloud2 innercloudMSG, outercloudMSG;
+    pcl::PointCloud<pcl::PointXYZ> innercloudPCL, outercloudPCL, racelinecloudPCL;
+    sensor_msgs::msg::PointCloud2 innercloudMSG, outercloudMSG, racelinecloudMSG;
 
     rclcpp::ParameterValue track_dir_param = node->declare_parameter("track_dir",rclcpp::ParameterValue(""));
     std::string track_dir = track_dir_param.get<std::string>();
@@ -118,46 +131,40 @@ int main(int argc, char** argv)
         if(!active_track.empty() && track_name.compare(active_track)!=0)
         {            
             track_name = active_track;
+
             std::string inner_filename = track_name + "_innerlimit.json";
             RCLCPP_INFO(node->get_logger(), "Openning file %s in directory %s.", inner_filename.c_str(), track_dir.c_str());
-            std::string inner_jsonpath = (fs::path(track_dir) / fs::path(inner_filename)).string();
-            Json::Value inner_boundary_dict = readJsonFile(node, inner_jsonpath);
-            Json::Value inner_boundary_x = inner_boundary_dict["x"];
-            Json::Value inner_boundary_y = inner_boundary_dict["y"];
-            Json::Value inner_boundary_z = inner_boundary_dict["z"];
-            for (unsigned int i =0; i < inner_boundary_x.size(); i++)
-            {
-                innercloudPCL.push_back(pcl::PointXYZ(inner_boundary_x[i].asDouble(), inner_boundary_y[i].asDouble(), inner_boundary_z[i].asDouble()));
-            }
-            RCLCPP_INFO(node->get_logger(), "Got %d points for the inner boundary.", innercloudPCL.size());
-
+            innercloudPCL = loadTrackFile(node,(fs::path(track_dir) / fs::path(inner_filename)).string());
 
             std::string outer_filename = track_name + "_outerlimit.json";
             RCLCPP_INFO(node->get_logger(), "Openning file %s in directory %s.", outer_filename.c_str(), track_dir.c_str());
-            std::string outer_jsonpath = (fs::path(track_dir) / fs::path(outer_filename)).string();
-            Json::Value outer_boundary_dict = readJsonFile(node, outer_jsonpath);
-            Json::Value outer_boundary_x = outer_boundary_dict["x"];
-            Json::Value outer_boundary_y = outer_boundary_dict["y"];
-            Json::Value outer_boundary_z = outer_boundary_dict["z"];
-            for (unsigned int i =0; i < outer_boundary_x.size(); i++)
-            {
-                outercloudPCL.push_back(pcl::PointXYZ(outer_boundary_x[i].asDouble(), outer_boundary_y[i].asDouble(), outer_boundary_z[i].asDouble()));
-            }
+            outercloudPCL = loadTrackFile(node,(fs::path(track_dir) / fs::path(outer_filename)).string());
             RCLCPP_INFO(node->get_logger(), "Got %d points for the outer boundary.", outercloudPCL.size());
-            pcl::PCLPointCloud2 innercloudPC2, outercloudPC2;
+
+            std::string raceline_filename = track_name + "_racingline.json";
+            RCLCPP_INFO(node->get_logger(), "Openning file %s in directory %s.", raceline_filename.c_str(), track_dir.c_str());
+            racelinecloudPCL = loadTrackFile(node,(fs::path(track_dir) / fs::path(raceline_filename)).string());
+            RCLCPP_INFO(node->get_logger(), "Got %d points for the optimal raceline.", racelinecloudPCL.size());
+
+            pcl::PCLPointCloud2 innercloudPC2, outercloudPC2, racelinecloudPC2;
             pcl::toPCLPointCloud2(pcl::PointCloud<pcl::PointXYZ>(innercloudPCL), innercloudPC2);
             pcl::toPCLPointCloud2(pcl::PointCloud<pcl::PointXYZ>(outercloudPCL), outercloudPC2);
+            pcl::toPCLPointCloud2(pcl::PointCloud<pcl::PointXYZ>(racelinecloudPCL), racelinecloudPC2);
             pcl_conversions::moveFromPCL(innercloudPC2, innercloudMSG);
             pcl_conversions::moveFromPCL(outercloudPC2, outercloudMSG);
+            pcl_conversions::moveFromPCL(racelinecloudPC2, racelinecloudMSG);
             innercloudMSG.header.frame_id = "track"; 
             outercloudMSG.header.frame_id = "track"; 
+            racelinecloudMSG.header.frame_id = "track"; 
 
         }
         innercloudMSG.header.stamp = node->now();
         outercloudMSG.header.stamp = innercloudMSG.header.stamp;
+        racelinecloudMSG.header.stamp = innercloudMSG.header.stamp;
 
         innerpub->publish(innercloudMSG);
         outerpub->publish(outercloudMSG);
+        racelinepub->publish(racelinecloudMSG);
     }
 
 
