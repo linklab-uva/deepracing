7246368c81ca826f9468240f141c0eb1874dd007
Trent Weiss <ttw2xk@virginia.edu>
2020-09-26 09:38:58 -0400

    fix a bug

--- data-logger/python/deepracing/raceline_utils/__init__.py
+++ data-logger/python/deepracing/raceline_utils/__init__.py
@@ -4,19 +4,18 @@ from scipy.interpolate import make_interp_spline, BSpline, CubicSpline
 from scipy.spatial.transform import Rotation as Rot
 
 def shiftRaceline(raceline: np.ndarray, reference_vec: np.ndarray, distance: float, s = None):
+    diffs = raceline[1:] - raceline[0:-1]
+    diffnorms = np.linalg.norm(diffs, axis=1, ord=2)
     if s is None:
-        diffs = raceline[1:] - raceline[0:-1]
-        diffnorms = np.linalg.norm(diffs, axis=1, ord=2)
         s_ = np.hstack([np.zeros(1), np.cumsum(diffnorms)])
-        nogood = diffnorms==0.0
-       # print(diffnorms[nogood])
-        good = np.hstack([np.array([True]), ~nogood])
-        s_ = s_[good]
-        raceline = raceline[good]
     else:
-        s_ = s
-    #racelinespl : BSpline = make_interp_spline(s_, raceline, k = k)
-    racelinespl : CubicSpline = CubicSpline(s_, raceline)
+        assert(s.shape[0]==raceline.shape[0])
+        s_ = s.copy()
+    nogood = diffnorms==0.0
+    good = np.hstack([np.array([True]), ~nogood])
+    s_ = s_[good]
+    racelinegood = raceline[good].copy()
+    racelinespl : CubicSpline = CubicSpline(s_, racelinegood)
     racelinetanspl = racelinespl.derivative()
     tangents = racelinetanspl(s_)
     tangent_norms = np.linalg.norm(tangents,axis=1,ord=2)
@@ -24,4 +23,4 @@ def shiftRaceline(raceline: np.ndarray, reference_vec: np.ndarray, distance: flo
     laterals = np.row_stack([np.cross(unit_tangents[i], reference_vec) for i in range(unit_tangents.shape[0])])
     lateral_norms = np.linalg.norm(laterals,axis=1,ord=2)
     unit_laterals = laterals/lateral_norms[:,np.newaxis]
-    return s_, raceline + distance*unit_laterals
+    return s_, racelinegood + distance*unit_laterals
