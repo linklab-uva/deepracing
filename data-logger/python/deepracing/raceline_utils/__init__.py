a48d90fc64904a6d2c7d48be7625f0c3ac458491
Trent Weiss <ttw2xk@virginia.edu>
2020-09-26 08:03:14 -0400

    remove duplicates

--- data-logger/python/deepracing/raceline_utils/__init__.py
+++ data-logger/python/deepracing/raceline_utils/__init__.py
@@ -1,16 +1,22 @@
 import numpy as np
 import scipy.interpolate
-from scipy.interpolate import make_interp_spline, BSpline
+from scipy.interpolate import make_interp_spline, BSpline, CubicSpline
 from scipy.spatial.transform import Rotation as Rot
 
-def shiftRaceline(raceline: np.ndarray, reference_vec: np.ndarray, distance: float, s = None, k = 3):
+def shiftRaceline(raceline: np.ndarray, reference_vec: np.ndarray, distance: float, s = None):
     if s is None:
         diffs = raceline[1:] - raceline[0:-1]
         diffnorms = np.linalg.norm(diffs, axis=1, ord=2)
         s_ = np.hstack([np.zeros(1), np.cumsum(diffnorms)])
+        nogood = diffnorms==0.0
+       # print(diffnorms[nogood])
+        good = np.hstack([np.array([True]), ~nogood])
+        s_ = s_[good]
+        raceline = raceline[good]
     else:
         s_ = s
-    racelinespl : BSpline = make_interp_spline(s_, raceline, k = k)
+    #racelinespl : BSpline = make_interp_spline(s_, raceline, k = k)
+    racelinespl : CubicSpline = CubicSpline(s_, raceline)
     racelinetanspl = racelinespl.derivative()
     tangents = racelinetanspl(s_)
     tangent_norms = np.linalg.norm(tangents,axis=1,ord=2)
