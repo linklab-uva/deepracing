e2489eb2484e8c7ad0d6a52c7b0a65ec9a7a5bf5
Trent Weiss <ttw2xk@virginia.edu>
2020-11-10 17:51:33 -0500

    update constraints

--- data-logger/python/deepracing/path_utils/optimization.py
+++ data-logger/python/deepracing/path_utils/optimization.py
@@ -15,29 +15,24 @@ class OptimWrapper():
         self.linearaccelmat[-1,0] = 1.0
         self.linearaccelmat*=(1.0/(2.0*ds))
         self.acentripetalmat = np.diag(1.0/radii)
-    # def getLinearVelConstraint(self, keep_feasible=True):
-    #     lb = np.zeros_like(self.radii)
-    #     ub = (self.maxspeed**2)*np.ones_like(self.radii)
-    #     return LinearConstraint(np.eye(lb.shape[0]), lb, ub, keep_feasible=keep_feasible)
+        
     def getLinearAccelConstraint(self, keep_feasible=False):
-        ub = self.maxlinearaccel*np.ones_like(self.radii)
-        lb = (-ub).copy()
-        return LinearConstraint(self.linearaccelmat, lb, ub, keep_feasible=keep_feasible)
+        return LinearConstraint(self.linearaccelmat, -self.maxlinearaccel, self.maxlinearaccel, keep_feasible=keep_feasible)
+
     def getCentripetalAccelConstraint(self, keep_feasible=False):
-        lb = np.zeros_like(self.radii)
-        ub = self.maxcentripetalaccel*np.ones_like(self.radii)
-        return LinearConstraint(self.acentripetalmat, lb, ub, keep_feasible=keep_feasible)
+        return LinearConstraint(self.acentripetalmat, 0, self.maxcentripetalaccel, keep_feasible=keep_feasible)
+
     def jac(self, xcurr):
         return -np.ones_like(xcurr)
+        
     def functional(self, xcurr):
         return -np.sum(xcurr)
+
     def optimize(self, x0 = None , method="SLSQP", maxiter=5):
-        lb = np.zeros_like(self.radii)
-        ub = (self.maxspeed**2)*np.ones_like(self.radii)
         if x0 is None:
-            x0 = 0.5*ub.copy()
+            x0 = 0.5*(self.maxspeed**2)*np.ones_like(self.radii)
         constraints = (self.getLinearAccelConstraint(), self.getCentripetalAccelConstraint())
-        return x0, minimize(self.functional, x0, method=method, jac=self.jac, constraints=constraints, options = {"maxiter": maxiter}, bounds=Bounds(lb, ub, keep_feasible=True))
+        return x0, minimize(self.functional, x0, method=method, jac=self.jac, constraints=constraints, options = {"maxiter": maxiter}, bounds=Bounds(0, self.maxspeed**2, keep_feasible=True))
 
 
 
