5672a71f303f2cbdd97925fb104d9f03a2b1ea6b
Trent Weiss <ttw2xk@virginia.edu>
2020-11-10 22:36:33 -0500

    yay

--- data-logger/python/convert_trackfile.py
+++ data-logger/python/convert_trackfile.py
@@ -76,8 +76,9 @@ final_vector = Xin[0,1:] - Xin[-1,1:]
 final_distance = np.linalg.norm(final_vector)
 print("initial final distance: %f" %(final_distance,))
 final_unit_vector = final_vector/final_distance
-nstretch = 3
-rstretch =  np.linspace(final_distance/nstretch,final_distance,nstretch)
+nstretch = 8
+rstretch =  np.linspace(final_distance/nstretch, final_distance,nstretch)
+# rstretch =  np.linspace(final_distance/nstretch,((nstretch-1)/nstretch)*final_distance,nstretch)
 final_stretch = np.row_stack([Xin[-1,1:] + rstretch[i]*final_unit_vector for i in range(rstretch.shape[0])])
 final_r =  rstretch + Xin[-1,0]
 Xin = np.row_stack((Xin, np.column_stack((final_r,final_stretch))))
@@ -94,7 +95,7 @@ if finalidx is None:
 else:
     finalextrassamps = abs(finalidx)
 
-rsamp = np.linspace(Xin[0,0], Xin[-1,0], num = num_samples + finalextrassamps)[0:finalidx]
+rsamp = np.linspace(Xin[0,0], Xin[-1,0], num = num_samples)# + finalextrassamps)#[0:finalidx]
 splinevals=spline(rsamp)
 rout = rsamp
 
@@ -110,22 +111,6 @@ tangents = tangentspline(rsamp)
 tangentnorms = np.linalg.norm(tangents, ord=2, axis=1)
 unit_tangents = tangents/tangentnorms[:,np.newaxis]
 
-accels = normalspline(rsamp)
-accelnorms = np.linalg.norm(accels, ord=2, axis=1)
-
-dotsquares = np.sum(tangents*accels, axis=1)**2
-
-radii = (tangentnorms**3)/np.sqrt((tangentnorms**2)*(accelnorms**2) - dotsquares)
-print(radii)
-#unit_normals = normals/normalnorms[:,np.newaxis]
-#print(unit_normals.shape)
-
-
-#if innerboundary:
-#     rotation = Rot.from_rotvec(np.array([0.0,0.0,-np.pi/2]))
-# else:
-#     rotation = Rot.from_rotvec(np.array([0.0,0.0,np.pi/2]))
-# unit_normals = np.matmul(rotation.as_matrix(), unit_tangents.transpose()).transpose()
 
 ref = np.column_stack([np.zeros_like(unit_tangents.shape[0]), np.ones_like(unit_tangents.shape[0]), np.zeros_like(unit_tangents.shape[0])]).astype(np.float64)
 if innerboundary:
@@ -139,8 +124,6 @@ unit_normals = np.cross(v2, unit_tangents)
 unit_normals = unit_normals/np.linalg.norm(unit_normals, axis=1, ord=2)[:,np.newaxis]
 
 
-
-
 normaltangentdots = np.sum(unit_tangents*unit_normals, axis=1)
 # print(normaltangentdots)
 # print(normaltangentdots.shape)
@@ -150,22 +133,7 @@ if not np.all(np.abs(normaltangentdots)<=1E-6):
 print("Max dot between normals and tangents: %f" % (np.max(normaltangentdots),) )
 
 
-# assert(unit_normals.shape[0]==splinevals.shape[0])
-# offset_points = splinevals + unit_normals*0.1
-# if innerboundary:
-#     wrongside = np.array( [ not polygon.contains(ShapelyPoint(splinevals[i,0], splinevals[i,2]))  for i in range(splinevals.shape[0]) ] )
-# else:
-#     wrongside = np.array( [ polygon.contains(ShapelyPoint(splinevals[i,0], splinevals[i,2]))  for i in range(splinevals.shape[0]) ] )
-# unit_normals[wrongside]*=-1.0
-
 offset_points = splinevals + unit_normals*1.0
-
-
-
-
-# normals = np.cross(up,unit_tangents)
-# if "innerlimit" in os.path.basename(trackfilein):
-#     normals*=-1.0
 
 
 X = np.column_stack((rout,splinevals))
@@ -194,33 +162,133 @@ plt.quiver(x, z, unit_normals[:,0], unit_normals[:,2], angles="xy", scale=4.0, s
 plt.show()
 
 
+rsampradii = np.linspace(rsamp[0], rsamp[-1], num=num_samples)
+positionsradii = spline(rsampradii)
 
-maxspeed = 150.0
-maxlinearaccel = 15.0
-maxcentripetalaccel = 9.8*4.0
-maxiter=30
-ds = np.mean(np.linalg.norm(splinevals[1:] - splinevals[0:-1], ord=2, axis=1))
-sqp = OptimWrapper(maxspeed, maxlinearaccel, maxcentripetalaccel, ds, radii)
+
+tangentsradii = tangentspline(rsampradii)
+tangentsradiinorms = np.linalg.norm(tangentsradii, ord=2, axis=1)
+unittangentsradii = tangentsradii/tangentsradiinorms[:,np.newaxis]
+
+
+accels = normalspline(rsampradii)
+accelnorms = np.linalg.norm(accels, ord=2, axis=1)
+
+dotsquares = np.sum(tangentsradii*accels, axis=1)**2
+
+radii = (tangentsradiinorms**3)/np.sqrt((tangentsradiinorms**2)*(accelnorms**2) - dotsquares)
+
+
+
+maxspeed = 86.0
+maxlinearaccel = 13.0
+maxcentripetalaccel = 9.8*3.0
+maxiter=100
+meands = np.mean(np.linalg.norm(splinevals[1:] - splinevals[0:-1], ord=2, axis=1))
+sqp = OptimWrapper(maxspeed, maxlinearaccel, maxcentripetalaccel, meands, radii)
 
 
 x0, res = sqp.optimize(maxiter=maxiter)
-print(res)
-print(x0)
+v0 = np.sqrt(x0)
+velsquares = res.x
+vels = np.sqrt(velsquares)
+#print(v0)
+print(vels)
+
+tlist = [0.0]
+for i in range(0, vels.shape[0]-1):
+    ds = rsampradii[i+1] - rsampradii[i]
+    dt = ds/vels[i]
+    tlist.append(tlist[-1] + dt)
+# dxfinal = positionsradii[0] - positionsradii[-1]
+# dsfinal =  np.linalg.norm(dxfinal, ord=2)
+# tlist.append(tlist[-1] + 0.75*dsfinal/vels[-1])
+# positionsradii = np.row_stack([positionsradii, positionsradii[-1] + 0.75*dxfinal])
+tparameterized = np.array(tlist)
+# print("tparameterized: %s" % (str(tparameterized),))
+
+truespline : scipy.interpolate.BSpline = scipy.interpolate.make_interp_spline(tparameterized, positionsradii)
+truesplinevel : scipy.interpolate.BSpline = truespline.derivative()
+truesplineaccel : scipy.interpolate.BSpline = truesplinevel.derivative()
+
+
+nout = 4000
+tsamp = np.linspace(tparameterized[0], tparameterized[-1], num=nout)
+print("dt: %f" % (tsamp[-1] - tsamp[0],))
+
+psamp = truespline(tsamp)
+xtrue = psamp[:,0]
+ytrue = psamp[:,1]
+ztrue = psamp[:,2]
+
+vsamp = truesplinevel(tsamp)
+xdottrue = vsamp[:,0]
+ydottrue = vsamp[:,1]
+zdottrue = vsamp[:,2]
+#print(unittangentsradii*vels[:,np.newaxis]-truesplinevel(tparameterized))
+
+asamp = truesplineaccel(tsamp)
+xdotdottrue = asamp[:,0]
+ydotdottrue = asamp[:,1]
+zdotdottrue = asamp[:,2]
+
+
+ref = np.column_stack([np.zeros_like(unit_tangents.shape[0]), np.ones_like(unit_tangents.shape[0]), np.zeros_like(unit_tangents.shape[0])]).astype(np.float64)
+if innerboundary:
+    ref[:,1]*=-1.0
+v1 = np.cross(unit_tangents, ref)
+v1 = v1/np.linalg.norm(v1, axis=1, ord=2)[:,np.newaxis]
+v2 =  np.cross(v1, unit_tangents)
+v2 = v2/np.linalg.norm(v2, axis=1, ord=2)[:,np.newaxis]
+
+
+
+fig2 = plt.figure()
+plt.xlim(np.max(xtrue)+10, np.min(xtrue)-10)
+# ax = fig.gca(projection='3d')
+# ax.scatter(x, y, z, c='r', marker='o', s =2.0*np.ones_like(x))
+# ax.quiver(x, y, z, unit_normals[:,0], unit_normals[:,1], unit_normals[:,2], length=50.0, normalize=True)
+plt.plot(positionsradii[:,0],positionsradii[:,2],'r')
+plt.scatter(xtrue, ztrue, c='b', marker='o', s = 16.0*np.ones_like(psamp[:,0]))
+plt.plot(xtrue[0], ztrue[0], 'g*')
+#plt.quiver(x, z, unit_normals[:,0], unit_normals[:,2], angles="xy", scale=4.0, scale_units="inches")
+plt.show()
+
+
+
+# print(vels.shape)
+# print(tparameterized.shape)
+
+
+
 
 
 
 jsonout = os.path.join(trackdir,os.path.splitext(trackfilein)[0] + ".json")
 jsondict : dict = {}
-jsondict["dist"] = X[:,0].tolist()
-jsondict["x"] = x.tolist()
-jsondict["y"] = y.tolist()
-jsondict["z"] = z.tolist()
-jsondict["x_tangent"] = x_tangent.tolist()
-jsondict["y_tangent"] = y_tangent.tolist()
-jsondict["z_tangent"] = z_tangent.tolist()
-jsondict["x_normal"] = x_normal.tolist()
-jsondict["y_normal"] = y_normal.tolist()
-jsondict["z_normal"] = z_normal.tolist()
+jsondict["dist"] = np.linspace(rsampradii[0], rsampradii[-1], num=nout).tolist()
+jsondict["t"] = tsamp.tolist()
+jsondict["x"] = xtrue.tolist()
+jsondict["y"] = ytrue.tolist()
+jsondict["z"] = ztrue.tolist()
+jsondict["vx"] = xdottrue.tolist()
+jsondict["vy"] = ydottrue.tolist()
+jsondict["vz"] = zdottrue.tolist()
+# jsondict["xvel"] = xdottrue.tolist()
+# jsondict["yvel"] = ydottrue.tolist()
+# jsondict["zvel"] = zdottrue.tolist()
+# jsondict["xaccel"] = xdotdottrue.tolist()
+# jsondict["yaccel"] = ydotdottrue.tolist()
+# jsondict["zaccel"] = zdotdottrue.tolist()
+# jsondict["x"] = x.tolist()
+# jsondict["y"] = y.tolist()
+# jsondict["z"] = z.tolist()
+# jsondict["x_tangent"] = x_tangent.tolist()
+# jsondict["y_tangent"] = y_tangent.tolist()
+# jsondict["z_tangent"] = z_tangent.tolist()
+# jsondict["x_normal"] = x_normal.tolist()
+# jsondict["y_normal"] = y_normal.tolist()
+# jsondict["z_normal"] = z_normal.tolist()
 with open(jsonout,"w") as f:
     json.dump( jsondict , f , indent=1 )
 print("First point: " + str(X[0,:]))
