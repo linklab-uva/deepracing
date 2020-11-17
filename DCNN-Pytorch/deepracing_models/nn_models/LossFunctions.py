5ee73a0cc94d9b2853929252d758710ab5f084ba
Trent Weiss <ttw2xk@virginia.edu>
2020-11-17 11:01:37 -0500

    transpose matmul result

--- DCNN-Pytorch/deepracing_models/nn_models/LossFunctions.py
+++ DCNN-Pytorch/deepracing_models/nn_models/LossFunctions.py
@@ -62,8 +62,8 @@ class BoundaryLoss(nn.Module):
     
     def forward(self, posesglobal, waypointslocal):
         posesinv = torch.inv(posesglobal)
-        boundarypointslocal = torch.matmul(posesinv, self.boundary)
-        boundarynormalslocal = torch.matmul(posesinv[:,0:3,0:3], self.boundarynormals)
+        boundarypointslocal = torch.matmul(posesinv, self.boundary)[0:3].transpose(0,1)
+        boundarynormalslocal = torch.matmul(posesinv[:,0:3,0:3], self.boundarynormals).transpose(0,1)
         _, dot_prods = signedDistances(waypointslocal, boundarypointslocal, boundarynormalslocal)
         if self.p is None:
             dot_prods_relu = self.relu(dot_prods)
