809a33c0b768b481c4fdd6e10c58539ae57ad8d3
Trent Weiss <ttw2xk@virginia.edu>
2021-06-18 15:09:49 -0400

    only numpy on time index for now.

--- DCNN-Pytorch/deepracing_models/data_loading/__init__.py
+++ DCNN-Pytorch/deepracing_models/data_loading/__init__.py
@@ -1,12 +1,10 @@
 import bisect
 import numpy as np
-import torch
-from typing import Union
 
 
 class TimeIndex:
-    def __init__(self, time_array : Union[np.ndarray, torch.Tensor], data_values : Union[np.ndarray, torch.Tensor]):
-        if torch.any(torch.as_tensor(time_array[1:]-time_array[:-1])<0):
+    def __init__(self, time_array : np.ndarray, data_values : np.ndarray):
+        if np.any((time_array[1:]-time_array[:-1])<0):
             raise ValueError("time_array must be everywhere non-decreasing")
         self.time_array = time_array
         self.data_values = data_values
@@ -17,7 +15,7 @@ class TimeIndex:
             raise ValueError("tmin (%f) cannot be outside range of time values [%f, %f]" %(tmin, self.time_array[0], self.time_array[-1]))
         if tmax > self.time_array[-1]:
             raise ValueError("tmax (%f) cannot be outside range of time values [%f, %f]" %(tmax, self.time_array[0], self.time_array[-1]))
-        leftbisect = max(bisect.bisect_left(self.data_values, tmin) - 1, 0)
-        rightbisect = min(bisect.bisect_right(self.data_values, tmax) + 1, self.time_array.shape[0]-1)
+        leftbisect = max(bisect.bisect_left(self.time_array, tmin) - 1, 0)
+        rightbisect = min(bisect.bisect_right(self.time_array, tmax) + 1, self.time_array.shape[0]-1)
 
         return (leftbisect, rightbisect),  self.data_values[leftbisect:rightbisect]
