7eb43b3815d370731f95137d676cc12fc9e3b4b5
Trent Weiss <ttw2xk@virginia.edu>
2021-07-20 15:26:01 -0400

    clean up some old files

--- deepracing_py/deepracing/evaluation_utils/__init__.py
+++ deepracing_py/deepracing/evaluation_utils/__init__.py
@@ -2,7 +2,8 @@ import numpy as np
 from shapely.geometry import Point as ShapelyPoint, MultiPoint#, Point2d as ShapelyPoint2d
 from shapely.geometry.polygon import Polygon
 from shapely.geometry import LinearRing
-
+import deepracing
+from tqdm import tqdm as tqdm
 def contiguous_regions(condition):
     """Finds contiguous True regions of the boolean array "condition". Returns
     a 2D array where the first column is the start index of the region and the
@@ -26,25 +27,37 @@ def contiguous_regions(condition):
 
     # Reshape the result into two columns
     return idx.reshape(-1,2).astype(np.int64)
-def lapMetrics(positions : np.ndarray, timestamps : np.ndarray, innerboundary_poly : Polygon, outerboundary_poly : Polygon):
-    inside_violations = np.array([innerboundary_poly.contains(ShapelyPoint(positions[i,0], positions[i,2])) for i in range(positions.shape[0])])
-    inner_contiguous_regions = contiguous_regions(inside_violations)
-    inside_violation_regions = np.column_stack([inner_contiguous_regions, -1*np.ones([inner_contiguous_regions.shape[0], 1], dtype=np.int64)])
-    outside_violations = np.array([not outerboundary_poly.contains(ShapelyPoint(positions[i,0], positions[i,2])) for i in range(positions.shape[0])])
-    outer_contiguous_regions = contiguous_regions(outside_violations)
-    outside_violation_regions = np.column_stack([outer_contiguous_regions, 1*np.ones([outer_contiguous_regions.shape[0], 1], dtype=np.int64)])
-    
-    all_violation_regions = np.row_stack([inside_violation_regions, outside_violation_regions])
+def lapMetrics(poses : np.ndarray, timestamps : np.ndarray, innerboundary_poly : Polygon, outerboundary_poly : Polygon, car_geom : deepracing.CarGeometry = deepracing.CarGeometry()):
+    wheel_positions_global = np.matmul(poses, car_geom.wheelPositions(dtype=poses.dtype))[:,0:3]
     delta_poly : Polygon = outerboundary_poly.difference(innerboundary_poly)
+    violations = np.zeros_like(poses[:,0,0], dtype=bool)
+    for i in tqdm(range(wheel_positions_global.shape[0]), desc="Checking for boundary violations", total=wheel_positions_global.shape[0]):
+        wheel_positions = wheel_positions_global[i]
+        wheel_distances = np.asarray([ delta_poly.distance(ShapelyPoint(wheel_positions[0, j], wheel_positions[2, j])) for j in range(4)])
+        wheels_on_track = np.asarray([ delta_poly.contains(ShapelyPoint(wheel_positions[0, j], wheel_positions[2, j])) for j in range(4)])
+        violations[i]=wheels_on_track.sum()<2
+            
 
-    I = np.argsort(all_violation_regions[:,0])
-    all_violation_regions = all_violation_regions[I]
+
+    # inside_violations = np.array([innerboundary_poly.contains(ShapelyPoint(positions[i,0], positions[i,2])) for i in range(positions.shape[0])])
+    # outside_violations = np.array([not outerboundary_poly.contains(ShapelyPoint(positions[i,0], positions[i,2])) for i in range(positions.shape[0])])
+
+    # inner_contiguous_regions = contiguous_regions(inside_violations)
+    # outer_contiguous_regions = contiguous_regions(outside_violations)
+    # outside_violation_regions = np.column_stack([outer_contiguous_regions, 1*np.ones([outer_contiguous_regions.shape[0], 1], dtype=np.int64)])
+    # inside_violation_regions = np.column_stack([inner_contiguous_regions, -1*np.ones([inner_contiguous_regions.shape[0], 1], dtype=np.int64)])
+    
+    all_violation_regions = contiguous_regions(violations)
+
+    # I = np.argsort(all_violation_regions[:,0])
+    # all_violation_regions = all_violation_regions[I]
 
     t0 = timestamps[0]
     tbf = []
     bfmaxdists = []
     bfmeandists = []
     bftimes = []
+    positions = poses[:,0:3,3]
     for i in range(all_violation_regions.shape[0]):
         region = all_violation_regions[i]
         tf = timestamps[region[0]]
