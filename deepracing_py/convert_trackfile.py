04a6cacae907de244d53699af1245952ac38fc69
Trent Weiss <ttw2xk@virginia.edu>
2021-08-21 18:42:48 -0400

    use PCA to project data onto a flat plane

--- deepracing_py/convert_trackfile.py
+++ deepracing_py/convert_trackfile.py
@@ -13,6 +13,8 @@ from deepracing.path_utils.optimization import OptimWrapper
 import deepracing.path_utils.geometric as geometric
 from shapely.geometry.polygon import Polygon
 from shapely.geometry import LinearRing
+import sklearn.decomposition
+import deepracing
 
 
 
@@ -23,8 +25,8 @@ parser.add_argument("ds", type=float, help="Sample the path at points this dista
 parser.add_argument("--maxiter", type=float, default=20, help="Maximum iterations to run the solver")
 parser.add_argument("--k", default=3, type=int, help="Degree of spline interpolation, ignored if num_samples is 0")
 parser.add_argument("--maxv", default=92.0, type=float, help="Max linear speed the car can have")
-parser.add_argument("--maxa", default=14.0, type=float, help="Max linear acceleration the car can have")
-parser.add_argument("--maxacent", default=25.0, type=float, help="Max centripetal acceleration the car can have")
+parser.add_argument("--maxa", default=20.0, type=float, help="Max linear acceleration the car can have")
+parser.add_argument("--maxacent", default=30.0, type=float, help="Max centripetal acceleration the car can have")
 parser.add_argument("--method", default="SLSQP", type=str, help="Optimization method to use")
 parser.add_argument("--out", default=None, type=str, help="Where to put the output file. Default is the same directory as the input .track file")
 #parser.add_argument("--negate_normals", action="store_true", help="Flip the sign all all of the computed normal vectors")
@@ -36,48 +38,77 @@ argdict = vars(args)
 # else:
 #     normalsign = 1.0
 trackfilein = os.path.abspath(argdict["trackfile"])
-isracingline = "racingline" in os.path.basename(trackfilein)
-isinnerboundary = "innerlimit" in os.path.basename(trackfilein)
-outerboundary = not (isracingline or isinnerboundary)
+trackname, identifier = str.split(os.path.splitext(os.path.basename(trackfilein))[0],"_")
 
+
+isracingline = identifier in {"racingline","minimumcurvature"} 
+isinnerboundary = "innerlimit" in os.path.basename(trackfilein)
+isouterboundary = not (isracingline or isinnerboundary)
 
 trackdir = os.path.abspath(os.path.dirname(trackfilein))
 print("trackdir: %s" %(trackdir,))
 ds = argdict["ds"]
 k = argdict["k"]
-trackin = np.loadtxt(trackfilein,delimiter=",",skiprows=2)
-
-I = np.argsort(trackin[:,0])
-track = trackin[I].copy()
-r = track[:,0].copy()
-
-
-
-Xin = np.zeros((track.shape[0],4))
-Xin[:,1] = track[:,1]
-Xin[:,2] = track[:,3]
-Xin[:,3] = track[:,2]
-Xin[:,0] = r
-Xin[:,0] = Xin[:,0] - Xin[0,0]
+if isracingline:
+    with open(deepracing.searchForFile(trackname+"_innerlimit.json", str.split(os.getenv("F1_TRACK_DIRS", os.pathsep))), "r") as f:
+        d = json.load(f)
+    innerboundary = np.column_stack([d[k] for k in ["x","y","z"]])
+    with open(deepracing.searchForFile(trackname+"_outerlimit.json", str.split(os.getenv("F1_TRACK_DIRS", os.pathsep))), "r") as f:
+        d = json.load(f)
+    outerboundary = np.column_stack([d[k] for k in ["x","y","z"]])
+    del d
+if str.lower(os.path.splitext(trackfilein)[1])==".csv":
+    trackin = np.loadtxt(trackfilein,delimiter=";")
+    trackin = trackin[:-1]
+    Xin = np.zeros((trackin.shape[0],4))
+    Xin[:,0] = trackin[:,0]
+    Xin[:,1] = trackin[:,1]
+    Xin[:,2] = 0.5*(np.mean(innerboundary[:,1]) + np.mean(outerboundary[:,1]))#np.zeros_like(trackin[:,1])
+    Xin[:,3] = trackin[:,2]
+    # x0 = np.square(0.75*np.asarray(trackin[:,-2]))
+    x0 = None
+    minimumcurvatureguess=True
+else:
+    trackin = np.loadtxt(trackfilein,delimiter=",",skiprows=2)
+    I = np.argsort(trackin[:,0])
+    track = trackin[I].copy()
+    r = track[:,0].copy()
+    Xin = np.zeros((track.shape[0],4))
+    Xin[:,1] = track[:,1]
+    # Xin[:,2] = np.mean(track[:,3])
+    Xin[:,2] = track[:,3]
+    Xin[:,3] = track[:,2]
+    Xin[:,0] = r
+    x0 = None
+    Xin[:,0] = Xin[:,0] - Xin[0,0]
+    minimumcurvatureguess=False
+if isracingline:
+    allpoints = np.concatenate([Xin[:,1:], innerboundary, outerboundary], axis=0)
+    pca = sklearn.decomposition.PCA(n_components=2)
+    pca.fit(allpoints)
+    Xin[:,1:] = pca.inverse_transform(pca.transform(Xin[:,1:]))
 final_vector = Xin[0,1:] - Xin[-1,1:]
 final_distance = np.linalg.norm(final_vector)
 print("initial final distance: %f" %(final_distance,))
 final_unit_vector = final_vector/final_distance
-# if final_distance>ds:
-#     extra_distance = final_distance-ds
-#     nstretch = 4
-#     rstretch =  np.linspace(extra_distance/final_distance, final_distance - ds, nstretch)
-#     # rstretch =  np.linspace(final_distance/nstretch,((nstretch-1)/nstretch)*final_distance,nstretch)
-#     final_stretch = np.row_stack([Xin[-1,1:] + rstretch[i]*final_unit_vector for i in range(rstretch.shape[0])])
-#     final_r =  rstretch + Xin[-1,0]
-#     Xin = np.row_stack((Xin, np.column_stack((final_r,final_stretch))))
-#     final_vector = Xin[0,1:] - Xin[-1,1:]
-#     final_distance = np.linalg.norm(final_vector)
+if final_distance>ds:
+    extra_distance = final_distance-ds
+    nstretch = 4
+    rstretch =  np.linspace(extra_distance/final_distance, final_distance - ds, nstretch)
+    # rstretch =  np.linspace(final_distance/nstretch,((nstretch-1)/nstretch)*final_distance,nstretch)
+    final_stretch = np.row_stack([Xin[-1,1:] + rstretch[i]*final_unit_vector for i in range(rstretch.shape[0])])
+    final_r =  rstretch + Xin[-1,0]
+    Xin = np.row_stack((Xin, np.column_stack((final_r,final_stretch))))
+    final_vector = Xin[0,1:] - Xin[-1,1:]
+    final_distance = np.linalg.norm(final_vector)
 print("final final distance: %f" %(final_distance,))
 
 fig1 = plt.figure()
 plt.plot(Xin[0,1], Xin[0,3], 'g*')
-plt.plot(Xin[:,1], Xin[:,3])
+plt.plot(Xin[:,1], Xin[:,3], c="blue")
+if isracingline:
+    plt.plot(innerboundary[:,0], innerboundary[:,2], c="black")
+    plt.plot(outerboundary[:,0], outerboundary[:,2], c="black")
 plt.title("Input Trackfile")
 plt.show()
 del fig1
@@ -97,6 +128,10 @@ else:
 rin = Xin[:,0].copy()
 rin = rin-rin[0]
 # rsamp = np.linspace(rin[0], rin[-1], num = int(round((Xin[-1,0]- Xin[0,0])/ds)))
+# if minimumcurvatureguess:
+#     rsamp = rin
+# else:
+#     rsamp = np.arange(rin[0], rin[-1], step = ds)
 rsamp = np.arange(rin[0], rin[-1], step = ds)
 
 
@@ -157,7 +192,11 @@ plt.scatter(xin, zin, c='b', marker='o', s = 16.0*np.ones_like(Xin[:,1]))
 # plt.scatter(x, z, c='r', marker='o', s = 4.0*np.ones_like(x))
 plt.plot(xsamp, zsamp, 'r')
 plt.plot(xsamp[0], zsamp[0], 'g*')
-plt.quiver(xsamp, zsamp, unit_normals[:,0], unit_normals[:,2], angles="xy", scale=4.0, scale_units="inches")
+if not isracingline:
+    plt.quiver(xsamp, zsamp, unit_normals[:,0], unit_normals[:,2], angles="xy", scale=4.0, scale_units="inches")
+else:
+    plt.plot(innerboundary[:,0], innerboundary[:,2], c="black")
+    plt.plot(outerboundary[:,0], outerboundary[:,2], c="black")
 try:
     plt.show()
 except:
@@ -170,7 +209,7 @@ if jsonout is None:
     jsonout = os.path.abspath(os.path.join(trackdir,os.path.splitext(os.path.basename(trackfilein))[0] + ".json"))
 
 print("jsonout: %s" %(jsonout,))
-if isinnerboundary or outerboundary:
+if isinnerboundary or isouterboundary:
     jsondict = dict()
     jsondict["rin"] = rin.tolist()
     jsondict["xin"] = xin.tolist()
@@ -192,13 +231,17 @@ print("Optimizing over a space of size: %d" %(rsamp.shape[0],), flush=True)
 
 
 radii = (tangentnorms**3)/(np.linalg.norm(np.cross(tangents, accels, axis=1), ord=2, axis=1) + 1E-6)
+maxcentripetalaccel = argdict["maxacent"]
+# if x0 is not None:
+#     violating=(x0/radii)>maxcentripetalaccel
+#     x0[violating]=radii[violating]*maxcentripetalaccel
 # radii[0:int(round(92.0/ds))] = np.inf
 # radii[-int(round(20.0/ds)):] = np.inf
 # radii[-2] = 0.5*(radii[-3] + radii[-1])
 #radii[-1] = 0.5*(radii[-2] + radii[0])
 # radii[0] = radii[1]
-idxrunup=-5
-radii[-5:] = np.linspace(radii[idxrunup], radii[0], num=5)#[1:]
+#idxrunup=-5
+#radii[-5:] = np.linspace(radii[idxrunup], radii[0], num=5)#[1:]
 
 
 rprint = 50
@@ -207,10 +250,9 @@ print("Final %d radii:\n%s" %(rprint, str(radii[-rprint:]),))
 
 print("Min radius: %f" % (np.min(radii)))
 print("Max radius: %f" % (np.max(radii)))
-
+print("radii.shape: %s", (str(radii.shape),))
 maxspeed = argdict["maxv"]
 maxlinearaccel = argdict["maxa"]
-maxcentripetalaccel = argdict["maxacent"]
 dsvec = np.array((rsamp[1:] - rsamp[:-1]).tolist() + [np.linalg.norm(Xsamp[-1] - Xsamp[0])])
 #dsvec[-int(round(40/ds)):] = np.inf
 print("Final %d delta s:\n%s" %(rprint, str(dsvec[-rprint:]),))
@@ -226,7 +268,7 @@ sqp = OptimWrapper(maxspeed, maxlinearaccel, maxcentripetalaccel, dsvec, radii)
 #method="trust-constr"
 method=argdict["method"]
 maxiter=argdict["maxiter"]
-x0, res = sqp.optimize(maxiter=maxiter,method=method,disp=True, keep_feasible=True)#,eps=100.0)
+x0, res = sqp.optimize(maxiter=maxiter,method=method,disp=True, keep_feasible=True, x0=x0)#,eps=100.0)
 print(vars(res), flush=True)
 v0 = np.sqrt(x0)
 velsquares = res.x
