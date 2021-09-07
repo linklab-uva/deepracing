deed6bc2039314cc7e6f5fa68b7d1cf46eba115c
Trent Weiss <ttw2xk@virginia.edu>
2021-09-07 17:19:35 -0400

    better script for getting src dir

--- addpythonpathsrc.bash
+++ addpythonpathsrc.bash
@@ -1,11 +1,19 @@
 #!/bin/bash
-scriptdir=$(dirname $(readlink -f "$0"))
-echo $scriptdir
-extrapythonpaths=$scriptdir/deepracing_py:$scriptdir/DCNN-Pytorch
+pushd . > /dev/null
+SCRIPT_PATH="${BASH_SOURCE[0]}";
+if ([ -h "${SCRIPT_PATH}" ]) then
+  while([ -h "${SCRIPT_PATH}" ]) do cd `dirname "$SCRIPT_PATH"`; SCRIPT_PATH=`readlink "${SCRIPT_PATH}"`; done
+fi
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+SCRIPT_PATH=`pwd`;
+popd  > /dev/null
+
+echo $SCRIPT_PATH
+extrapythonpaths=$SCRIPT_PATH/deepracing_py:$SCRIPT_PATH/DCNN-Pytorch
 echo $extrapythonpaths
 
-# if [[ -z "${PYTHONPATH}" ]]; then
-#   export PYTHONPATH=${extrapythonpaths}
-# else
-#   export PYTHONPATH=${PYTHONPATH}:${extrapythonpaths}
-# fi
+if [[ -z "${PYTHONPATH}" ]]; then
+  export PYTHONPATH=${extrapythonpaths}
+else
+  export PYTHONPATH=${PYTHONPATH}:${extrapythonpaths}
+fi
