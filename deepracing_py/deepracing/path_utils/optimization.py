816584bbf2967e22751e511a2dd8352d6e6f1690
Trent Weiss <ttw2xk@virginia.edu>
2022-04-29 13:59:34 -0400

    update braking constraints

--- deepracing_py/deepracing/path_utils/optimization.py
+++ deepracing_py/deepracing/path_utils/optimization.py
@@ -33,8 +33,8 @@ class BrakingConstraint():
         self.buffer = np.zeros_like(self.ds)
         # speeds = np.asarray([        0.0, 25.0, 30.0, 40.0, 60.0, 84.0, 150.0])
         # braking_limits = np.asarray([7.5, 7.5,  16.5, 21.0, 34.5, 40.0, 40.0])*factor
-        speeds : np.ndarray = np.flip(np.asarray([          125.0,  84.00,  46.00,  25.0,  0.00 ], dtype=ds.dtype))
-        braking_limits : np.ndarray  = np.flip(np.asarray([ 40.25 , 40.25 , 35.00,  17.5,  15.5 ], dtype=ds.dtype)*factor)
+        speeds : np.ndarray = np.flip(np.asarray([          125.0,  84.00,  46.00,  17.5,  0.00 ], dtype=ds.dtype))
+        braking_limits : np.ndarray  = np.flip(np.asarray([ 42.50 , 42.50 , 35.00,  18.0,  15.5 ], dtype=ds.dtype)*factor)
 
         self.braking_spline : scipy.interpolate.BSpline = scipy.interpolate.make_interp_spline(speeds, braking_limits, k=1)
         self.braking_spline_der : scipy.interpolate.BSpline = self.braking_spline.derivative()
