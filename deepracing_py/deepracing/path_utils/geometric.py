8a1ef61b2418d7fe2b7aa9bb462337cbf3251f53
Trent Weiss <ttw2xk@virginia.edu>
2022-06-15 16:13:43 -0400

    assume first and last point are the same point

--- deepracing_py/deepracing/path_utils/geometric.py
+++ deepracing_py/deepracing/path_utils/geometric.py
@@ -12,19 +12,19 @@ class SplineSpeedWrapper():
 
 def computeTangentsAndNormals(r, X, k = 3, ds = 1.0, ref = np.array([0.0,1.0,0.0], dtype=np.float64)):
     
-    finaldelta = np.linalg.norm(X[-1] - X[0], ord=2)
-    Xaug = np.concatenate([X, X[0].reshape(1,-1)], axis=0)
-    raug = np.concatenate([r, np.asarray([r[-1]+finaldelta])], axis=0)
-    raug = raug-raug[0]
-    garbagespline : interp.BSpline = interp.make_interp_spline(raug, Xaug, k=k, bc_type="periodic")
+    # finaldelta = np.linalg.norm(X[-1] - X[0], ord=2)
+    # Xaug = np.concatenate([X, X[0].reshape(1,-1)], axis=0)
+    # raug = np.concatenate([r, np.asarray([r[-1]+finaldelta])], axis=0)
+    # raug = raug-raug[0]
+    garbagespline : interp.BSpline = interp.make_interp_spline(r, X, k=k, bc_type="periodic")
     garbagesplineder : interp.BSpline = garbagespline.derivative()
-    truedistances : np.ndarray = np.zeros_like(raug)
+    truedistances : np.ndarray = np.zeros_like(r)
     for i in range(1, truedistances.shape[0]):
-        rsumsamp : np.ndarray = np.linspace(raug[i-1], raug[i], num=5)
+        rsumsamp : np.ndarray = np.linspace(r[i-1], r[i], num=5)
         velsubsamp : np.ndarray = np.linalg.norm(garbagesplineder(rsumsamp), ord=2, axis=1)
         truedistances[i] = truedistances[i-1] + integrate.simpson(velsubsamp, x=rsumsamp)
 
-    spline : interp.BSpline = interp.make_interp_spline(truedistances, Xaug, k=k, bc_type="periodic")
+    spline : interp.BSpline = interp.make_interp_spline(truedistances, X, k=k, bc_type="periodic")
     tangentspline : interp.BSpline = spline.derivative(nu=1)
     rsamp = np.linspace(truedistances[0], truedistances[-1]-ds, num = int(round(truedistances[-1]/ds)))
     points = spline(rsamp)
