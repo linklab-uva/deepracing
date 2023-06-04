bfde856d0bf75b67bd67c92b6f2cec6773347085
Trent Weiss <ttw2xk@virginia.edu>
2023-06-04 10:05:45 -0400

    header guard

--- data-logger/include/f1_datalogger/filesystem_helper.h
+++ data-logger/include/f1_datalogger/filesystem_helper.h
@@ -1,7 +1,10 @@
+#ifndef INCLUDE_F1_DATALOGGER_FILESYSTEM_HELPER_H_
+#define INCLUDE_F1_DATALOGGER_FILESYSTEM_HELPER_H_
 #ifdef BOOST_FILESYSTEM
   #include <boost/filesystem.hpp>
   namespace fs = boost::filesystem;
 #else
   #include <filesystem>
   namespace fs = std::filesystem;
+#endif
 #endif
