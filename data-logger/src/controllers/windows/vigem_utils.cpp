059664ed15190fd92a2085ac5100293bfec50a07
Trent Weiss <ttw2xk@virginia.edu>
2023-06-05 14:40:04 -0400

    add nan checks

--- data-logger/src/controllers/windows/vigem_utils.cpp
+++ data-logger/src/controllers/windows/vigem_utils.cpp
@@ -13,23 +13,33 @@ XINPUT_GAMEPAD toXinput(const F1ControlCommand& f1_interface_command)
 }
 void toXinputInplace(const F1ControlCommand& f1_interface_command, XINPUT_GAMEPAD& rtn)
 {
-    //short goes from -32,768 to 32,767
-    //but left (negative) on the joystick is steering to the left, which is a positive steering angle in our convention.
-    double steering = std::clamp<double>(f1_interface_command.steering, -1.0, 1.0);
-    if (steering>0.0)
-    {
-        rtn.sThumbLX = (SHORT)std::round(-32768.0*steering);
-    }
-    else
-    {
-        rtn.sThumbLX = (SHORT)std::round(-32767.0*steering);
-    }
     rtn.sThumbLY=rtn.sThumbRX=rtn.sThumbRY=0;
-    //BYTE (unsigned char) goes from 0 to 255
-    double throttle = std::clamp<double>(f1_interface_command.throttle, -1.0, 1.0);
-    double brake = std::clamp<double>(f1_interface_command.brake, -1.0, 1.0);
-    rtn.bRightTrigger = (BYTE)std::round(255.0*throttle);
-    rtn.bLeftTrigger = (BYTE)std::round(255.0*brake);
+    //Don't update values that are NaN
+    if(!std::isnan<double>(f1_interface_command.steering))
+    {
+        //short goes from -32,768 to 32,767
+        //but left (negative) on the joystick is steering to the left, which is a positive steering angle in our convention.
+        double steering = std::clamp<double>(f1_interface_command.steering, -1.0, 1.0);
+        if (steering>0.0)
+        {
+            rtn.sThumbLX = (SHORT)std::round(-32768.0*steering);
+        }
+        else
+        {
+            rtn.sThumbLX = (SHORT)std::round(-32767.0*steering);
+        }
+    }
+    if(!std::isnan<double>(f1_interface_command.throttle))
+    {
+        //BYTE (unsigned char) goes from 0 to 255
+        double throttle = std::clamp<double>(f1_interface_command.throttle, -1.0, 1.0);
+        rtn.bRightTrigger = (BYTE)std::round(255.0*throttle);
+    }
+    if(!std::isnan<double>(f1_interface_command.brake))
+    {
+        double brake = std::clamp<double>(f1_interface_command.brake, -1.0, 1.0);
+        rtn.bLeftTrigger = (BYTE)std::round(255.0*brake);
+    }
 }
 
 }
