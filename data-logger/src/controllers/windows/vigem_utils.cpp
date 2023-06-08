1f9d71c4b447b77bf5146fa8da853d79fac0c379
Trent Weiss <ttw2xk@virginia.edu>
2023-06-07 22:15:34 -0400

    just linear for now.

--- data-logger/src/controllers/windows/vigem_utils.cpp
+++ data-logger/src/controllers/windows/vigem_utils.cpp
@@ -1,6 +1,9 @@
 #include <f1_datalogger/controllers/internal/vigem_utils.h>
 #include <algorithm>
 #include <cmath>
+#include <cstdlib>
+#include <cstddef>
+#include <iostream>
 
 namespace deepf1
 {
@@ -19,15 +22,35 @@ void toXinputInplace(const F1ControlCommand& f1_interface_command, XINPUT_GAMEPA
     {
         //short goes from -32,768 to 32,767
         //but left (negative) on the joystick is steering to the left, which is a positive steering angle in our convention.
-        double steering = std::clamp<double>(f1_interface_command.steering, -1.0, 1.0);
-        if (steering>0.0)
-        {
-            rtn.sThumbLX = (SHORT)std::round(-32768.0*steering);
-        }
-        else
-        {
-            rtn.sThumbLX = (SHORT)std::round(-32767.0*steering);
-        }
+        double xratio = f1_interface_command.steering;
+        double xratioabs = std::abs(xratio);
+        rtn.sThumbLX = std::round(-32767.0*xratio);
+        rtn.sThumbLY = 0;
+        // double xangle = xratio*M_PI_2;
+
+        // double sinxangle = std::sin(xangle);
+        // double sinxangleabs = std::abs(sinxangle);
+
+        // double asin = std::asin(xratio);
+        // if (xratio>0.0)
+        // {
+        //     // rtn.sThumbLX = (SHORT)std::round(-32765.0*(asin/M_PI_2));
+        // }
+        // else if(xratio<0.0)
+        // {
+        //     // rtn.sThumbLX = (SHORT)std::round(32765.0*(-asin/M_PI_2));
+        //     // rtn.sThumbLX = std::round(32767.0*sinxangleabs);
+        //     rtn.sThumbLX = std::round(-32760.0*xratio);
+        // }
+        // else
+        // {
+        //     rtn.sThumbLX = 0;
+        // }
+        // double yratio = std::sqrt(1.0-(xratio*xratio));
+        // double yangle = yratio*M_PI_2;
+        // double sinyangle = std::sin(yangle);
+        // rtn.sThumbLY = std::round(32765.0*sinyangle);
+        // std::cerr<<"rtn.sThumbLX: " << rtn.sThumbLX << std::endl;
     }
     if(!std::isnan<double>(f1_interface_command.throttle))
     {
