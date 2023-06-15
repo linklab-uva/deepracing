f14ac922ce868f3e8474ca0c7bbdfd10faf3ef54
Trent Weiss <ttw2xk@virginia.edu>
2023-06-15 15:46:57 -0400

    allow specifying viewpoint pose

--- deepracing_py/deepracing/path_utils/__init__.py
+++ deepracing_py/deepracing/path_utils/__init__.py
@@ -1,6 +1,10 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
 
-def numpyToPCD(x : np.ndarray, points : np.ndarray, filepath : str, x_name : str = "time"):
+def numpyToPCD(x : np.ndarray, points : np.ndarray, filepath : str, 
+               x_name : str = "time", viewpoint_pos : np.ndarray = np.zeros(3), viewpoint_rot : Rotation = Rotation.identity()):
+
+    viewpoint_pose = np.concatenate([viewpoint_rot.as_quat(), viewpoint_pos])
     with open(filepath, "w") as f:
         headerlines = ["VERSION 0.7\n", 
                        "FIELDS x y z %s\n" % (x_name,), 
@@ -9,7 +13,7 @@ def numpyToPCD(x : np.ndarray, points : np.ndarray, filepath : str, x_name : str
                        "COUNT 1 1 1 1\n", 
                        "WIDTH %d\n" % (points.shape[0],),
                        "HEIGHT 1\n",
-                       "VIEWPOINT 0 0 0 1 0 0 0\n",
+                       "VIEWPOINT %.3f %.3f %.3f %.3f %.3f %.3f %.3f\n" % tuple(viewpoint_pose.tolist()),
                        "POINTS %d\n" % (points.shape[0],),
                        "DATA ascii\n"]
         f.writelines(headerlines)
