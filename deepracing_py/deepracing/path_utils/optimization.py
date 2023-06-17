759947f4ff1d19f202887017bb1ee98f09be95b9
Trent Weiss <ttw2xk@virginia.edu>
2023-06-17 08:28:35 -0400

    force non-zero slope

--- deepracing_py/deepracing/path_utils/optimization.py
+++ deepracing_py/deepracing/path_utils/optimization.py
@@ -68,7 +68,7 @@ class LinearAccelConstraint():
         self.linearaccelmat=generate_linear_accel_mat(ds)
         self.buffer = np.zeros_like(self.ds)
         speeds = np.asarray([              0.00,  36.0,  91.5,  150.0])
-        forward_accel_limits = np.asarray([17.5,  17.5,  0.00,  0.000])*factor
+        forward_accel_limits = np.asarray([17.5,  17.25,  0.00,  0.000])*factor
         self.forward_accel_spline : scipy.interpolate.BSpline = scipy.interpolate.make_interp_spline(speeds, forward_accel_limits, k=1)
         self.forward_accel_spline_der : scipy.interpolate.BSpline = self.forward_accel_spline.derivative()
      #   print(self.linearaccelmat.toarray()[[0,1,2,3,-4,-3,-2,-1]], flush=True)
