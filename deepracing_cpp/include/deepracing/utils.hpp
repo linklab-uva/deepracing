7fe648acf4eb736c4e3d7754f6f99cd8d494ee82
Trent Weiss <ttw2xk@virginia.edu>
2023-07-13 19:15:26 -0400

    add track id to track name map to deepracing_cpp

---
+++ deepracing_cpp/include/deepracing/utils.hpp
@@ -0,0 +1,13 @@
+#ifndef DEEPRACING_CPP__UTILS_H_
+#define DEEPRACING_CPP__UTILS_H_
+#include <map>
+#include <string>
+
+namespace deepracing
+{
+    inline std::map<std::size_t, std::string> trackNames();
+}
+
+
+
+#endif
---
+++ deepracing_cpp/src/utils.cpp
@@ -0,0 +1,41 @@
+#include <deepracing/utils.hpp>
+std::map<std::size_t, std::string> deepracing::trackNames()
+{
+    std::map<std::size_t, std::string> rtn;
+    rtn[0] = "Australia";
+    rtn[1] = "Canada";
+    rtn[2] = "China";
+    rtn[3] = "Bahrain";
+    rtn[4] = "Catalunya";
+    rtn[5] = "Monaco";
+    rtn[6] = "Montreal";
+    rtn[7] = "Britain";
+    rtn[8] = "Hockenheim";
+    rtn[9] = "Hungaroring";
+    rtn[10] = "Spa";
+    rtn[11] = "Monza";
+    rtn[12] = "Singapore";
+    rtn[13] = "Suzuka";
+    rtn[14] = "AbuDhabi";
+    rtn[15] = "TexasF1";
+    rtn[16] = "Brazil";
+    rtn[17] = "Austria";
+    rtn[18] = "Sochi";
+    rtn[19] = "Mexico";
+    rtn[20] = "Azerbaijan";
+    rtn[21] = "Sakhir Short";
+    rtn[22] = "Silverstone Short";
+    rtn[23] = "Texas Short";
+    rtn[24] = "Suzuka Short";
+    rtn[25] = "Hanoi";
+    rtn[26] = "Zandvoort";
+    rtn[27] = "Imola";
+    rtn[28] = "Portimao";
+    rtn[29] = "Jeddah";
+    rtn[30] = "Miami";
+    rtn[31] = "VegasF1";
+    rtn[32] = "Losail";
+    rtn[33] = "VegasIndycar";
+    rtn[34] = "TexasIndycar";
+    return rtn;
+}
