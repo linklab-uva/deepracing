3073c198e86d2575bb3ff44322918d3c295437c4
Trent Weiss <ttw2xk@virginia.edu>
2023-08-11 18:36:01 -0400

    use pybind11::bytes to do transcription

--- deepracing_cpp/waymo_tensorflow/py_waymo_conversions.cpp
+++ deepracing_cpp/waymo_tensorflow/py_waymo_conversions.cpp
@@ -1,16 +1,22 @@
 #include <pybind11/pybind11.h>
 #include <waymo_open_dataset/data_conversion/scenario_conversion.h>
+#include <google/protobuf/util/json_util.h>
 
-pybind11::bytes scenario_to_tfexample(pybind11::bytes bytesin) {
+pybind11::bytes scenario_to_tfexample(const pybind11::bytes& scenario_bytes) {
     waymo::open_dataset::Scenario scenario;
-    scenario.ParseFromString(bytesin);
+    scenario.ParseFromString(scenario_bytes);
+    // google::protobuf::util::JsonStringToMessage(scenario_json, &scenario);
+    // std::string scenario_json;
+    // google::protobuf::util::MessageToJsonString(scenario, &scenario_json);
+    // std::cout<<scenario_json<<std::endl;
     waymo::open_dataset::MotionExampleConversionConfig config;
     std::map<std::string,int> counters;
     absl::StatusOr<tensorflow::Example> converted_ptr = waymo::open_dataset::ScenarioToExample(scenario, config, &counters);
     const tensorflow::Example& converted = *converted_ptr;
     std::string out;
+    // google::protobuf::util::MessageToJsonString(converted, &out);
     converted.SerializeToString(&out);
-    return pybind11::bytes(out.c_str());
+    return pybind11::bytes(out);
 }
 
 PYBIND11_MODULE(py_waymo_conversions, m) {
