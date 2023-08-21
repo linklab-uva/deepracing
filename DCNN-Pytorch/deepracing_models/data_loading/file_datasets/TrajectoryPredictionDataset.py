a723bcf82ae77847db491f7c20023c12826b04cc
Trent Weiss <ttw2xk@virginia.edu>
2023-08-21 10:20:16 -0400

    greatly simplify dataset loading

--- DCNN-Pytorch/deepracing_models/data_loading/file_datasets/TrajectoryPredictionDataset.py
+++ DCNN-Pytorch/deepracing_models/data_loading/file_datasets/TrajectoryPredictionDataset.py
@@ -2,158 +2,78 @@
 import torch
 import torch.utils
 import torch.utils.data
-import numpy as np
-import typing
+import numpy as np, numpy.lib.npyio as npio
 import deepracing_models
 import deepracing_models.math_utils
-from deepracing_models.math_utils import SimplePathHelper
-from deepracing.path_utils.smooth_path_helper import SmoothPathHelper
+import deepracing_models.data_loading
 from tqdm import tqdm
 import torch.jit
+import os
+import yaml
 
 class TrajectoryPredictionDataset(torch.utils.data.Dataset):
-    def __init__(self,\
-                 target_vehicle_positions : np.ndarray,\
-                 target_vehicle_velocities : np.ndarray,\
-                 inner_boundary_helper : SmoothPathHelper,\
-                 inner_boundary_corresponding_arclengths : np.ndarray,\
-                 outer_boundary_helper : SmoothPathHelper,\
-                 outer_boundary_corresponding_arclengths : np.ndarray,\
-                 centerline_helper : SmoothPathHelper,\
-                 centerline_corresponding_arclengths : np.ndarray,\
-                 raceline_helper : SmoothPathHelper,\
-                 raceline_corresponding_arclengths : np.ndarray,\
-                 metadata : dict,
-                 ego_vehicle_positions : np.ndarray = None,\
-                 ego_vehicle_velocities : np.ndarray = None,\
-                 ego_vehicle_accelerations : typing.Union[np.ndarray,None] = None,\
-                 target_vehicle_accelerations : typing.Union[np.ndarray,None] = None,\
-                 orient_to_inner_boundary : bool = False):
-        self.metadata = metadata.copy()
-        self.target_vehicle_positions : np.ndarray = target_vehicle_positions.copy()
-        self.target_vehicle_velocities : np.ndarray = target_vehicle_velocities.copy()
-        self.inner_boundary_helper : SmoothPathHelper = inner_boundary_helper
-        self.outer_boundary_helper : SmoothPathHelper = outer_boundary_helper
-        self.centerline_helper : SmoothPathHelper = centerline_helper
-        self.raceline_helper : SmoothPathHelper = raceline_helper
-        self.inner_boundary_corresponding_arclengths = inner_boundary_corresponding_arclengths.copy()
-        self.outer_boundary_corresponding_arclengths = outer_boundary_corresponding_arclengths.copy()
-        self.centerline_corresponding_arclengths = centerline_corresponding_arclengths.copy()
-        self.raceline_corresponding_arclengths = raceline_corresponding_arclengths.copy()
-        # if (ego_vehicle_accelerations is None) and (target_vehicle_accelerations is not None):
-        #     raise ValueError("target_vehicle_accelerations was provided, but not ego_vehicle_accelerations. Must provide either both or neither")
-        # if (target_vehicle_accelerations is None) and (ego_vehicle_accelerations is not None):
-        #     raise ValueError("ego_vehicle_accelerations was provided, but not target_vehicle_accelerations. Must provide either both or neither")
-        if (target_vehicle_accelerations is not None):
-            self.target_vehicle_accelerations : np.ndarray = target_vehicle_accelerations.copy()
+    def __init__(self, metadatafile : str, subset_flag : deepracing_models.data_loading.SubsetFlag,\
+                  kbezier=3, dtype=torch.float64, device=torch.device("cpu")):
+        with open(metadatafile, "r") as f:
+            self.metadata = yaml.load(f, Loader=yaml.SafeLoader)
+        directory : str = os.path.dirname(metadatafile)
+        if subset_flag == deepracing_models.data_loading.SubsetFlag.TRAIN:
+            npfile : str = os.path.join(directory, self.metadata["train_data"])
+            self.len = self.metadata["num_train_samples"]
+        elif subset_flag == deepracing_models.data_loading.SubsetFlag.VAL:
+            npfile : str = os.path.join(directory, self.metadata["val_data"])
+            self.len = self.metadata["num_val_samples"]
+        elif subset_flag == deepracing_models.data_loading.SubsetFlag.TEST:
+            npfile : str = os.path.join(directory, self.metadata["test_data"])
+            self.len = self.metadata["num_test_samples"]
+        elif subset_flag == deepracing_models.data_loading.SubsetFlag.ALL:
+            npfile : str = os.path.join(directory, self.metadata["all_data"])
+            self.len = self.metadata["num_samples"]
         else:
-            self.target_vehicle_accelerations = None
-        if (ego_vehicle_accelerations is not None):
-            self.ego_vehicle_accelerations : np.ndarray = ego_vehicle_accelerations.copy()
-        else:
-            self.ego_vehicle_accelerations = None
+            raise ValueError("Invalid subset_flag: %d" % (subset_flag,))
+
+        self.data_dict : dict[str, torch.Tensor] = dict()
+        with open(npfile, "rb") as f:
+            npdict : npio.NpzFile = np.load(f)
+            for k in npdict.keys():
+                arr : np.ndarray = npdict[k]
+                if not arr.shape[0] == self.len:
+                    raise ValueError("Data array %s has length %d not consistent with metadata length %d" % (k, arr.shape[0], self.len))
+                self.data_dict[k] = torch.as_tensor(arr.copy(), dtype=dtype, device=device)
+        
+        self.data_dict["reference_curves"] = torch.empty([self.len, 4, kbezier+1, 3], dtype=dtype, device=device)
+
+        for i in tqdm(range(self.len), desc="Fitting bezier curves"):
+
+            left_bd_r : torch.Tensor = self.data_dict["future_left_bd_arclength"][i]
+            left_bd_s = (left_bd_r - left_bd_r[0])/(left_bd_r[-1] - left_bd_r[0])
+            self.data_dict["future_left_bd_arclength"][i] = left_bd_r - left_bd_r[0]
+
+            right_bd_r : torch.Tensor = self.data_dict["future_right_bd_arclength"][i]
+            right_bd_s = (right_bd_r - right_bd_r[0])/(right_bd_r[-1] - right_bd_r[0])
+            self.data_dict["future_right_bd_arclength"][i] = right_bd_r - right_bd_r[0]
+
+            centerline_r : torch.Tensor = self.data_dict["future_centerline_arclength"][i]
+            centerline_s = (centerline_r - centerline_r[0])/(centerline_r[-1] - centerline_r[0])
+            self.data_dict["future_centerline_arclength"][i] = centerline_r - centerline_r[0]
+
+            raceline_r : torch.Tensor = self.data_dict["future_raceline_arclength"][i]
+            raceline_s = (raceline_r - raceline_r[0])/(raceline_r[-1] - raceline_r[0])
+            self.data_dict["future_raceline_arclength"][i] = raceline_r - raceline_r[0]
 
 
-        if (ego_vehicle_velocities is None) and (ego_vehicle_positions is not None):
-            raise ValueError("ego_vehicle_positions was provided, but not ego_vehicle_velocities. Must provide either both or neither")
-        if (ego_vehicle_positions is None) and (ego_vehicle_velocities is not None):
-            raise ValueError("ego_vehicle_velocities was provided, but not ego_vehicle_positions. Must provide either both or neither")
-        if (ego_vehicle_positions is not None) and (ego_vehicle_velocities is not None):
-            self.ego_vehicle_positions : np.ndarray = ego_vehicle_positions.copy()
-            self.ego_vehicle_velocities : np.ndarray = ego_vehicle_velocities.copy()
-        else:
-            self.ego_vehicle_positions = None
-            self.ego_vehicle_velocities = None
-        self.orient_to_inner_boundary = orient_to_inner_boundary
-        rforward = metadata["rforward"]
-        tforward = metadata["predictiontime"]
-        self.rdelta : np.ndarray = np.linspace(0.0, rforward, num=21, dtype=target_vehicle_positions.dtype)      
-        self.tdelta_label : np.ndarray = np.linspace(0.0, tforward, num=51, dtype=target_vehicle_positions.dtype)      
+            left_bd : torch.Tensor = self.data_dict["future_left_bd"][i]
+            right_bd : torch.Tensor = self.data_dict["future_right_bd"][i]
+            centerline : torch.Tensor = self.data_dict["future_centerline"][i]
+            raceline : torch.Tensor = self.data_dict["future_raceline"][i]
+            all_lines : torch.Tensor = torch.stack([left_bd, right_bd, centerline, raceline], dim=0)
+
+            all_s : torch.Tensor = torch.stack([left_bd_s, right_bd_s, centerline_s, raceline_s], dim=0)
+ 
+            self.data_dict["reference_curves"][i] = deepracing_models.math_utils.bezierLsqfit(all_lines, kbezier, t=all_s)[1]
+
     def __len__(self):
-        return self.target_vehicle_positions.shape[0]-100
+        return self.len
     def __getitem__(self, index):
-        idxnow = index + 50
-        ihistorystart = idxnow - 30
-        ihistoryend = idxnow + 1
-        ifutureend = idxnow + 51
-
-        target_position_history : np.ndarray = self.target_vehicle_positions[ihistorystart:ihistoryend]
-        target_velocity_history : np.ndarray = self.target_vehicle_velocities[ihistorystart:ihistoryend]
-        target_positions_future : np.ndarray = self.target_vehicle_positions[idxnow:ifutureend]
-        target_velocity_future : np.ndarray = self.target_vehicle_velocities[idxnow:ifutureend]
-        p0 : np.ndarray = target_positions_future[0]
-
-        r_ib : float = float(self.inner_boundary_corresponding_arclengths[idxnow])
-        r_ob : float = float(self.outer_boundary_corresponding_arclengths[idxnow])
-        r_cl : float = float(self.centerline_corresponding_arclengths[idxnow])
-        r_ol : float = float(self.raceline_corresponding_arclengths[idxnow])
-
-        inner_boundary_input : np.ndarray = self.inner_boundary_helper.point(self.rdelta + r_ib)
-        outer_boundary_input : np.ndarray = self.outer_boundary_helper.point(self.rdelta + r_ob)
-
-        # t_ib : np.ndarray = self.inner_boundary_helper.t_of_r(r_ib)
-        # t_ob : np.ndarray = self.outer_boundary_helper.t_of_r(r_ob)
-        # t_cl : np.ndarray = self.centerline_helper.t_of_r(r_cl)
-        # t_ol : np.ndarray = self.raceline_helper.t_of_r(r_ol)
-
-
-
-        # outer_boundary_label : np.ndarray = self.outer_boundary_helper.spline_time(self.tdelta_label + t_ob)
-        # inner_boundary_label : np.ndarray = self.inner_boundary_helper.spline_time(self.tdelta_label + t_ib)
-        # centerline_label : np.ndarray = self.centerline_helper.spline_time(self.tdelta_label + t_cl)
-        # raceline_label : np.ndarray = self.raceline_helper.spline_time(self.tdelta_label + t_ol)
-
-
-        outer_boundary_label : np.ndarray = self.outer_boundary_helper.point(self.outer_boundary_corresponding_arclengths[idxnow:ifutureend])
-        inner_boundary_label : np.ndarray = self.inner_boundary_helper.point(self.inner_boundary_corresponding_arclengths[idxnow:ifutureend])
-        centerline_label : np.ndarray = self.centerline_helper.point(self.centerline_corresponding_arclengths[idxnow:ifutureend])
-        raceline_label : np.ndarray = self.raceline_helper.point(self.raceline_corresponding_arclengths[idxnow:ifutureend])
-        
-        
-        if self.orient_to_inner_boundary:
-            tangent0 : np.ndarray = self.inner_boundary_helper.direction(r_ib)
-        else:    
-            vel0 : np.ndarray = target_velocity_history[-1]
-            tangent0 : np.ndarray = vel0/np.linalg.norm(vel0, ord=2, axis=0)
-
-        rotmat : np.ndarray = np.stack([tangent0, tangent0[[1,0]]*np.asarray([-1.0, 1.0], dtype=tangent0.dtype)], axis=1)
-
-        rotmatinv : np.ndarray = np.transpose(rotmat)
-        translationinv : np.ndarray = rotmatinv @ -p0
-
-
-        outdict : dict = dict()
-        outdict["target_position_history"] = (rotmatinv @ target_position_history.T).T + translationinv
-        outdict["target_position_future"] = (rotmatinv @ target_positions_future.T).T + translationinv
-        outdict["target_velocity_history"] = (rotmatinv @ target_velocity_history.T).T
-        outdict["target_velocity_future"] = (rotmatinv @ target_velocity_future.T).T
-        outdict["target_speed_future"] = np.linalg.norm(outdict["target_velocity_future"], ord=2, axis=1)
-        if self.ego_vehicle_positions is not None:
-            ego_position_history : np.ndarray = self.ego_vehicle_positions[ihistorystart:ihistoryend]
-            ego_velocity_history : np.ndarray = self.ego_vehicle_velocities[ihistorystart:ihistoryend]
-            outdict["ego_position_history"] = (rotmatinv @ ego_position_history.T).T + translationinv
-            outdict["ego_velocity_history"] = (rotmatinv @ ego_velocity_history.T).T
-        outdict["right_bound_input"] = (rotmatinv @ inner_boundary_input.T).T + translationinv
-        outdict["left_bound_input"] = (rotmatinv @ outer_boundary_input.T).T + translationinv
-        outdict["raceline_label"] = (rotmatinv @ raceline_label.T).T + translationinv
-        outdict["centerline_label"] = (rotmatinv @ centerline_label.T).T + translationinv
-        outdict["right_bound_label"] = (rotmatinv @ inner_boundary_label.T).T + translationinv
-        outdict["left_bound_label"] = (rotmatinv @ outer_boundary_label.T).T + translationinv
-        if self.target_vehicle_accelerations is not None:
-            # ego_acceleration_history : np.ndarray = self.ego_vehicle_accelerations[ihistorystart:ihistoryend]
-            # outdict["ego_acceleration_history"] = (rotmatinv @ ego_acceleration_history.T).T
-            target_acceleration_history : np.ndarray = self.target_vehicle_accelerations[ihistorystart:ihistoryend]
-            target_acceleration_future : np.ndarray = self.target_vehicle_accelerations[idxnow:ifutureend]
-            outdict["target_acceleration_history"] = (rotmatinv @ target_acceleration_history.T).T
-            outdict["target_acceleration_future"] = (rotmatinv @ target_acceleration_future.T).T
-        if self.ego_vehicle_accelerations is not None:
-            # ego_acceleration_history : np.ndarray = self.ego_vehicle_accelerations[ihistorystart:ihistoryend]
-            # outdict["ego_acceleration_history"] = (rotmatinv @ ego_acceleration_history.T).T
-            ego_acceleration_history : np.ndarray = self.ego_vehicle_accelerations[ihistorystart:ihistoryend]
-            ego_acceleration_future : np.ndarray = self.ego_vehicle_accelerations[idxnow:ifutureend]
-            outdict["ego_acceleration_history"] = (rotmatinv @ ego_acceleration_history.T).T
-            outdict["ego_acceleration_future"] = (rotmatinv @ ego_acceleration_future.T).T
-        # outdict["trackname"]=self.trackname
-        return outdict
-        
+        return {k : self.data_dict[k][index] for k in self.data_dict.keys()}
         
