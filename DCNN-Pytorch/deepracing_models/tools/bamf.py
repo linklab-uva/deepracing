48ffc25fb83e273fbe12af83eda308054f9ba818
Trent Weiss <ttw2xk@virginia.edu>
2023-10-29 11:59:56 -0400

    ready for first go

--- DCNN-Pytorch/deepracing_models/tools/bamf.py
+++ DCNN-Pytorch/deepracing_models/tools/bamf.py
@@ -11,7 +11,6 @@ import traceback
 import sys
 import deepracing_models.math_utils
 import tqdm
-comet_experiment : comet_ml.Experiment | None = None
 # t = torch.linspace(0.0, 3.0, steps=num_segments+1).to(device=device, dtype=dtype).unsqueeze(0).expand(batchdim, num_segments+1)
 # tstart = t[:,:-1]
 # dt = t[:,1:] - tstart
@@ -39,28 +38,38 @@ comet_experiment : comet_ml.Experiment | None = None
 # print(poscurveout.shape)
 
 def errorcb(exception):
-    global comet_experiment
     for elem in traceback.format_exception(exception):
         print(elem, flush=True, file=sys.stderr)
 
 def train(config : dict = None, tempdir : str = None, num_epochs : int = 200, 
-          workers : int = 0, api_key=None, dtype : np.dtype | None = None, gpu : int = -1):
-    global comet_experiment
+          workers : int = 0, comet_experiment : comet_ml.Experiment | None = None, gpu : int = -1):
 
+    if config is None:
+        raise ValueError("keyword arg \"config\" is mandatory")
     if tempdir is None:
         raise ValueError("keyword arg \"tempdir\" is mandatory")
-    num_segments = 10
-    kbezier = 3
+    if comet_experiment is not None:
+        tempdir = os.path.join(tempdir, comet_experiment.get_name())
+        os.makedirs(tempdir)
+    netconfig = config["network"]
+    num_segments = netconfig["num_segments"]
+    kbezier = netconfig["kbezier"]
+    with_batchnorm = netconfig["with_batchnorm"]
     network : BAMF = BAMF( history_dimension = 6,
             num_segments = num_segments, 
-            kbezier = kbezier
-        ).float().train()
+            kbezier = kbezier,
+            with_batchnorm = with_batchnorm
+        ).train()
     if gpu>0:
         network = network.cuda(gpu)
+    trainerconfig = config["trainer"]
+    if trainerconfig["float32"]:
+        network = network.float()
+    else:
+        network = network.double()
     firstparam = next(network.parameters())
     device = firstparam.device
     dtype = firstparam.dtype
-    trainerconfig = config["trainer"]
     lr = float(trainerconfig["learning_rate"])
     betas = tuple(trainerconfig["betas"])
     weight_decay = trainerconfig["weight_decay"]
@@ -71,29 +80,33 @@ def train(config : dict = None, tempdir : str = None, num_epochs : int = 200,
     search_dirs = dataconfig["dirs"]
     datasets : list[FD.TrajectoryPredictionDataset] = []
     for search_dir in search_dirs:
-        datasets += load_datasets_from_files(search_dir, dtype=np.asarray(torch.ones(1, dtype=dtype).numpy()).dtype)
+        datasets += load_datasets_from_files(search_dir, dtype=np.float64)
     concat_dataset : torchdata.ConcatDataset = torchdata.ConcatDataset(datasets)
     dataloader : torchdata.DataLoader = torchdata.DataLoader(concat_dataset, batch_size=64, pin_memory=True, shuffle=True, num_workers=workers)
-    dataloader_enumerate = enumerate(dataloader)
-    Nhistory = datasets[0].metadata["numsamples_history"]
     Nfuture = datasets[0].metadata["numsamples_prediction"]
-    thistory = datasets[0].metadata["historytime"]
     tfuture = datasets[0].metadata["predictiontime"]
     tsegs : torch.Tensor = torch.linspace(0.0, tfuture, steps=num_segments+1, device=device, dtype=dtype)
     tstart_ = tsegs[:-1]
     dt_ = tsegs[1:] - tstart_
     tsamp_ : torch.Tensor = torch.linspace(0.0, tfuture, steps=Nfuture, device=device, dtype=dtype)
-    tq : tqdm.tqdm | enumerate = tqdm.tqdm(dataloader_enumerate, desc="Yay")
     coordinate_idx_history = [0,1]
     quaternion_idx_history = [2,3]
-    for epoch in range(200):
+    for epoch in range(1, num_epochs+1):
+        dataloader_enumerate = enumerate(dataloader)
+        if comet_experiment is None:
+            tq : tqdm.tqdm  = tqdm.tqdm(dataloader_enumerate, desc="Yay")
+        else:
+            tq : enumerate  = dataloader_enumerate
+        print("Running epoch %d" % (epoch,), flush=True)
+        if comet_experiment is not None:
+            comet_experiment.set_epoch(epoch)    
         for (i, dict_) in tq:
             datadict : dict[str,torch.Tensor] = dict_
             position_history = datadict["hist"][:,:,coordinate_idx_history].to(device=device, dtype=dtype)
             vel_history = datadict["hist_vel"][:,:,coordinate_idx_history].to(device=device, dtype=dtype)
 
             quat_history = datadict["hist_quats"][:,:,quaternion_idx_history].to(device=device, dtype=dtype)
-            quat_history = quat_history*torch.sign(quat_history[:,:,-1])[...,None]
+            quat_history = quat_history*(torch.sign(quat_history[:,:,-1])[...,None])
 
             position_future = datadict["fut"].to(device=device, dtype=dtype)
             vel_future = datadict["fut_vel"].to(device=device, dtype=dtype)
@@ -121,21 +134,42 @@ def train(config : dict = None, tempdir : str = None, num_epochs : int = 200,
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            tq.set_postfix({"loss" : loss.item()})
+            if comet_experiment is None:
+                tq.set_postfix({"loss" : loss.item()})
+            elif (i%20)==0:
+                comet_experiment.log_metric("ade", loss.item())
+        model_file = os.path.join(tempdir, "model_%d.pt" % (epoch,))
+        optimizer_file = os.path.join(tempdir, "optimizer_%d.pt" % (epoch,))
+        with open(model_file, "wb") as f:
+            torch.save(network.state_dict(), f)
+        with open(optimizer_file, "wb") as f:
+            torch.save(optimizer.state_dict(), f)
+        if comet_experiment is not None:
+            comet_experiment.log_asset(model_file, file_name=os.path.basename(model_file), copy_to_tmp=False)  
+            comet_experiment.log_asset(optimizer_file, file_name=os.path.basename(optimizer_file), copy_to_tmp=False)  
 
     
 
 
 
 def prepare_and_train(argdict : dict):
-    global comet_experiment
     tempdir = argdict["tempdir"]
     workers = argdict["workers"]
     config_file = argdict["config_file"]
     gpu = argdict["gpu"]
     with open(config_file, "r") as f:
         config : dict = yaml.load(f, Loader=yaml.SafeLoader)
-    train(config=config, workers=workers, tempdir=tempdir, api_key=os.getenv("COMET_API_KEY"), gpu=gpu)
+    api_key=os.getenv("COMET_API_KEY")
+    if api_key is not None:
+        comet_experiment = comet_ml.Experiment(api_key=api_key, 
+                                               project_name="bamf", 
+                                               workspace="electric-turtle",
+                                               auto_metric_logging=False
+                                               )
+        comet_experiment.log_asset(config_file, file_name="config.yaml", overwrite=True)
+    else:
+        comet_experiment = None
+    train(config=config, workers=workers, tempdir=tempdir, comet_experiment = comet_experiment, gpu=gpu)
 
     
             
